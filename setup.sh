0d0f8b5f11bea3b70cb42301454c23af9bb6d1a1
jpata <joosep.pata@gmail.com>
2012-09-08 10:24:31 +0300

    added corect tags for 5_3_3*

--- setup.sh
+++ setup.sh
@@ -4,12 +4,18 @@ cmsrel CMSSW_5_3_3_patch3
 cd CMSSW_5_3_3_patch3
 cmsenv
 cd $CMSSW_BASE/src
-addpkg DataFormats/PatCandidates            V06-05-06
-addpkg PhysicsTools/PatAlgos                V08-09-31
-addpkg PhysicsTools/PatUtils                V03-09-26
-addpkg CommonTools/ParticleFlow             V00-03-16
-addpkg CommonTools/RecoUtils                V00-00-12
-addpkg CommonTools/RecoAlgos                V00-03-23
-addpkg DataFormats/ParticleFlowCandidate    V15-03-02      
-addpkg DataFormats/TrackReco                V10-02-02      
-addpkg DataFormats/VertexReco               V02-00-04 
+addpkg PhysicsTools/PatAlgos   V08-09-23      
+addpkg PhysicsTools/PatUtils   V03-09-23      
+addpkg CommonTools/ParticleFlow   V00-03-16
+addpkg CommonTools/RecoUtils   V00-00-12                                  
+addpkg CommonTools/RecoAlgos  V00-03-23        
+addpkg DataFormats/ParticleFlowCandidate   V15-03-02      
+addpkg DataFormats/TrackReco   V10-02-02      
+addpkg DataFormats/VertexReco   V02-00-04 
+
+##Electron ID setup
+#cvs co -r V00-00-13 -d EGamma/EGammaAnalysisTools UserCode/EGamma/EGammaAnalysisTools
+#cd EGamma/EGammaAnalysisTools/data
+#cat download.url | xargs wget
+
+cd $CMSSW_BASE/src
