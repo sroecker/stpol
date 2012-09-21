579a8f30a252a1c72fdb69fd912532e4f329dc62
jpata <joosep.pata@gmail.com>
2012-09-21 16:23:01 +0300

    simpleEventAnalyzer more flexible

--- CMSSW_5_3_4_cand1/src/SingleTopPolarization/SimpleEventAnalyzer/simpleeventanalyzer_cfg.py
+++ CMSSW_5_3_4_cand1/src/SingleTopPolarization/SimpleEventAnalyzer/simpleeventanalyzer_cfg.py
@@ -7,15 +7,19 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
-    # replace 'myfile.root' with the source file you want to use
-    fileNames = cms.untracked.vstring(
-        'file:/home/joosep/singletop/stpol/patTuple_slim_numEvent100.root'
+    fileNames = cms.untracked.vstring(""
+#        'file:/home/joosep/singletop/stpol/patTuple_slim_numEvent100.root'
 #        'file:/hdfs/local/stpol/joosep/FEFF01BD-87DC-E111-BC9E-003048678F8E.root'
     )
 )
 
-process.demo = cms.EDAnalyzer('SimpleEventAnalyzer'
+from SingleTopPolarization.Analysis.cmdlineParsing import enableCommandLineArguments
+enableCommandLineArguments(process)
+
+process.simpleAnalyzer = cms.EDAnalyzer(
+	'SimpleEventAnalyzer',
+	interestingCollection = cms.untracked.string("goodElectrons")
 )
 
 
-process.p = cms.Path(process.demo)
+process.p = cms.Path(process.simpleAnalyzer)
