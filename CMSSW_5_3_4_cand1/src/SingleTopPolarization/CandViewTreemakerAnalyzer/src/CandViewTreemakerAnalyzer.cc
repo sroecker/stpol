401c6603573d6016b116f16dbab93d1ce264bb63
jpata <joosep.pata@gmail.com>
2012-09-24 17:05:50 +0300

    candviewtreemaker is now generic friendly

--- CMSSW_5_3_4_cand1/src/SingleTopPolarization/CandViewTreemakerAnalyzer/src/CandViewTreemakerAnalyzer.cc
+++ CMSSW_5_3_4_cand1/src/SingleTopPolarization/CandViewTreemakerAnalyzer/src/CandViewTreemakerAnalyzer.cc
@@ -1,19 +1,25 @@
 // -*- C++ -*-
 //
-// Package:    CandViewTreemakerAnalyzer
-// Class:      CandViewTreemakerAnalyzer
+// Package:    GenericViewTreemakerAnalyzer
+// Class:      GenericViewTreemakerAnalyzer
 // 
-/**\class CandViewTreemakerAnalyzer CandViewTreemakerAnalyzer.cc SingleTopPolarization/CandViewTreemakerAnalyzer/src/CandViewTreemakerAnalyzer.cc
+/**\class GenericViewTreemakerAnalyzerGenericViewTreemakerAnalyzer GenericViewTreemakerAnalyzer.cc SingleTopPolarization/GenericViewTreemakerAnalyzer/src/GenericViewTreemakerAnalyzer.cc
 
- Description: This class produces a TTree, the branches of which are the specified components of a reco::Candidate collection
+ Description: This class produces a TTree, the branches of which are the specified components of given collections
 
  Implementation:
      This class is implemented using the StringObjectFunction to allow generic
      object properties to be outputted to the TTree. All configuration is done
-     via the python interface.
+     via the python interface. The first template argument T is a collection
+     type object which must be present in the file. A typical value for T
+     is edm::View<reco::Candidate> or reco::CandidateCollection (aka OwnVector).
+
+     The objects in the collection are treated as instances of the class C, meaning
+     that T::value_type must be a base type of C. A typical value for C is
+     pat::Jet if you wish to access the jet properties of an object.
 */
 //
-// Original Author:  
+// Original Author: Joosep Pata
 //         Created:  Fri Sep 21 11:28:31 EEST 2012
 // $Id$
 //
@@ -42,6 +48,16 @@
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 
+//pat::Jet
+#include <DataFormats/PatCandidates/interface/Jet.h>
+#include <DataFormats/PatCandidates/interface/Muon.h>
+
+//OwnVector
+#include <DataFormats/Common/interface/OwnVector.h>
+
+#include "CommonTools/UtilAlgos/interface/SingleObjectSelector.h"
+#include "CommonTools/UtilAlgos/interface/StringCutObjectSelector.h"
+
 #include "TTree.h"
 
 //for ITOA
@@ -54,10 +70,11 @@
 #define ITOA(A) (static_cast<std::ostringstream*>(&(std::ostringstream()<<A) )->str())
 #define D_NAN (std::numeric_limits<double>::quiet_NaN())
 
-class CandViewTreemakerAnalyzer : public edm::EDAnalyzer {
+template <typename T, typename C>
+class GenericViewTreemakerAnalyzer : public edm::EDAnalyzer {
    public:
-      explicit CandViewTreemakerAnalyzer(const edm::ParameterSet&);
-      ~CandViewTreemakerAnalyzer();
+      explicit GenericViewTreemakerAnalyzer(const edm::ParameterSet&);
+      ~GenericViewTreemakerAnalyzer();
 
       static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
@@ -80,10 +97,10 @@ class CandViewTreemakerAnalyzer : public edm::EDAnalyzer {
       StringObjectFunction that allows the variable to be extracted from the
       collection.
       */
-      std::map<std::string, std::map<std::string, StringObjectFunction<reco::Candidate>*>> quantities;
+      std::map<std::string, std::map<std::string, StringObjectFunction<C>* > > quantities;
 
       //All the branch variables
-      std::map<std::string, std::map<std::string, std::vector<double*>>> treeValues;
+      std::map<std::string, std::map<std::string, std::vector<double*> > > treeValues;
 
       //Maximum number of elements per collection
       std::map<std::string, int> maxElems;
@@ -91,6 +108,9 @@ class CandViewTreemakerAnalyzer : public edm::EDAnalyzer {
       edm::Service<TFileService> fs;
 
       TTree* outTree;
+
+      bool makeTree;
+      std::string treeName;
 };
 
 //
@@ -104,32 +124,41 @@ class CandViewTreemakerAnalyzer : public edm::EDAnalyzer {
 //
 // constructors and destructor
 //
-CandViewTreemakerAnalyzer::CandViewTreemakerAnalyzer(const edm::ParameterSet& iConfig)
-
+template <typename T, typename C>
+GenericViewTreemakerAnalyzer<T, C>::GenericViewTreemakerAnalyzer(const edm::ParameterSet& iConfig)
 {
-  outTree = fs->make<TTree>("eventTree", "eventTree");
+
+  makeTree = iConfig.getUntrackedParameter<bool>("makeTree", true);
+  treeName = iConfig.getUntrackedParameter<std::string>("treeName", "eventTree");
+
+  if(makeTree) {
+    outTree = fs->make<TTree>(treeName.c_str(), treeName.c_str());
+  } else {
+    outTree = fs->getObject<TTree>(treeName.c_str());
+  }
 
   auto collectionsPSets = iConfig.getUntrackedParameter<std::vector<edm::ParameterSet>>("collections");
   for (auto& colPSet : collectionsPSets) {
 
     //The name of the collection to analyze
-    const auto& collection = colPSet.getUntrackedParameter<std::string>("collection");
+    const auto& collection = colPSet.template getUntrackedParameter<std::string>("collection");
 
     //The maximum number of elements from a collection to write down
-    maxElems[collection] = colPSet.getUntrackedParameter<int>("maxElems");
+    maxElems[collection] = colPSet.template getUntrackedParameter<int>("maxElems");
 
     //The vector of variables to get from the collections
-    const auto& varPSets = colPSet.getUntrackedParameter<std::vector<edm::ParameterSet>>("variables");
+    const auto& varPSets = colPSet.template getUntrackedParameter<std::vector<edm::ParameterSet>>("variables");
 
     for (auto& varPSet : varPSets) {
 
       //The name of the variable in the output TTree
-      const auto& tag = varPSet.getUntrackedParameter<std::string>("tag");
+      const auto& tag = varPSet.template getUntrackedParameter<std::string>("tag");
 
       //The expression to evaluate on an element of the collection
-      const auto& expr = varPSet.getUntrackedParameter<std::string>("expr");
+      const auto& expr = varPSet.template getUntrackedParameter<std::string>("expr");
 
-      quantities[collection][tag] = new StringObjectFunction<reco::Candidate>(expr, false);
+      StringObjectFunction<C>* t = new StringObjectFunction<C>(expr, false);
+      quantities[collection][tag] = t;
       for(int i=0;i<maxElems[collection];i++) {
         double* d = new double(D_NAN);
         treeValues[collection][tag].push_back(d);
@@ -141,7 +170,8 @@ CandViewTreemakerAnalyzer::CandViewTreemakerAnalyzer(const edm::ParameterSet& iC
 }
 
 
-CandViewTreemakerAnalyzer::~CandViewTreemakerAnalyzer()
+template <typename T, typename C>
+GenericViewTreemakerAnalyzer<T, C>::~GenericViewTreemakerAnalyzer()
 {
   for(auto& collectionVarMap : quantities) {
     auto& varMap = collectionVarMap.second;
@@ -167,14 +197,15 @@ CandViewTreemakerAnalyzer::~CandViewTreemakerAnalyzer()
 //
 
 // ------------ method called for each event  ------------
+template <typename T, typename C>
 void
-CandViewTreemakerAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+GenericViewTreemakerAnalyzer<T, C>::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
   //std::cout << "Begin processing event" << std::endl;
 
   using namespace edm;
 
-  Handle<edm::View<reco::Candidate>> objects;
+  Handle<T> objects;
 
   //Initialize all branch variables
   for (auto& cols : treeValues) {
@@ -197,7 +228,9 @@ CandViewTreemakerAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSet
       for (auto& var : varMap) {
         auto& tag = var.first;
         auto& varParser = *(var.second);
-        const double&& value = varParser(obj);
+        //const double value = varParser((pat::Jet)(obj));
+        const C* pObj = static_cast<const C*>(&obj);
+        const double value = varParser(*pObj);
         //std::cout << collectionName << "[" << i << "] " << tag << " " << value << std::endl;
         if (i<maxElems[collectionName]) {
           *(treeValues[collectionName][tag][i]) = value;
@@ -226,50 +259,75 @@ CandViewTreemakerAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSet
 
 
 // ------------ method called once each job just before starting event loop  ------------
+template <typename T, typename C>
 void 
-CandViewTreemakerAnalyzer::beginJob()
+GenericViewTreemakerAnalyzer<T, C>::beginJob()
 {
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
+template <typename T, typename C>
 void 
-CandViewTreemakerAnalyzer::endJob() 
+GenericViewTreemakerAnalyzer<T, C>::endJob() 
 {
 }
 
 // ------------ method called when starting to processes a run  ------------
+template <typename T, typename C>
 void 
-CandViewTreemakerAnalyzer::beginRun(edm::Run const&, edm::EventSetup const&)
+GenericViewTreemakerAnalyzer<T, C>::beginRun(edm::Run const&, edm::EventSetup const&)
 {
 }
 
 // ------------ method called when ending the processing of a run  ------------
+template <typename T, typename C>
 void 
-CandViewTreemakerAnalyzer::endRun(edm::Run const&, edm::EventSetup const&)
+GenericViewTreemakerAnalyzer<T, C>::endRun(edm::Run const&, edm::EventSetup const&)
 {
 }
 
 // ------------ method called when starting to processes a luminosity block  ------------
+template <typename T, typename C>
 void 
-CandViewTreemakerAnalyzer::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+GenericViewTreemakerAnalyzer<T, C>::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
 {
 }
 
 // ------------ method called when ending the processing of a luminosity block  ------------
+template <typename T, typename C>
 void 
-CandViewTreemakerAnalyzer::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+GenericViewTreemakerAnalyzer<T, C>::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
 {
 }
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+template <typename T, typename C>
 void
-CandViewTreemakerAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+GenericViewTreemakerAnalyzer<T, C>::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   //The following says we do not know what parameters are allowed so do no validation
   // Please change this to state exactly what you do use, even if it is no parameters
-  //edm::ParameterSetDescription desc;
-  //desc.setUnknown();
-  //descriptions.addDefault(desc);
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
 }
+
+// typedef SingleObjectSelector<edm::View<pat::Jet>,
+// StringCutObjectSelector< pat::Jet>,
+// edm::OwnVector<pat::Jet, edm::ClonePolicy<pat::Jet>> > JetViewSelector;
+
+//typedef SingleObjectSelector< edm::View<pat::Jet>, StringCutObjectSelector<pat::Jet, true>, edm::OwnVector<pat::Jet, edm::ClonePolicy<pat::Jet>> > JetViewSelector;
+
+typedef GenericViewTreemakerAnalyzer<edm::View<reco::Candidate>, reco::Candidate> CandViewTreemakerAnalyzer;
+typedef GenericViewTreemakerAnalyzer<edm::View<reco::Candidate>, pat::Jet> JetViewTreemakerAnalyzer;
+typedef GenericViewTreemakerAnalyzer<reco::CandidateCollection, pat::Jet> JetCandOwnVectorTreemakerAnalyzer;
+typedef GenericViewTreemakerAnalyzer<reco::CandidateCollection, pat::Muon> MuonCandOwnVectorTreemakerAnalyzer;
+//typedef GenericViewTreemakerAnalyzer<edm::OwnVector<pat::Jet, edm::ClonePolicy<pat::Jet>>> JetVectorTreemakerAnalyzer;
 
 //define this as a plug-in
 DEFINE_FWK_MODULE(CandViewTreemakerAnalyzer);
+DEFINE_FWK_MODULE(JetViewTreemakerAnalyzer);
+DEFINE_FWK_MODULE(JetCandOwnVectorTreemakerAnalyzer);
+DEFINE_FWK_MODULE(MuonCandOwnVectorTreemakerAnalyzer);
+//DEFINE_FWK_MODULE(JetVectorTreemakerAnalyzer);
+//DEFINE_FWK_MODULE(JetViewSelector);
+
