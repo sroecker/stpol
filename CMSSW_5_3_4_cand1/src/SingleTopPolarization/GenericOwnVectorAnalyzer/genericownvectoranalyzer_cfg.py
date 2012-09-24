c3af1090b3d1e48a145b582656b5a0e2ec835367
jpata <joosep.pata@gmail.com>
2012-09-24 17:09:40 +0300

    added new generic example ownvector analyzer with object casting

---
+++ CMSSW_5_3_4_cand1/src/SingleTopPolarization/GenericOwnVectorAnalyzer/genericownvectoranalyzer_cfg.py
@@ -0,0 +1,26 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(""
+#        'file:/home/joosep/singletop/stpol/patTuple_slim_numEvent100.root'
+#        'file:/hdfs/local/stpol/joosep/FEFF01BD-87DC-E111-BC9E-003048678F8E.root'
+    )
+)
+
+#Command-line arguments
+from SingleTopPolarization.Analysis.cmdlineParsing import enableCommandLineArguments
+enableCommandLineArguments(process)
+
+process.simpleAnalyzer = cms.EDAnalyzer(
+	'GenericOwnVectorAnalyzer',
+	interestingCollection = cms.untracked.string("bTagsTCHPT")
+)
+
+
+process.p = cms.Path(process.simpleAnalyzer)
