95fb7733f8a88039ced1a301c3b48fe6258fa457
jpata <joosep.pata@gmail.com>
2012-09-25 18:49:34 +0300

    correct lepton veto

--- CMSSW_5_3_4_cand1/src/SingleTopPolarization/Analysis/python/selection_step2_cfg.py
+++ CMSSW_5_3_4_cand1/src/SingleTopPolarization/Analysis/python/selection_step2_cfg.py
@@ -1,10 +1,14 @@
 import FWCore.ParameterSet.Config as cms
+from SingleTopPolarization.Analysis.eventCounting import *
 
-process = cms.Process("STPOL2")
+process = cms.Process("STPOLSEL2")
+countProcessed(process)
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
@@ -12,18 +16,10 @@ process.source = cms.Source("PoolSource",
     )
 )
 
-process.out = cms.OutputModule("PoolOutputModule",
-    fileName = cms.untracked.string('output_step2.root'),
-     SelectEvents = cms.untracked.PSet(
-         SelectEvents = cms.vstring('p')
-     ),
-    outputCommands = cms.untracked.vstring('keep *')
-)
-process.outpath = cms.EndPath(process.out)
-
 #Command-line arguments
 from SingleTopPolarization.Analysis.cmdlineParsing import enableCommandLineArguments
 enableCommandLineArguments(process)
+
 
 process.bTagsTCHPtight = cms.EDFilter(
 	"CandViewSelector",
@@ -50,13 +46,46 @@ process.oneIsoMu = cms.EDFilter(
     maxNumber = cms.uint32(1),
 )
 
-process.looseMuVeto = cms.EDFilter(
+process.oneIsoEle = cms.EDFilter(
     "PATCandViewCountFilter",
-    src = cms.InputTag("looseVetoMuons"),
-    minNumber = cms.uint32(2),
-    maxNumber = cms.uint32(2),
+    src = cms.InputTag("goodSignalElectrons"),
+    minNumber = cms.uint32(1),
+    maxNumber = cms.uint32(1),
 )
 
+#in mu path we must have 1 loose muon (== THE isolated muon)
+process.looseMuVetoMu = cms.EDFilter(
+    "PATCandViewCountFilter",
+    src = cms.InputTag("looseVetoMuons"),
+    minNumber = cms.uint32(1),
+    maxNumber = cms.uint32(1),
+)
+
+#In Muon path we must have 0 loose electrons
+process.looseEleVetoMu = cms.EDFilter(
+    "PATCandViewCountFilter",
+    src = cms.InputTag("looseVetoElectrons"),
+    minNumber = cms.uint32(0),
+    maxNumber = cms.uint32(0),
+)
+
+#In Electron path we must have 1 loose electron (== the isolated electron)
+process.looseEleVetoEle = cms.EDFilter(
+    "PATCandViewCountFilter",
+    src = cms.InputTag("looseVetoElectrons"),
+    minNumber = cms.uint32(1),
+    maxNumber = cms.uint32(1),
+)
+
+#In Electron path we must have 0 loose muons
+process.looseMuVetoEle = cms.EDFilter(
+    "PATCandViewCountFilter",
+    src = cms.InputTag("looseVetoMuons"),
+    minNumber = cms.uint32(0),
+    maxNumber = cms.uint32(0),
+)
+
+#Require exactly N jets
 process.nJets = cms.EDFilter(
     "PATCandViewCountFilter",
     src = cms.InputTag("goodJets"),
@@ -64,6 +93,7 @@ process.nJets = cms.EDFilter(
     maxNumber = cms.uint32(2),
 )
 
+#Require exactly M bTags of the given type
 process.mBTags = cms.EDFilter(
     "PATCandViewCountFilter",
     src = cms.InputTag("bTagsTCHPtight"),
@@ -71,12 +101,43 @@ process.mBTags = cms.EDFilter(
     maxNumber = cms.uint32(1),
 )
 
-process.p = cms.Path(
+process.muonPathPreCount = cms.EDProducer("EventCountProducer")
+process.muPath = cms.Path(
+    process.muonPathPreCount *
     process.oneIsoMu * 
-    process.looseMuVeto * 
+    process.looseMuVetoMu * 
+    process.looseEleVetoMu * 
     process.nJets *
     process.bTagsCSVmedium *
     process.bTagsCSVtight *
     process.bTagsTCHPtight *
     process.mBTags
 )
+countAfter(process, process.muPath, ["oneIsoMu", "looseMuVetoMu", "looseEleVetoMu", "nJets", "mBTags"])
+
+process.elePath = cms.Path(
+    process.muonPathPreCount *
+    process.oneIsoEle * 
+    process.looseEleVetoEle * 
+    process.looseMuVetoEle * 
+    process.nJets *
+    process.bTagsCSVmedium *
+    process.bTagsCSVtight *
+    process.bTagsTCHPtight *
+    process.mBTags
+)
+countAfter(process, process.elePath, ["oneIsoEle", "looseEleVetoEle", "looseMuVetoEle", "nJets", "mBTags"])
+
+
+#-----------------------------------------------
+# Outpath
+#-----------------------------------------------
+
+process.out = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string('out_step2.root'),
+     SelectEvents = cms.untracked.PSet(
+         SelectEvents = cms.vstring(['muPath', 'elePath'])
+     ),
+    outputCommands = cms.untracked.vstring('keep *')
+)
+process.outpath = cms.EndPath(process.out)
