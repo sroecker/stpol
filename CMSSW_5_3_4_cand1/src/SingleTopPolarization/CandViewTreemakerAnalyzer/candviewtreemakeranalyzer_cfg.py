10651dafef9e938e640753782577002cfee40d49
jpata <joosep.pata@gmail.com>
2012-09-25 09:12:36 +0300

    renamed process

--- CMSSW_5_3_4_cand1/src/SingleTopPolarization/CandViewTreemakerAnalyzer/candviewtreemakeranalyzer_cfg.py
+++ CMSSW_5_3_4_cand1/src/SingleTopPolarization/CandViewTreemakerAnalyzer/candviewtreemakeranalyzer_cfg.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("Demo")
+process = cms.Process("TREEMAKER")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
@@ -9,7 +9,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        'file:myfile.root'
+    	""
     )
 )
 
