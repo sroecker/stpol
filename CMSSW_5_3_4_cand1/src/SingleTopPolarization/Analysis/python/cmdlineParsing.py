c1544419d8c774f479dd584f4c6493f1efd8bd25
jpata <joosep.pata@gmail.com>
2012-09-27 11:00:47 +0300

    default outputfile (actually not needed)

--- CMSSW_5_3_4_cand1/src/SingleTopPolarization/Analysis/python/cmdlineParsing.py
+++ CMSSW_5_3_4_cand1/src/SingleTopPolarization/Analysis/python/cmdlineParsing.py
@@ -4,6 +4,9 @@ import FWCore.ParameterSet.Config as cms
 def enableCommandLineArguments(process):
     options = VarParsing('analysis')
     options.parseArguments()
+    # if options.outputFile=="":
+    # 	options.outputFile = "out_%s.root" % process.name_()
+    # print "output: %s" % options.outputFile
     process.source.fileNames = cms.untracked.vstring(options.inputFiles)
     process.maxEvents = cms.untracked.PSet(
       input = cms.untracked.int32(options.maxEvents)
