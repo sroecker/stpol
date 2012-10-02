93e95abc13565c981f8f51bc045ffb4743b876d8
jpata <joosep.pata@gmail.com>
2012-10-02 19:07:08 +0300

    added tops to trees

--- CMSSW_5_3_4/src/SingleTopPolarization/Analysis/python/treemaker_step3_cfg.py
+++ CMSSW_5_3_4/src/SingleTopPolarization/Analysis/python/treemaker_step3_cfg.py
@@ -8,7 +8,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    	""
+        ""
     )
 )
 
@@ -16,189 +16,223 @@ from SingleTopPolarization.Analysis.cmdlineParsing import enableCommandLineArgum
 enableCommandLineArguments(process)
 
 process.treesBJets = cms.EDAnalyzer('JetCandOwnVectorTreemakerAnalyzer',
-	makeTree = cms.untracked.bool(True),
-	treeName = cms.untracked.string("eventTree"),
-	collections = cms.untracked.VPSet(
-		cms.untracked.PSet(
-			collection = cms.untracked.string("bTagsTCHPtight"),
-			maxElems = cms.untracked.int32(1),
-			variables = cms.untracked.VPSet(
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Pt"),
-					expr = cms.untracked.string("pt")
-				), 
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Eta"),
-					expr = cms.untracked.string("eta")
-				),
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Phi"),
-					expr = cms.untracked.string("phi")
-				),
-				cms.untracked.PSet(
-					tag = cms.untracked.string("bTag"),
-					expr = cms.untracked.string("bDiscriminator('trackCountingHighPurBJetTags')")
-				),
-			)
-		),
-	)
+        makeTree = cms.untracked.bool(True),
+        treeName = cms.untracked.string("eventTree"),
+        collections = cms.untracked.VPSet(
+                cms.untracked.PSet(
+                        collection = cms.untracked.string("bTagsTCHPtight"),
+                        maxElems = cms.untracked.int32(1),
+                        variables = cms.untracked.VPSet(
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Pt"),
+                                        expr = cms.untracked.string("pt")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Eta"),
+                                        expr = cms.untracked.string("eta")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Phi"),
+                                        expr = cms.untracked.string("phi")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("bTag"),
+                                        expr = cms.untracked.string("bDiscriminator('trackCountingHighPurBJetTags')")
+                                ),
+                        )
+                ),
+        )
+)
+
+process.treesTop = cms.EDAnalyzer('CompositeCandViewTreemakerAnalyzer',
+        makeTree = cms.untracked.bool(True),
+        treeName = cms.untracked.string("eventTree"),
+        collections = cms.untracked.VPSet(
+                cms.untracked.PSet(
+                        collection = cms.untracked.string("recoTop"),
+                        maxElems = cms.untracked.int32(1),
+                        variables = cms.untracked.VPSet(
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Pt"),
+                                        expr = cms.untracked.string("pt")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Eta"),
+                                        expr = cms.untracked.string("eta")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Phi"),
+                                        expr = cms.untracked.string("phi")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Et"),
+                                        expr = cms.untracked.string("et")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("topMass"),
+                                        expr = cms.untracked.string("p4().M()")
+                                ),
+                        )
+                ),
+        )
 )
 
 process.treesJets = cms.EDAnalyzer('JetCandViewTreemakerAnalyzer',
-	makeTree = cms.untracked.bool(True),
-	treeName = cms.untracked.string("eventTree"),
-	collections = cms.untracked.VPSet(
-		cms.untracked.PSet(
-			collection = cms.untracked.string("goodJets"),
-			maxElems = cms.untracked.int32(2),
-			variables = cms.untracked.VPSet(
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Pt"),
-					expr = cms.untracked.string("pt")
-				), 
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Eta"),
-					expr = cms.untracked.string("eta")
-				),
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Phi"),
-					expr = cms.untracked.string("phi")
-				),
-				cms.untracked.PSet(
-					tag = cms.untracked.string("bTag"),
-					expr = cms.untracked.string("bDiscriminator('trackCountingHighPurBJetTags')")
-				),
-			)
-		),
-	)
+        makeTree = cms.untracked.bool(True),
+        treeName = cms.untracked.string("eventTree"),
+        collections = cms.untracked.VPSet(
+                cms.untracked.PSet(
+                        collection = cms.untracked.string("goodJets"),
+                        maxElems = cms.untracked.int32(2),
+                        variables = cms.untracked.VPSet(
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Pt"),
+                                        expr = cms.untracked.string("pt")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Eta"),
+                                        expr = cms.untracked.string("eta")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Phi"),
+                                        expr = cms.untracked.string("phi")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("bTag"),
+                                        expr = cms.untracked.string("bDiscriminator('trackCountingHighPurBJetTags')")
+                                ),
+                        )
+                ),
+        )
 )
 
 process.treesMu = cms.EDAnalyzer('MuonCandViewTreemakerAnalyzer',
-	makeTree = cms.untracked.bool(True),
-	treeName = cms.untracked.string("eventTree"),
-	collections = cms.untracked.VPSet(
-		cms.untracked.PSet(
-			collection = cms.untracked.string("goodSignalMuons"),
-			maxElems = cms.untracked.int32(1),
-			variables = cms.untracked.VPSet(
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Pt"),
-					expr = cms.untracked.string("pt")
-				), 
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Eta"),
-					expr = cms.untracked.string("eta")
-				),
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Phi"),
-					expr = cms.untracked.string("phi")
-				),
-				cms.untracked.PSet(
-					tag = cms.untracked.string("deltaBetaCorrRelIso"),
-					expr = cms.untracked.string("userFloat('deltaBetaCorrRelIso')")
-				),
-				cms.untracked.PSet(
-					tag = cms.untracked.string("dz"),
-					expr = cms.untracked.string("userFloat('dz')")
-				),
-				cms.untracked.PSet(
-					tag = cms.untracked.string("track_hitPattern_trackerLayersWithMeasurement"),
-					expr = cms.untracked.string("userFloat('track_hitPattern_trackerLayersWithMeasurement')")
-				),
-				cms.untracked.PSet(
-					tag = cms.untracked.string("globalTrack_hitPattern_numberOfValidMuonHits"),
-					expr = cms.untracked.string("userFloat('globalTrack_hitPattern_numberOfValidMuonHits')")
-				),
-				cms.untracked.PSet(
-					tag = cms.untracked.string("innerTrack_hitPattern_numberOfValidPixelHits"),
-					expr = cms.untracked.string("userFloat('innerTrack_hitPattern_numberOfValidPixelHits')")
-				),
-			)
-		),
-		cms.untracked.PSet(
-			collection = cms.untracked.string("goodQCDMuons"),
-			maxElems = cms.untracked.int32(2),
-			variables = cms.untracked.VPSet(
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Pt"),
-					expr = cms.untracked.string("pt")
-				), 
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Eta"),
-					expr = cms.untracked.string("eta")
-				),
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Phi"),
-					expr = cms.untracked.string("phi")
-				),
-				cms.untracked.PSet(
-					tag = cms.untracked.string("deltaBetaCorrRelIso"),
-					expr = cms.untracked.string("userFloat('deltaBetaCorrRelIso')")
-				),
-			)
-		),
-	)
+        makeTree = cms.untracked.bool(True),
+        treeName = cms.untracked.string("eventTree"),
+        collections = cms.untracked.VPSet(
+                cms.untracked.PSet(
+                        collection = cms.untracked.string("goodSignalMuons"),
+                        maxElems = cms.untracked.int32(1),
+                        variables = cms.untracked.VPSet(
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Pt"),
+                                        expr = cms.untracked.string("pt")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Eta"),
+                                        expr = cms.untracked.string("eta")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Phi"),
+                                        expr = cms.untracked.string("phi")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("deltaBetaCorrRelIso"),
+                                        expr = cms.untracked.string("userFloat('deltaBetaCorrRelIso')")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("dz"),
+                                        expr = cms.untracked.string("userFloat('dz')")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("track_hitPattern_trackerLayersWithMeasurement"),
+                                        expr = cms.untracked.string("userFloat('track_hitPattern_trackerLayersWithMeasurement')")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("globalTrack_hitPattern_numberOfValidMuonHits"),
+                                        expr = cms.untracked.string("userFloat('globalTrack_hitPattern_numberOfValidMuonHits')")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("innerTrack_hitPattern_numberOfValidPixelHits"),
+                                        expr = cms.untracked.string("userFloat('innerTrack_hitPattern_numberOfValidPixelHits')")
+                                ),
+                        )
+                ),
+                cms.untracked.PSet(
+                        collection = cms.untracked.string("goodQCDMuons"),
+                        maxElems = cms.untracked.int32(2),
+                        variables = cms.untracked.VPSet(
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Pt"),
+                                        expr = cms.untracked.string("pt")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Eta"),
+                                        expr = cms.untracked.string("eta")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Phi"),
+                                        expr = cms.untracked.string("phi")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("deltaBetaCorrRelIso"),
+                                        expr = cms.untracked.string("userFloat('deltaBetaCorrRelIso')")
+                                ),
+                        )
+                ),
+        )
 )
 
 process.treesEle = cms.EDAnalyzer('ElectronCandViewTreemakerAnalyzer',
-	makeTree = cms.untracked.bool(True),
-	treeName = cms.untracked.string("eventTree"),
-	collections = cms.untracked.VPSet(
-		cms.untracked.PSet(
-			collection = cms.untracked.string("goodSignalElectrons"),
-			maxElems = cms.untracked.int32(1),
-			variables = cms.untracked.VPSet(
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Pt"),
-					expr = cms.untracked.string("pt")
-				), 
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Eta"),
-					expr = cms.untracked.string("eta")
-				),
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Phi"),
-					expr = cms.untracked.string("phi")
-				),
-				cms.untracked.PSet(
-					tag = cms.untracked.string("rhoCorrRelIso"),
-					expr = cms.untracked.string("userFloat('rhoCorrRelIso')")
-				),
-			)
-		),
-		cms.untracked.PSet(
-			collection = cms.untracked.string("goodQCDElectrons"),
-			maxElems = cms.untracked.int32(2),
-			variables = cms.untracked.VPSet(
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Pt"),
-					expr = cms.untracked.string("pt")
-				), 
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Eta"),
-					expr = cms.untracked.string("eta")
-				),
-				cms.untracked.PSet(
-					tag = cms.untracked.string("Phi"),
-					expr = cms.untracked.string("phi")
-				),
-				cms.untracked.PSet(
-					tag = cms.untracked.string("rhoCorrRelIso"),
-					expr = cms.untracked.string("userFloat('rhoCorrRelIso')")
-				),
-			)
-		),
-	)
+        makeTree=cms.untracked.bool(True),
+        treeName = cms.untracked.string("eventTree"),
+        collections = cms.untracked.VPSet(
+                cms.untracked.PSet(
+                        collection = cms.untracked.string("goodSignalElectrons"),
+                        maxElems = cms.untracked.int32(1),
+                        variables = cms.untracked.VPSet(
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Pt"),
+                                        expr = cms.untracked.string("pt")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Eta"),
+                                        expr = cms.untracked.string("eta")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Phi"),
+                                        expr = cms.untracked.string("phi")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("rhoCorrRelIso"),
+                                        expr = cms.untracked.string("userFloat('rhoCorrRelIso')")
+                                ),
+                        )
+                ),
+                cms.untracked.PSet(
+                        collection = cms.untracked.string("goodQCDElectrons"),
+                        maxElems = cms.untracked.int32(2),
+                        variables = cms.untracked.VPSet(
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Pt"),
+                                        expr = cms.untracked.string("pt")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Eta"),
+                                        expr = cms.untracked.string("eta")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("Phi"),
+                                        expr = cms.untracked.string("phi")
+                                ),
+                                cms.untracked.PSet(
+                                        tag = cms.untracked.string("rhoCorrRelIso"),
+                                        expr = cms.untracked.string("userFloat('rhoCorrRelIso')")
+                                ),
+                        )
+                ),
+        )
 )
 
 process.TFileService = cms.Service(
     "TFileService",
-    fileName = cms.string("out_step3_trees.root"),
+    fileName=cms.string("out_step3_trees.root"),
 )
 
 process.p = cms.Path(
-	process.treesJets *
-	process.treesBJets *
-	process.treesMu *
-	process.treesEle
+		process.treesTop *
+        process.treesJets *
+        process.treesBJets *
+        process.treesMu *
+        process.treesEle
 )
