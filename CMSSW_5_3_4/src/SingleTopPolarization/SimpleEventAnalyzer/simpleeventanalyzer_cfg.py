3a7016752564c9dbfc5b006d030cefd85f0b4a2e
jpata <joosep.pata@gmail.com>
2012-10-02 19:09:23 +0300

    changed message logging

--- CMSSW_5_3_4/src/SingleTopPolarization/SimpleEventAnalyzer/simpleeventanalyzer_cfg.py
+++ CMSSW_5_3_4/src/SingleTopPolarization/SimpleEventAnalyzer/simpleeventanalyzer_cfg.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("Demo")
 
-process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
