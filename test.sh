02fddddc90c0f181558f99d8cc53191685586c99
jpata <joosep.pata@gmail.com>
2012-10-04 12:46:02 +0300

    removed step3:treemaker

--- test.sh
+++ test.sh
@@ -6,5 +6,4 @@ ANCODE=$CMSSW_BASE'/src/SingleTopPolarization/Analysis/python'
 
 cmsRun $ANCODE/selection_step2_cfg.py inputFiles=file:$STEP1OF outputFile=$STEP2OF
 cmsRun $ANCODE/efficiency_cfg.py inputFiles=file:$STEP2OF
-cmsRun $ANCODE/treemaker_step3_cfg.py inputFiles=file:$STEP2OF
 python analysis.py -b
