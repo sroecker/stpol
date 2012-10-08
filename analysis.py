2a03c562fc78edadf95df195a82716150ab56602
jpata <joosep.pata@gmail.com>
2012-10-08 19:04:56 +0300

    formatting

--- analysis.py
+++ analysis.py
@@ -12,57 +12,57 @@ if args.fileName=="":
     sys.exit(1)
 
 def tb(batch, canv=None):
-	if canv==None:
-		ROOT.gROOT.SetBatch(batch)
-	else:
-		canv.SetBatch(batch)
+    if canv==None:
+        ROOT.gROOT.SetBatch(batch)
+    else:
+        canv.SetBatch(batch)
 
 
 def etaPlots():
-	f = ROOT.TFile("out_step3_trees.root")
-	print f
-	trees = dict()
+    f = ROOT.TFile("out_step3_trees.root")
+    print f
+    trees = dict()
 
-	trees["jets"] = f.Get("treesJets").Get("eventTree")
-	trees["muons"] = f.Get("treesMu").Get("eventTree")
+    trees["jets"] = f.Get("treesJets").Get("eventTree")
+    trees["muons"] = f.Get("treesMu").Get("eventTree")
 
-	t = trees["jets"]
-	t.AddFriend(trees["muons"])
-	ROOT.gROOT.cd()
+    t = trees["jets"]
+    t.AddFriend(trees["muons"])
+    ROOT.gROOT.cd()
 
-	c = ROOT.TCanvas()
-	tb(True, c)
-	h0 = ROOT.TH1F("h0", "h0", 10, 0, 5)
-	h1 = ROOT.TH1F("h1", "h1", 10, 0, 5)
-	t.Draw("abs(goodJets_0_Eta)>>h0")
-	t.Draw("abs(goodJets_1_Eta)>>h1")
-	tb(False, c)
-	c.Close()
+    c = ROOT.TCanvas()
+    tb(True, c)
+    h0 = ROOT.TH1F("h0", "h0", 10, 0, 5)
+    h1 = ROOT.TH1F("h1", "h1", 10, 0, 5)
+    t.Draw("abs(goodJets_0_Eta)>>h0")
+    t.Draw("abs(goodJets_1_Eta)>>h1")
+    tb(False, c)
+    c.Close()
 
-	h0.SetLineColor(ROOT.kRed)
-	h1.SetLineColor(ROOT.kBlue)
+    h0.SetLineColor(ROOT.kRed)
+    h1.SetLineColor(ROOT.kBlue)
 
-	c = ROOT.TCanvas()
-	h0.Draw()
-	h1.Draw("SAME")
-	return c, h0, h1
+    c = ROOT.TCanvas()
+    h0.Draw()
+    h1.Draw("SAME")
+    return c, h0, h1
 
 def effCalcs(inFile):
-	f = ROOT.TFile(inFile)
+    f = ROOT.TFile(inFile)
 
-	histKeys = f.Get("efficiencyAnalyzerMu").GetListOfKeys()
-	hists = [histKeys[i].GetName() for i in range(len(histKeys))]
+    hists = [f.Get("efficiencyAnalyzerMu").Get("muPath"), f.Get("efficiencyAnalyzerEle").Get("elePath")]
 
-	paths = dict()
-	for hn in hists:
-		h = f.Get("efficiencyAnalyzerMu").Get(hn)
-		path = list()
-		for i in range(1,h.GetNbinsX()+1):
-			path.append(h.GetBinContent(i))
-		paths[hn] = path
-	f.Close()
-	return paths
+    paths = dict()
+    for h in hists:
+        path = list()
+        for i in range(1,h.GetNbinsX()+1):
+            path.append(h.GetBinContent(i))
+        paths[h.GetName()] = path
+    f.Close()
+    return paths
 
 A = effCalcs(args.fileName)
-muCounts = map(int, A["muPath"][-7:])
+muCounts = map(int, A["muPath"])
+eleCounts = map(int, A["elePath"])
 print("mu counts: " + ' | '.join('{0}'.format(k) for k in muCounts))
+print("ele counts: " + ' | '.join('{0}'.format(k) for k in eleCounts))
