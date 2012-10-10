8a95a6a80aafd9767806613ecc070978ed26d19a
jpata <joosep.pata@gmail.com>
2012-10-10 08:41:32 +0300

    cmdline args to analysis.py

--- test.sh
+++ test.sh
@@ -5,4 +5,4 @@ STEP2OF='out_step2.root'
 ANCODE=$CMSSW_BASE'/src/SingleTopPolarization/Analysis/python'
 
 cmsRun $ANCODE/selection_step2_cfg.py inputFiles=file:$STEP1OF outputFile=$STEP2OF
-python analysis.py -b
+python2.7 analysis.py --file=out_step2_trees.root --batch
