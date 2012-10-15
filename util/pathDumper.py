90e37a279ecb2f29bb3def764fa96282b8edb859
jpata <joosep.pata@gmail.com>
2012-10-15 17:22:31 +0300

    documentation

--- util/pathDumper.py
+++ util/pathDumper.py
@@ -1,16 +1,20 @@
+# Usage: python2.7 pathDumper.py my_cfg myPath
+# my_cfg.py must exist in the cwd
 
 import sys
 import os
 sys.path.append(os.getcwd())
 import importlib
-path = sys.argv[1]
-sys.argv = sys.argv[2:]
-sys.argv.insert(0, path + ".py")
-mod = importlib.import_module(path)
+fname = sys.argv[1]
+ipath = sys.argv[2]
+
+sys.argv = sys.argv[3:]
+sys.argv.insert(0, fname + ".py")
+mod = importlib.import_module(fname)
 process = mod.process
 
-path = process.singleTopPathStep1Mu
-s = map(lambda x: str(x) + "=" + repr(x), [getattr(process, x) for x in str(path).split("+")])
+path = getattr(process, ipath)
+s = map(lambda x: str(x) + " = " + repr(x), [getattr(process, x) for x in str(path).split("+")])
 of = open(path.label() + "_dump.txt", "w")
 of.writelines(s)
 of.close()
