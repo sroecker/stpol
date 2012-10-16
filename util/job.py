c7dc32971deb409bd8134c1b865add7e9036be38
jpata <joosep.pata@gmail.com>
2012-10-16 20:03:52 +0300

    moved job.py

--- util/job.py
+++ util/job.py
@@ -16,8 +16,11 @@ def runJob(jobName, cfg, inf, ofdir=""):
 Tbar_53X = "/store/mc/Summer12_DR53X/T_t-channel_TuneZ2star_8TeV-powheg-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/0077EE51-88DC-E111-88BE-0018F3D09684.root"
 TTBar = "/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/001C868B-B2E1-E111-9BE3-003048D4DCD8.root"
 WJets = "/store/mc/Summer12_DR53X/WJetsToLNu_TuneZ2Star_8TeV-madgraph-tarball/AODSIM/PU_S10_START53_V7A-v1/0001/FE206A01-E4D0-E111-B360-003048673FE6.root"
-#runJob("sync_Tbar_53X", "step1_cfg.py", Tbar_53X, ofdir="sync_step1")
-runJob("sync_Tbar_53X", "step1_noTauTrue_cfg.py", Tbar_53X, ofdir="sync_step1")
+data1 = "/hdfs/cms/store/data/Run2012A/SingleMu/AOD/13Jul2012-v1/00000/FCB9E566-82D3-E111-BD26-0017A4770814.root"
+
+runJob("sync_Tbar_53X", "step1_cfg.py", Tbar_53X, ofdir="sync_step1")
+#runJob("sync_Tbar_53X", "step1_noTauTrue_cfg.py", Tbar_53X, ofdir="sync_step1")
 #runJob("sync_Tbar_53X", "step1_noSkim_cfg.py", Tbar_53X, ofdir="sync_step1")
 #runJob("TTBar_", "step1_cfg.py", TTBar, "testRun_v1")
 #runJob("WJets_", "step1_cfg.py", WJets, "testRun_v1")
+#runJob("data1_", "step1_Data_cfg.py", data1, "testRun_v1")
