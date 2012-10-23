67293315b726a87d5973a3321d51ce957496a636
jpata <joosep.pata@gmail.com>
2012-10-23 10:47:59 +0300

    moved run confs to subdirs

--- runconfs/step1_data_grid_cfg.py
+++ runconfs/step1_data_grid_cfg.py
@@ -1,4 +1,4 @@
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 from SingleTopPolarization.Analysis.selection_step1_cfg import SingleTopStep1
 
-SingleTopStep1(process, doSkimming=True, doSlimming=True)
+SingleTopStep1(process, doSkimming=True, isMC=False, fileName="")
