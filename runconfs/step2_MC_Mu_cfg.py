34c67d57e78e58239be0b033b3e131e917b47c2d
jpata <joosep.pata@gmail.com>
2012-11-08 20:00:42 +0200

    added various run configurations

--- runconfs/step2_MC_Mu_cfg.py
+++ runconfs/step2_MC_Mu_cfg.py
@@ -1,4 +1,4 @@
 import FWCore.ParameterSet.Config as cms
 from SingleTopPolarization.Analysis.selection_step2_cfg import SingleTopStep2
 
-process = SingleTopStep2(isMC=True, filterHLT=True, onGrid=True, doElectron=False)
+process = SingleTopStep2(isMC=True, filterHLT=True, doDebug=False, doElectron=False)
