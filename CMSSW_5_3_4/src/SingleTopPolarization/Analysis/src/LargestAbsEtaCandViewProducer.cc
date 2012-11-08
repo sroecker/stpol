47d267faa4766939364516cf33ca5b238eff0bb4
jpata <joosep.pata@gmail.com>
2012-11-08 12:12:06 +0200

    added largest eta cand selector for fwdJet selection

---
+++ CMSSW_5_3_4/src/SingleTopPolarization/Analysis/src/LargestAbsEtaCandViewProducer.cc
@@ -0,0 +1,22 @@
+#include <memory>
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include <CommonTools/UtilAlgos/interface/ObjectSelector.h>
+#include <CommonTools/UtilAlgos/interface/SortCollectionSelector.h>
+#include <DataFormats/RecoCandidate/interface/RecoCandidate.h>
+#include <DataFormats/Candidate/interface/CompositeCandidate.h>
+#include <cmath>
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+template<typename T>
+struct GreaterByAbsEta {
+  typedef T first_argument_type;
+  typedef T second_argument_type;
+  
+  bool operator()( const T & t1, const T & t2 ) const {
+    return std::fabs(t1.eta())>std::fabs(t2.eta());
+  }
+};
+typedef ObjectSelector< SortCollectionSelector< reco::CandidateView, GreaterByAbsEta<reco::Candidate> > > LargestAbsEtaCandViewProducer;
+DEFINE_FWK_MODULE(LargestAbsEtaCandViewProducer);
+
