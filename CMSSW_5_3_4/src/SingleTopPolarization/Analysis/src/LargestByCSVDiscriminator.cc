bb053ba1d2abef9b231cfed7a34e9619f7e816d5
jpata <joosep.pata@gmail.com>
2012-11-12 14:31:47 +0200

    added missing c file for largest by csv discriminator

---
+++ CMSSW_5_3_4/src/SingleTopPolarization/Analysis/src/LargestByCSVDiscriminator.cc
@@ -0,0 +1,23 @@
+#include <memory>
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include <CommonTools/UtilAlgos/interface/ObjectSelector.h>
+#include <CommonTools/UtilAlgos/interface/SortCollectionSelector.h>
+#include <DataFormats/Candidate/interface/CompositeCandidate.h>
+#include <DataFormats/Candidate/interface/Candidate.h>
+#include <cmath>
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include <DataFormats/PatCandidates/interface/Jet.h>
+
+template<typename T>
+struct GreaterByCSVDiscriminator {
+  typedef T first_argument_type;
+  typedef T second_argument_type;
+  
+  bool operator()( const T & t1, const T & t2 ) const {
+    return ((const pat::Jet&)t1).bDiscriminator("combinedSecondaryVertexBJetTags") > ((const pat::Jet&)t2).bDiscriminator("combinedSecondaryVertexBJetTags");
+  }
+};
+typedef ObjectSelector< SortCollectionSelector< edm::View<reco::Candidate>, GreaterByCSVDiscriminator<reco::Candidate> > > LargestCSVDiscriminatorJetViewProducer;
+DEFINE_FWK_MODULE(LargestCSVDiscriminatorJetViewProducer);
+
