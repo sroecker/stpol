2112252d5c08b17de002d9faa4a948cff2c2ee5f
jpata <joosep.pata@gmail.com>
2012-11-22 11:15:26 +0200

    possibility to reverse iso cut

--- runconfs/step2_cfg.py
+++ runconfs/step2_cfg.py
@@ -7,6 +7,7 @@ doMuon = args.inArgs("mu")
 doElectron = args.inArgs("ele")
 nJets = args.getArg("nJ", default=2)
 nBTags = args.getArg("nB", default=1)
+antiIso = args.inArgs("antiIso")
 
 if args.inArgs("sig"):
     channel = "sig"
@@ -19,4 +20,4 @@ doDebug = args.inArgs("DEBUG")
 import FWCore.ParameterSet.Config as cms
 from SingleTopPolarization.Analysis.selection_step2_cfg import SingleTopStep2
 
-process = SingleTopStep2(isMC=isMC, filterHLT=filterHLT, doMuon=doMuon, doElectron=doElectron, channel=channel, onGrid=onGrid, nJets=nJets, nBTags=nBTags, doDebug=doDebug)
+process = SingleTopStep2(isMC=isMC, filterHLT=filterHLT, doMuon=doMuon, doElectron=doElectron, channel=channel, onGrid=onGrid, nJets=nJets, nBTags=nBTags, doDebug=doDebug, reverseIsoCut=antiIso)
