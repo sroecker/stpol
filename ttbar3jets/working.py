1d19f7249ebc623f3bad124012df6bb87e555404
Morten Piibeleht <morten.piibeleht@cern.ch>
2012-11-28 02:22:38 +0200

    working.py now accepts several files as CLI params.

--- ttbar3jets/working.py
+++ ttbar3jets/working.py
@@ -1,20 +1,18 @@
-# Run with python -i working.py <filename>
+# Run with python -i working.py [<filenames>, ...]
 import sys
 import ROOT
 from ROOT import TH1F,THStack,TFile,TBrowser,TColor,TCanvas,TLegend
 
-fname = sys.argv[1]
+for fname in sys.argv[1:]:
+	tfile = TFile(fname)
+	if tfile.IsZombie():
+		print 'Error opening file "%s"'%fname
+		continue
+	hist = tfile.Get('efficiencyAnalyzerMu').Get('muPath')
+	tree = tfile.Get('treesCands').Get('eventTree')
 
-# Configuration
-#fname = 'stpol_TTBar_3J1T_numEvent1000000_trees.root'
-tfile = TFile(fname)
-if tfile.IsZombie():
-	print 'Error opening file'
-	exit()
-hist = tfile.Get('efficiencyAnalyzerMu').Get('muPath')
-tree = tfile.Get('treesCands').Get('eventTree')
-
-print 'File: ', fname
-for i in range(1,11):
-	print "%2i: %f"%(i, hist.GetBinContent(i))
-print "Valid events:", tree.GetEntries('_recoTop_0_Mass==_recoTop_0_Mass')
+	print 'File: ', fname
+	for i in range(1,11):
+		print "%2i: %f"%(i, hist.GetBinContent(i))
+	print "Valid events:", tree.GetEntries('_recoTop_0_Mass==_recoTop_0_Mass')
+	print
