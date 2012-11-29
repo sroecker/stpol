385eb53ae52c006b84928460348b3bf9a48ed14b
jpata <joosep.pata@gmail.com>
2012-11-29 09:33:54 +0200

    debugging

--- runconfs/step2_sync_cfg.py
+++ runconfs/step2_sync_cfg.py
@@ -1,4 +1,4 @@
 import FWCore.ParameterSet.Config as cms
 from SingleTopPolarization.Analysis.selection_step2_cfg import SingleTopStep2
 
-process = SingleTopStep2(isMC=True, filterHLT=True, doMuon=True, doElectron=True, channel='sig', cutJets=True, muonIsoType="deltaBetaCorrRelIso", eleMetType="MET")
+process = SingleTopStep2(isMC=True, filterHLT=True, doMuon=True, doElectron=True, channel='sig', cutJets=True, muonIsoType="deltaBetaCorrRelIso", eleMetType="MET", doDebug=True)
