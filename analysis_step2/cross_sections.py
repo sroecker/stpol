9fcdb8db9147ecf36df588de4c05368217baad5d
jpata <joosep.pata@gmail.com>
2012-12-07 18:02:35 +0200

    added cross-sections file

---
+++ analysis_step2/cross_sections.py
@@ -0,0 +1,16 @@
+#Cross-sections from AN2012-273-v7, page 4
+xs = {
+      "T_t": 56.4
+    , "Tbar_t": 30.7
+    , "T_s": 3.79
+    , "Tbar_s": 1.76
+    , "T_tW": 11.1
+    . "Tbar_tW": 11.1
+    , "TTBar": 234
+    , "WJets": 36257.2
+    , "ZJets": 3503.71
+    , "WW": 54.838
+    , "WZ": 32.3161
+    , "ZZ": 134680
+}
+
