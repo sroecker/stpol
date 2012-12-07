62ea77f677ee8d1dd1eb9f34163e3427a300e83c
jpata <joosep.pata@gmail.com>
2012-12-07 18:01:45 +0200

    added script to create all trees locally

---
+++ util/run_trees.sh
@@ -0,0 +1,8 @@
+. ./util/bcmsRun runconfs/step2_MC_SIG_cfg.py fileLists/T_t.txt trees/T_t
+. ./util/bcmsRun runconfs/step2_MC_SIG_cfg.py fileLists/Tbar_t.txt trees/Tbar_t
+. ./util/bcmsRun runconfs/step2_MC_BKG_cfg.py fileLists/WJets_v3_1_2.txt trees/WJets
+. ./util/bcmsRun runconfs/step2_MC_BKG_cfg.py fileLists/T_tW.txt trees/T_tW
+. ./util/bcmsRun runconfs/step2_MC_BKG_cfg.py fileLists/Tbar_tW.txt trees/Tbar_tW
+. ./util/bcmsRun runconfs/step2_MC_BKG_cfg.py fileLists/T_s.txt trees/T_s
+. ./util/bcmsRun runconfs/step2_MC_BKG_cfg.py fileLists/Tbar_s.txt trees/Tbar_s
+. ./util/bcmsRun runconfs/step2_MC_BKG_cfg.py fileLists/TTBar.txt trees/TTBar
