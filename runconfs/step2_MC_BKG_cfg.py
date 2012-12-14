f3f31eae44a1c64e1c90cd442a03a0231823f24c
jpata <joosep.pata@gmail.com>
2012-12-14 16:12:26 +0200

    added missing files

--- runconfs/step2_MC_BKG_cfg.py
+++ runconfs/step2_MC_BKG_cfg.py
@@ -1,8 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 from SingleTopPolarization.Analysis.selection_step2_cfg import SingleTopStep2, Config
 
-Config.channel = Config.Channel.signal
-Config.filterHLT = False
-Config.doElectron = False
-
+Config.channel = Config.Channel.background
 process = SingleTopStep2()
