b48b615c1a9021b65b0fcf8a7410f1cb8451efb5
jpata <joosep.pata@gmail.com>
2012-12-20 11:46:27 +0200

    choosing correct btag

--- runconfs/step2_sync_cfg.py
+++ runconfs/step2_sync_cfg.py
@@ -2,7 +2,11 @@ import FWCore.ParameterSet.Config as cms
 from SingleTopPolarization.Analysis.selection_step2_cfg import SingleTopStep2, Config
 
 Config.channel = Config.Channel.signal
-Config.filterHLT = False
-Config.doDebug = False
+Config.filterHLT = True
+Config.doDebug = True
+Config.Jets.cutJets = True
+Config.Jets.bTagDiscriminant = Config.Jets.BTagDiscriminant.CSV_MVA
+Config.Jets.bTagWorkingPoint = Config.Jets.BTagWorkingPoint.CSVM
 
+Config.Leptons.cutOnTransverseMass = True
 process = SingleTopStep2()
