598bbea6c753a6cf519e0f994e36f5a948640b58
jpata <joosep.pata@gmail.com>
2012-12-21 16:41:40 +0200

    single isolated lepton

--- CMSSW_5_3_5/src/SingleTopPolarization/Analysis/python/electrons_step2_cfi.py
+++ CMSSW_5_3_5/src/SingleTopPolarization/Analysis/python/electrons_step2_cfi.py
@@ -11,260 +11,263 @@ reverseIsoCut - 'True' to choose the anti-isolated leptons, 'False' to choose is
 """
 def ElectronSetup(process, conf):
 
-	goodElectronCut = "%s>30" % conf.Electrons.pt
-	goodElectronCut += "&& abs(eta)<2.5"
-	goodElectronCut += "&& !(1.4442 < abs(superCluster.eta) < 1.5660)"
-	goodElectronCut += "&& passConversionVeto()"
-	#goodElectronCut += "&& (0.0 < electronID('mvaTrigV0') < 1.0)"
-	if conf.Electrons.cutOnMVA:
-		goodElectronCut += "&& electronID('mvaTrigV0') > %f" % conf.Electrons.mvaCut
-	goodSignalElectronCut = goodElectronCut
-	goodSignalElectronCut += '&& abs(userFloat("dxy")) < 0.02'
-	goodSignalElectronCut += '&& userInt("gsfTrack_trackerExpectedHitsInner_numberOfHits") <= 0'
+    goodElectronCut = "%s>30" % conf.Electrons.pt
+    goodElectronCut += "&& abs(eta)<2.5"
+    goodElectronCut += "&& !(1.4442 < abs(superCluster.eta) < 1.5660)"
+    goodElectronCut += "&& passConversionVeto()"
+    #goodElectronCut += "&& (0.0 < electronID('mvaTrigV0') < 1.0)"
+    if conf.Electrons.cutOnMVA:
+        goodElectronCut += "&& electronID('mvaTrigV0') > %f" % conf.Electrons.mvaCut
+    goodSignalElectronCut = goodElectronCut
+    goodSignalElectronCut += '&& abs(userFloat("dxy")) < 0.02'
+    goodSignalElectronCut += '&& userInt("gsfTrack_trackerExpectedHitsInner_numberOfHits") <= 0'
 
-	if conf.Electrons.cutOnIso:
-	    if conf.Electrons.reverseIsoCut:
-	    #Choose anti-isolated region
-	        goodSignalElectronCut += ' && userFloat("{0}") >= {1} && userFloat("{0}") < {1}'.format(
-	            conf.Electrons.relIsoType,
-	            conf.Electrons.relIsoCutRangeAntiIsolatedRegion[0],
-	            conf.Electrons.relIsoCutRangeAntiIsolatedRegion[1]
-	            )
-	    #Choose isolated region
-	    else:
-	        goodSignalElectronCut += ' && userFloat("{0}") >= {1} && userFloat("{0}") < {2}'.format(
-	            conf.Electrons.relIsoType,
-	            conf.Electrons.relIsoCutRangeIsolatedRegion[0],
-	            conf.Electrons.relIsoCutRangeIsolatedRegion[1]
-	        )
-
-
-	looseVetoElectronCut = "%s > 20" % conf.Electrons.pt
-	looseVetoElectronCut += "&& abs(eta) < 2.5"
-	looseVetoElectronCut += "&& electronID('mvaTrigV0') > %f" % 0.1
-	looseVetoElectronCut += '&& userFloat("{0}") < {1}'.format(conf.Electrons.relIsoType, conf.Electrons.looseVetoRelIsoCut)
-
-	#Loose veto electrons must not overlap with good signal electrons
-	looseVetoElectronCut += "&& !(%s)" % goodSignalElectronCut
-
-	process.goodSignalElectrons = cms.EDFilter("CandViewSelector",
-	  src=cms.InputTag("elesWithIso"), cut=cms.string(goodSignalElectronCut)
-	)
-
-	process.looseVetoElectrons = cms.EDFilter("CandViewSelector",
-	  src=cms.InputTag("elesWithIso"), cut=cms.string(looseVetoElectronCut)
-	)
-
-	process.oneIsoEle = cms.EDFilter(
-		"PATCandViewCountFilter",
-		src=cms.InputTag("goodSignalElectrons"),
-		minNumber=cms.uint32(1),
-		maxNumber=cms.uint32(1),
-	)
-
-	process.electronCount = cms.EDProducer(
-		"CollectionSizeProducer<reco::Candidate>",
-		src = cms.InputTag("goodSignalElectrons")
-	)
-
-	#If loose veto electrons don't have any overlap with signal electrons, there must be none
-	process.looseEleVetoEle = cms.EDFilter(
-		"PATCandViewCountFilter",
-		src=cms.InputTag("looseVetoElectrons"),
-		minNumber=cms.uint32(0),
-		maxNumber=cms.uint32(0),
-	)
-	process.looseMuVetoEle = cms.EDFilter(
-		"PATCandViewCountFilter",
-		src=cms.InputTag("looseVetoMuons"),
-		minNumber=cms.uint32(0),
-		maxNumber=cms.uint32(0),
-	)
-
-	#####################
-	# MET/MtW cutting   #
-	#####################
-	#Either use MET cut or MtW cut
-	if conf.Electrons.transverseMassType == conf.Leptons.WTransverseMassType.MET:
-	    process.goodMETsEle = cms.EDFilter("CandViewSelector",
-	      src=cms.InputTag("patMETs"), cut=cms.string("pt>%f" % conf.Electrons.transverseMassCut)
-	    )
-
-	    process.metEleSequence = cms.Sequence(
-	        process.goodMETsEle
-	    )
-
-	    if conf.Leptons.cutOnTransverseMass:
-	        process.hasMETEle = cms.EDFilter("PATCandViewCountFilter",
-	            src = cms.InputTag("goodMETsEle"),
-	            minNumber = cms.uint32(1),
-	            maxNumber = cms.uint32(1)
-	        )
-	        process.metMuSequence.insert(-1, process.hasMETEle)
-
-	elif conf.Electrons.transverseMassType == conf.Leptons.WTransverseMassType.MtW:
-
-	    #produce the muon and MET invariant transverse mass
-	    process.eleAndMETMT = cms.EDProducer('CandTransverseMassProducer',
-	        collections=cms.untracked.vstring(["patMETs", "goodSignalElectrons"])
-	    )
-
-	    process.metEleSequence = cms.Sequence(
-	        process.eleAndMETMT
-	    )
-
-	    if conf.Leptons.cutOnTransverseMass:
-	        process.hasEleMETMT = cms.EDFilter('EventDoubleFilter',
-	            src=cms.InputTag("eleAndMETMT"),
-	            min=cms.double(conf.Electrons.transverseMassCut),
-	            max=cms.double(9999999)
-	        )
-	        process.metEleSequence += process.hasEleMETMT
+    if conf.Electrons.cutOnIso:
+        if conf.Electrons.reverseIsoCut:
+        #Choose anti-isolated region
+            goodSignalElectronCut += ' && userFloat("{0}") >= {1} && userFloat("{0}") < {2}'.format(
+                conf.Electrons.relIsoType,
+                conf.Electrons.relIsoCutRangeAntiIsolatedRegion[0],
+                conf.Electrons.relIsoCutRangeAntiIsolatedRegion[1]
+                )
+        #Choose isolated region
+        else:
+            goodSignalElectronCut += ' && userFloat("{0}") >= {1} && userFloat("{0}") < {2}'.format(
+                conf.Electrons.relIsoType,
+                conf.Electrons.relIsoCutRangeIsolatedRegion[0],
+                conf.Electrons.relIsoCutRangeIsolatedRegion[1]
+            )
 
 
-	# if metType == "MET":
-	# 	process.goodMETs = cms.EDFilter("CandViewSelector",
-	# 	  src=cms.InputTag("patMETs"),
-	# 	  cut=cms.string("pt>35")
-	# 	)
-	# 	process.hasMET = cms.EDFilter(
-	# 		"PATCandViewCountFilter",
-	# 		src=cms.InputTag("goodMETs"),
-	# 		minNumber=cms.uint32(1),
-	# 		maxNumber=cms.uint32(1),
-	# 	)
-	# 	process.metEleSequence = cms.Sequence(process.goodMETs*process.hasMET)
-	# elif metType == "MtW":
-	# 	process.eleAndMETMT = cms.EDProducer('CandTransverseMassProducer',
-	# 		collections=cms.untracked.vstring(["patMETs", "goodSignalElectrons"])
-	# 	)
-	# 	process.hasEleMETMT = cms.EDFilter('EventDoubleFilter',
-	# 		src=cms.InputTag("eleAndMETMT"),
-	# 		min=cms.double(40),
-	# 		max=cms.double(9999999)
-	# 	)
-	# 	process.metEleSequence = cms.Sequence(process.eleAndMETMT * process.hasEleMETMT)
-	# else:
-	# 	print "WARNING: MET type not specified in electron channel"
+    looseVetoElectronCut = "%s > 20" % conf.Electrons.pt
+    looseVetoElectronCut += "&& abs(eta) < 2.5"
+    looseVetoElectronCut += "&& electronID('mvaTrigV0') > %f" % 0.1
+    looseVetoElectronCut += '&& userFloat("{0}") < {1}'.format(conf.Electrons.relIsoType, conf.Electrons.looseVetoRelIsoCut)
 
-	process.recoNuProducerEle = cms.EDProducer('ClassicReconstructedNeutrinoProducer',
-		leptonSrc=cms.InputTag("goodSignalLeptons"),
-		bjetSrc=cms.InputTag("btaggedJets"),
+    #Loose veto electrons must not overlap with good signal electrons
+    looseVetoElectronCut += "&& !(%s)" % goodSignalElectronCut
 
-		#either patMETs if cutting on ele + MET transverse mass or goodMETs if cutting on patMETs->goodMets pt
-		metSrc=cms.InputTag("goodMETs" if conf.Electrons.transverseMassType == conf.Leptons.WTransverseMassType.MET else "patMETs"),
-	)
+    process.goodSignalElectrons = cms.EDFilter("CandViewSelector",
+      src=cms.InputTag("elesWithIso"), cut=cms.string(goodSignalElectronCut)
+    )
 
-	if conf.doDebug:
-		process.oneIsoEleIDs = cms.EDAnalyzer('EventIDAnalyzer', name=cms.untracked.string("IDoneIsoEle"))
-		process.eleVetoIDs = cms.EDAnalyzer('EventIDAnalyzer', name=cms.untracked.string("IDeleVeto"))
-		process.metIDS = cms.EDAnalyzer('EventIDAnalyzer', name=cms.untracked.string("MET"))
-		process.NJetIDs = cms.EDAnalyzer('EventIDAnalyzer', name=cms.untracked.string("NJet"))
-		process.electronAnalyzer = cms.EDAnalyzer('SimpleElectronAnalyzer', interestingCollections=cms.untracked.VInputTag("elesWithIso"))
-		process.electronVetoAnalyzer = cms.EDAnalyzer('SimpleElectronAnalyzer', interestingCollections=cms.untracked.VInputTag("looseVetoElectrons"))
-		process.metAnalyzer = cms.EDAnalyzer('SimpleMETAnalyzer', interestingCollections=cms.untracked.VInputTag("patMETs"))
+    process.looseVetoElectrons = cms.EDFilter("CandViewSelector",
+      src=cms.InputTag("elesWithIso"), cut=cms.string(looseVetoElectronCut)
+    )
+
+    process.oneIsoEle = cms.EDFilter(
+        "PATCandViewCountFilter",
+        src=cms.InputTag("goodSignalElectrons"),
+        minNumber=cms.uint32(1),
+        maxNumber=cms.uint32(1),
+    )
+
+    process.singleIsoEle = cms.EDFilter("CandViewSelector", src=cms.InputTag("goodSignalElectrons"), cut=cms.string(""))
+
+    process.electronCount = cms.EDProducer(
+        "CollectionSizeProducer<reco::Candidate>",
+        src = cms.InputTag("goodSignalElectrons")
+    )
+
+    #If loose veto electrons don't have any overlap with signal electrons, there must be none
+    process.looseEleVetoEle = cms.EDFilter(
+        "PATCandViewCountFilter",
+        src=cms.InputTag("looseVetoElectrons"),
+        minNumber=cms.uint32(0),
+        maxNumber=cms.uint32(0),
+    )
+    process.looseMuVetoEle = cms.EDFilter(
+        "PATCandViewCountFilter",
+        src=cms.InputTag("looseVetoMuons"),
+        minNumber=cms.uint32(0),
+        maxNumber=cms.uint32(0),
+    )
+
+    #####################
+    # MET/MtW cutting   #
+    #####################
+    #Either use MET cut or MtW cut
+    if conf.Electrons.transverseMassType == conf.Leptons.WTransverseMassType.MET:
+        process.goodMETsEle = cms.EDFilter("CandViewSelector",
+          src=cms.InputTag("patMETs"), cut=cms.string("pt>%f" % conf.Electrons.transverseMassCut)
+        )
+
+        process.metEleSequence = cms.Sequence(
+            process.goodMETsEle
+        )
+
+        if conf.Leptons.cutOnTransverseMass:
+            process.hasMETEle = cms.EDFilter("PATCandViewCountFilter",
+                src = cms.InputTag("goodMETsEle"),
+                minNumber = cms.uint32(1),
+                maxNumber = cms.uint32(1)
+            )
+            process.metMuSequence.insert(-1, process.hasMETEle)
+
+    elif conf.Electrons.transverseMassType == conf.Leptons.WTransverseMassType.MtW:
+
+        #produce the muon and MET invariant transverse mass
+        process.eleAndMETMT = cms.EDProducer('CandTransverseMassProducer',
+            collections=cms.untracked.vstring(["patMETs", "goodSignalElectrons"])
+        )
+
+        process.metEleSequence = cms.Sequence(
+            process.eleAndMETMT
+        )
+
+        if conf.Leptons.cutOnTransverseMass:
+            process.hasEleMETMT = cms.EDFilter('EventDoubleFilter',
+                src=cms.InputTag("eleAndMETMT"),
+                min=cms.double(conf.Electrons.transverseMassCut),
+                max=cms.double(9999999)
+            )
+            process.metEleSequence += process.hasEleMETMT
+
+
+    # if metType == "MET":
+    #     process.goodMETs = cms.EDFilter("CandViewSelector",
+    #       src=cms.InputTag("patMETs"),
+    #       cut=cms.string("pt>35")
+    #     )
+    #     process.hasMET = cms.EDFilter(
+    #         "PATCandViewCountFilter",
+    #         src=cms.InputTag("goodMETs"),
+    #         minNumber=cms.uint32(1),
+    #         maxNumber=cms.uint32(1),
+    #     )
+    #     process.metEleSequence = cms.Sequence(process.goodMETs*process.hasMET)
+    # elif metType == "MtW":
+    #     process.eleAndMETMT = cms.EDProducer('CandTransverseMassProducer',
+    #         collections=cms.untracked.vstring(["patMETs", "goodSignalElectrons"])
+    #     )
+    #     process.hasEleMETMT = cms.EDFilter('EventDoubleFilter',
+    #         src=cms.InputTag("eleAndMETMT"),
+    #         min=cms.double(40),
+    #         max=cms.double(9999999)
+    #     )
+    #     process.metEleSequence = cms.Sequence(process.eleAndMETMT * process.hasEleMETMT)
+    # else:
+    #     print "WARNING: MET type not specified in electron channel"
+
+    process.recoNuProducerEle = cms.EDProducer('ClassicReconstructedNeutrinoProducer',
+        leptonSrc=cms.InputTag("singleIsoEle"),
+        bjetSrc=cms.InputTag("btaggedJets"),
+
+        #either patMETs if cutting on ele + MET transverse mass or goodMETs if cutting on patMETs->goodMets pt
+        metSrc=cms.InputTag("goodMETs" if conf.Electrons.transverseMassType == conf.Leptons.WTransverseMassType.MET else "patMETs"),
+    )
+
+    if conf.doDebug:
+        process.oneIsoEleIDs = cms.EDAnalyzer('EventIDAnalyzer', name=cms.untracked.string("IDoneIsoEle"))
+        process.eleVetoIDs = cms.EDAnalyzer('EventIDAnalyzer', name=cms.untracked.string("IDeleVeto"))
+        process.metIDS = cms.EDAnalyzer('EventIDAnalyzer', name=cms.untracked.string("MET"))
+        process.NJetIDs = cms.EDAnalyzer('EventIDAnalyzer', name=cms.untracked.string("NJet"))
+        process.electronAnalyzer = cms.EDAnalyzer('SimpleElectronAnalyzer', interestingCollections=cms.untracked.VInputTag("elesWithIso"))
+        process.electronVetoAnalyzer = cms.EDAnalyzer('SimpleElectronAnalyzer', interestingCollections=cms.untracked.VInputTag("looseVetoElectrons"))
+        process.metAnalyzer = cms.EDAnalyzer('SimpleMETAnalyzer', interestingCollections=cms.untracked.VInputTag("patMETs"))
 
 """
 Configures the electron path with full selection.
-channel:	'sig' - runs on signal (t-channel or tbar channel). Generator level comparisons are turned on.
-			'bkg' - runs on background (anything else). Generator level comparisons turned off.
+channel:    'sig' - runs on signal (t-channel or tbar channel). Generator level comparisons are turned on.
+            'bkg' - runs on background (anything else). Generator level comparisons turned off.
 """
 def ElectronPath(process, conf):
-	process.elePathPreCount = cms.EDProducer("EventCountProducer")
+    process.elePathPreCount = cms.EDProducer("EventCountProducer")
 
-	process.efficiencyAnalyzerEle = cms.EDAnalyzer('EfficiencyAnalyzer'
-	, histogrammableCounters = cms.untracked.vstring(["elePath"])
-	, elePath = cms.untracked.vstring([
-		"PATTotalEventsProcessedCount",
-		"singleTopPathStep1ElePreCount",
-		"singleTopPathStep1ElePostCount",
-		"elePathPreCount",
-		"elePathStepHLTsyncElePostCount",
-		"elePathOneIsoElePostCount",
-		"elePathLooseEleVetoElePostCount",
-		"elePathLooseMuVetoElePostCount",
-		"elePathNJetsPostCount",
-		"elePathMetEleSequencePostCount",
-		"elePathMBTagsPostCount"
-		]
-	))
+    process.efficiencyAnalyzerEle = cms.EDAnalyzer('EfficiencyAnalyzer'
+    , histogrammableCounters = cms.untracked.vstring(["elePath"])
+    , elePath = cms.untracked.vstring([
+        "PATTotalEventsProcessedCount",
+        "singleTopPathStep1ElePreCount",
+        "singleTopPathStep1ElePostCount",
+        "elePathPreCount",
+        "elePathStepHLTsyncElePostCount",
+        "elePathOneIsoElePostCount",
+        "elePathLooseEleVetoElePostCount",
+        "elePathLooseMuVetoElePostCount",
+        "elePathNJetsPostCount",
+        "elePathMetEleSequencePostCount",
+        "elePathMBTagsPostCount"
+        ]
+    ))
 
-	process.elePath = cms.Path(
-		process.muonsWithIso *
-		process.elesWithIso *
+    process.elePath = cms.Path(
+        process.muonsWithIso *
+        process.elesWithIso *
 
-		process.elePathPreCount *
+        process.elePathPreCount *
 
-		process.stepHLTsyncEle *
+        process.stepHLTsyncEle *
 
-		process.goodSignalElectrons *
-		process.electronCount *
-		process.looseVetoElectrons *
-		process.oneIsoEle *
+        process.goodSignalElectrons *
+        process.electronCount *
+        process.looseVetoElectrons *
+        process.oneIsoEle *
+        process.singleIsoEle *
 
-		process.looseEleVetoEle *
-		process.looseVetoMuons *
-		process.looseMuVetoEle *
+        process.looseEleVetoEle *
+        process.looseVetoMuons *
+        process.looseMuVetoEle *
 
-		process.jetSequence *
-		process.nJets *
+        process.jetSequence *
+        process.nJets *
 
-		process.metEleSequence *
-		process.goodSignalLeptons *
+        process.metEleSequence *
+        process.goodSignalLeptons *
 
-		process.mBTags *
+        process.mBTags *
 
-		process.topRecoSequenceEle *
-		process.efficiencyAnalyzerEle
-	)
+        process.topRecoSequenceEle *
+        process.efficiencyAnalyzerEle
+    )
 
-	#Insert debugging modules for printout
-	if conf.doDebug:
-		process.elePath.insert(
-			process.elePath.index(process.oneIsoEle)+1,
-			process.oneIsoEleIDs
-		)
-		process.elePath.insert(
-			process.elePath.index(process.oneIsoEle),
-			process.electronAnalyzer
-		)
-		process.elePath.insert(
-			process.elePath.index(process.looseEleVetoEle),
-			process.electronVetoAnalyzer
-		)
-		process.elePath.insert(
-			process.elePath.index(process.looseEleVetoEle)+1,
-			process.eleVetoIDs
-		)
-		process.elePath.insert(
-			process.elePath.index(process.metEleSequence)+1,
-			process.metIDS
-		)
-		process.elePath.insert(
-			process.elePath.index(process.nJets)+1,
-			process.NJetIDs
-		)
-		process.elePath.insert(
-			process.elePath.index(process.metEleSequence),
-			process.metAnalyzer
-		)
+    #Insert debugging modules for printout
+    if conf.doDebug:
+        process.elePath.insert(
+            process.elePath.index(process.oneIsoEle)+1,
+            process.oneIsoEleIDs
+        )
+        process.elePath.insert(
+            process.elePath.index(process.oneIsoEle),
+            process.electronAnalyzer
+        )
+        process.elePath.insert(
+            process.elePath.index(process.looseEleVetoEle),
+            process.electronVetoAnalyzer
+        )
+        process.elePath.insert(
+            process.elePath.index(process.looseEleVetoEle)+1,
+            process.eleVetoIDs
+        )
+        process.elePath.insert(
+            process.elePath.index(process.metEleSequence)+1,
+            process.metIDS
+        )
+        process.elePath.insert(
+            process.elePath.index(process.nJets)+1,
+            process.NJetIDs
+        )
+        process.elePath.insert(
+            process.elePath.index(process.metEleSequence),
+            process.metAnalyzer
+        )
 
 
-	if conf.isMC and conf.channel == conf.Channel.signal:
-		#Put the parton level study after the top reco sequence.
-		process.elePath.insert(
-			process.elePath.index(process.topRecoSequenceEle)+1,
-			process.partonStudyCompareSequence
-		)
+    if conf.isMC and conf.channel == conf.Channel.signal:
+        #Put the parton level study after the top reco sequence.
+        process.elePath.insert(
+            process.elePath.index(process.topRecoSequenceEle)+1,
+            process.partonStudyCompareSequence
+        )
 
-	eventCounting.countAfter(process, process.elePath,
-		[
-		"stepHLTsyncEle",
-		"oneIsoEle",
-		"looseEleVetoEle",
-		"looseMuVetoEle",
-		"metEleSequence",
-		"nJets",
-		"mBTags"
-		]
-	)
+    eventCounting.countAfter(process, process.elePath,
+        [
+        "stepHLTsyncEle",
+        "oneIsoEle",
+        "looseEleVetoEle",
+        "looseMuVetoEle",
+        "metEleSequence",
+        "nJets",
+        "mBTags"
+        ]
+    )
