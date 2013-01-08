0d8bf426a6aaf86912f9ae72ad8035ff142e5f33
jpata <joosep.pata@gmail.com>
2013-01-08 18:56:31 +0200

    removed old files

--- CMSSW_5_3_5/src/SingleTopPolarization/BTagSystematicsWeightProducer/python/btagsystematicsweightproducer_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-demo = cms.EDProducer('BTagSystematicsWeightProducer',
-    SFb=cms.string("x"),
-    SFc=cms.string("x"),
-    SFl=cms.string("x"),
-    SFlErrXBins=cms.vdouble([0,10,20]),
-    SFlErrYBins=cms.vdouble([5,8])
-)
