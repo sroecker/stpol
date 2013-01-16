4453a6d6af30958c95f1c424474099fcdc52298f
Joosep Pata <joosep.pata@gmail.com>
2013-01-16 15:54:40 +0200

    return codes

--- build.sh
+++ build.sh
@@ -23,6 +23,11 @@ fi
 
 echo "Pushing"
 git push ssh://joosep@ied.hep.kbfi.ee/home/joosep/singletop/stpol/ build
+if [ $? -ne 0]; then
+    exit $?
+fi
+
 echo "Building..."
 ssh ied.hep.kbfi.ee "sh /home/joosep/singletop/stpol/build_scram.sh"
 echo "Done building!"
+exit $?
