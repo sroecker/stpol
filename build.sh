b62cc1bc10b3d8fa3a8d94cf394b076371f91b07
Joosep Pata <joosep.pata@gmail.com>
2013-01-16 15:04:39 +0200

    external build system working

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 #  Created by Joosep Pata on 1/16/13.
 #
 echo "Pushing"
-git push ssh://joosep@ied.hep.kbfi.ee/home/joosep/singletop/stpol/
+git push ssh://joosep@ied.hep.kbfi.ee/home/joosep/singletop/stpol/ build
 echo "Building..."
 ssh ied.hep.kbfi.ee "sh /home/joosep/singletop/stpol/build_scram.sh"
 echo "Done building!"
