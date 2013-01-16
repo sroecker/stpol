5b13ceda879e1a3f5d8a6e76321674b6b164dafa
Joosep Pata <joosep.pata@gmail.com>
2013-01-16 15:49:32 +0200

    added sanity checks

--- build.sh
+++ build.sh
@@ -6,6 +6,21 @@
 #  Created by Joosep Pata on 1/16/13.
 #  joosep.pata@cern.ch
 #
+
+#Check branch
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ "$BRANCH" != "build" ]; then
+    echo "ERROR: Not on branch 'build', instead on '$BRANCH'"
+    exit 1
+fi
+
+#Check for uncommitted changes
+git diff --quiet --exit-code
+if [ $? -ne 0 ]; then
+    echo "Uncommitted changes, exiting"
+    exit 1
+fi
+
 echo "Pushing"
 git push ssh://joosep@ied.hep.kbfi.ee/home/joosep/singletop/stpol/ build
 echo "Building..."
