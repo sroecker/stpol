1f909e228e4ac881ed7f7117a5e717b85015ac54
Joosep Pata <joosep.pata@gmail.com>
2013-01-16 14:21:01 +0200

    working on remote build system

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#  build.sh
+#  stpol
+#
+#  Created by Joosep Pata on 1/16/13.
+#
+echo "Pushing"
+git push ssh://joosep@ied.hep.kbfi.ee/home/joosep/singletop/stpol/
+echo "Building..."
+ssh ied.hep.kbfi.ee "sh /home/joosep/singletop/stpol/build_scram.sh"
+echo "Done building!"
