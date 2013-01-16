20529238d743be89a38bd1ad5d45a598b92215eb
jpata <joosep.pata@gmail.com>
2013-01-16 15:31:46 +0200

    added build scripts

---
+++ build_scram.sh
@@ -0,0 +1,28 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+#git status
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$BRANCH" == "master" ]
+then
+    echo "On branch 'master'"
+    git diff --quiet --exit-code
+    if [ $? -ne 0 ]
+    then
+        echo "Uncommitted changes, exiting"
+        exit 1
+    fi
+    git checkout build
+else
+    echo "Not on branch 'master'"
+    git diff --quiet --exit-code
+    if [ $? -ne 0 ]
+    then
+        echo "Uncommitted changes, exiting"
+        exit 1
+    fi
+fi
+source setenv.sh
+cd CMSSW_5_3_7_patch4
+scram b -j 7
+git checkout master
---
+++ setenv.sh
@@ -0,0 +1,4 @@
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cd CMSSW_5_3_7_patch4
+cmsenv
+cd ..
