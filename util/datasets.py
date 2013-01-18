dbde3881fb1c3271b20cd0fd5c60879a2ffbe078
Joosep Pata <joosep.pata@gmail.com>
2013-01-18 17:44:55 +0200

    comments and cleanup.

--- util/datasets.py
+++ util/datasets.py
@@ -1,8 +1,18 @@
+#!/usr/bin/python2.7
+"""
+This file collects the various datasets used for the stpol analysis and creates
+crab.cfg files from these datasets based on a template.
+Author: Joosep Pata joosep.pata@cern.ch
+"""
+
 import sys
 import os
 
 import argparse
 
+"""
+Represents a lumi file.
+"""
 class Lumi:
     lumiBase8TeV = "https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions12/8TeV/"
 
@@ -23,6 +33,9 @@ lumis = {
 }
 
 
+"""
+Represents a generic datasets.
+"""
 class DS:
     def __init__(self, name, ds):
         self.name = name
@@ -36,6 +49,9 @@ class DS:
         out = out.replace("WORKDIR", workdir)
         return out
 
+"""
+Represents a Real Data dataset
+"""
 class DS_Data(DS):
     def __init__(self, name, ds, lumi, globalTag):
         DS.__init__(self, name, ds)
@@ -103,16 +119,27 @@ step1_MC = [
     , DS("QCD_EM6", "/QCD_Pt_350_EMEnriched_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM")
 
     , DS("DYJets", "/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM")
+    
+    #From https://indico.cern.ch/getFile.py/access?contribId=1&resId=0&materialId=slides&confId=228739
+    #/TTJets_SemiLeptMGDecays_8TeV-madgraph
+    #/TTJets_FullLeptMGDecays_8TeV-madgraph
+    #/TbarToLeptons_t-channel_TuneZ2star_8TeV-powheg-tauola
+    #/TToLeptons_t-channel_TuneZ2star_8TeV-powheg-tauola
+    #, DS("TTbarSemiLeptonic", "/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM")
+    #, DS("TTbar", "/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM")
+    
 ]
 
+"""
+Possible dataset sets to process
+"""
 possible_ds = {
     "S1D": step1_data,
     "S1MC": step1_MC
     }
 
-
-
-parser = argparse.ArgumentParser(description='Process some integers.')
+parser = argparse.ArgumentParser(description='Creates crab.cfg files based on \
+                                              a template file.')
 parser.add_argument("-t", "--tag", type=str, default="notag",
                     help="A unique tag for publishing")
 parser.add_argument("-T", "--template", type=str, default="", required=True,
@@ -143,118 +170,4 @@ for ds in dslist:
     of.write(cfg)
     of.close()
     print "{0} done".format(ofn)
-
-# /QCD_Pt_170_250_BCtoE_TuneZ2star_8TeV_pythia6/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER
-# /QCD_Pt_170_250_EMEnriched_TuneZ2star_8TeV_pythia6/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER
-# /QCD_Pt_20_30_EMEnriched_TuneZ2star_8TeV_pythia6/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER
-
-# /QCD_Pt_250_350_BCtoE_TuneZ2star_8TeV_pythia6/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER
-# /QCD_Pt_250_350_EMEnriched_TuneZ2star_8TeV_pythia6/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER
-# /QCD_Pt_30_80_EMEnriched_TuneZ2star_8TeV_pythia6/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER
-# /QCD_Pt_350_BCtoE_TuneZ2star_8TeV_pythia6/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER
-# /QCD_Pt_350_EMEnriched_TuneZ2star_8TeV_pythia6/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER
-# /QCD_Pt_80_170_EMEnriched_TuneZ2star_8TeV_pythia6/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER
-
-# step2_DS = {
-#     "T_t":          "/T_t-channel_TuneZ2star_8TeV-powheg-tauola/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER",
-#     "Tbar_t":       "/Tbar_t-channel_TuneZ2star_8TeV-powheg-tauola/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER",
-
-#     "T_s":          "/T_s-channel_TuneZ2star_8TeV-powheg-tauola/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER",
-#     "Tbar_s":       "/Tbar_s-channel_TuneZ2star_8TeV-powheg-tauola/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER",
-
-#     "T_tW":         "/T_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER",
-#     "Tbar_tW":      "/Tbar_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER",
-
-#     "TTbar":        "/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER",
-
-#     "WJets":       "/WJetsToLNu_TuneZ2Star_8TeV-madgraph-tarball/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER",
-
-#     "WW":           "/WW_TuneZ2star_8TeV_pythia6_tauola/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER",
-#     "WZ":           "/WZ_TuneZ2star_8TeV_pythia6_tauola/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER",
-#     "ZZ":           "/ZZ_TuneZ2star_8TeV_pythia6_tauola/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER",
-
-#     # "GJets1":       "/GJets_HT-200To400_8TeV-madgraph/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM",
-#     # "GJets2":       "/GJets_HT-400ToInf_8TeV-madgraph/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM",
-
-#     "QCD_Mu":       "/QCD_Pt_20_MuEnrichedPt_15_TuneZ2star_8TeV_pythia6/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER",
-
-#     # "QCD_BCtoE1":   "/QCD_Pt_20_30_BCtoE_TuneZ2star_8TeV_pythia6/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER",
-#     # "QCD_BCtoE2":   "/QCD_Pt_30_80_BCtoE_TuneZ2star_8TeV_pythia6/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER",
-#     # "QCD_BCtoE3":   "/QCD_Pt_80_170_BCtoE_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM",
-#     # "QCD_BCtoE4":   "/QCD_Pt_170_250_BCtoE_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM",
-#     # "QCD_BCtoE5":   "/QCD_Pt_170_250_BCtoE_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM",
-#     # "QCD_BCtoE5":   "/QCD_Pt_250_350_BCtoE_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM",
-#     # "QCD_BCtoE6":   "/QCD_Pt_350_BCtoE_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v2/AODSIM",
-
-#     # "QCD_EM1":      "/QCD_Pt_20_30_EMEnriched_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM",
-#     # "QCD_EM2":      "/QCD_Pt_30_80_EMEnriched_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM",
-#     # "QCD_EM3":      "/QCD_Pt_80_170_EMEnriched_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM",
-#     # "QCD_EM4":      "/QCD_Pt_170_250_EMEnriched_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM",
-#     # "QCD_EM5":      "/QCD_Pt_250_350_EMEnriched_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM",
-#     # "QCD_EM6":      "/QCD_Pt_350_EMEnriched_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM",
-
-#     "DYJets":       "/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball/jpata-stpol_v3_1-33f82354a36574c1158b3181d92c6119/USER",
-# }
-
-
-
-# tag = args.tag
-# template = read_template(args.template)
-# doData = args.step1Data
-
-
-# def parse_data_template(name, tag, dataset, lumi, ofdir):
-#     ofile = ofdir + "/" + "crab_%s_%s.cfg" % (name, tag)
-#     workdir = "WD_%s_%s" % (name, tag)
-#     out = template
-#     out = out.replace("DATASET", dataset)
-#     out = out.replace("WORKDIR", workdir)
-#     out = out.replace("TAG", tag)
-#     lumifile = lumis[lumi]
-#     lumifile = lumifile[lumifile.rindex("/")+1:]
-#     out = out.replace("LUMIFILE", lumifile)
-#     f = open(ofile, "w")
-#     f.write(out)
-#     f.close()
-#     print "Wrote %s" % ofile
-
-# if doData:
-#     #Prepare output directory
-#     ofdir = "crabs_%s" % tag
-#     ofdir_data = ofdir + "/data"
-#     ofdir_mc = ofdir + "/mc"
-#     os.mkdir(ofdir)
-#     os.mkdir(ofdir_data)
-#     os.mkdir(ofdir_mc)
-#     for (name, dataset) in step1_data.items():
-#         parse_data_template(name, tag, dataset.ds, dataset.lumi, ofdir_data)
-
-#fname = sys.argv[1]
-#print "Opening template %s" % fname
-#f = open(fname)
-#if len(sys.argv)>2:
-#    tag = sys.argv[2]
-#else:
-#    tag = "NOTAG"
-#
-#template = f.read()
-#f.close()
-#
-#ofdir = "crabs_%s" % tag
-#os.mkdir(ofdir)
-#
-#ds = step2_DS
-#
-#for (k, v) in ds.items():
-#    print k
-#    temp = template
-#    if "DATASET" in temp:
-#        temp = temp.replace("DATASET", ds[k])
-#    if "WORKDIR" in template:
-#        temp = temp.replace("WORKDIR", "WD_%s" % k)
-#    if "TAG" in temp:
-#        temp = temp.replace("TAG", tag)
-#    f = open("%s/crab_%s.cfg" % (ofdir, k), "w")
-#    f.write(temp)
-#    f.close()
 
