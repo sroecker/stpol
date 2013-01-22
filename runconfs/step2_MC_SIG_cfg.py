fef32fbad2ab99bc38e66267eb9c660a41b02e82
jpata <joosep.pata@gmail.com>
2013-01-22 21:15:58 +0200

    disabled pattuple output

--- runconfs/step2_MC_SIG_cfg.py
+++ runconfs/step2_MC_SIG_cfg.py
@@ -2,5 +2,5 @@ import FWCore.ParameterSet.Config as cms
 from SingleTopPolarization.Analysis.selection_step2_cfg import SingleTopStep2, Config
 
 Config.channel = Config.Channel.signal
-Config.skipPatTupleOutput = False
+#Config.skipPatTupleOutput = False
 process = SingleTopStep2()
