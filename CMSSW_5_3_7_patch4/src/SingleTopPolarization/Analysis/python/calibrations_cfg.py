a423f226c7596acdce22367dc0cabc805571a166
Joosep Pata <joosep.pata@gmail.com>
2013-01-23 10:16:02 +0200

    added python file for run-time calibrations, only b-tagging effs at the moment

---
+++ CMSSW_5_3_7_patch4/src/SingleTopPolarization/Analysis/python/calibrations_cfg.py
@@ -0,0 +1,17 @@
+bTaggingEfficiencies = dict()
+
+class BTaggingEfficiency:
+    def __init__(self, eff_b, eff_c, eff_l):
+        self.eff_b = eff_b
+        self.eff_c = eff_c
+        self.eff_l = eff_l
+
+eff_ex_WJets = {"eff_b": 0.9545454545454546, "eff_c": 1.0, "eff_l": 0.12307692307692308}
+eff_ex_T_t = {"eff_b": 0.9869394862864606, "eff_c": 0.0, "eff_l": 0.00019790223629527012}
+eff_ex_TTbar = {"eff_b": 0.7639405204460966, "eff_c": 0.09345794392523364, "eff_l": 0.0027472527472527475}
+
+
+bTaggingEfficiencies["T_t"] = BTaggingEfficiency(eff_ex_T_t["eff_b"], eff_ex_WJets["eff_c"], eff_ex_T_t["eff_l"])
+bTaggingEfficiencies["Tbar_t"] = BTaggingEfficiency(eff_ex_T_t["eff_b"], eff_ex_WJets["eff_c"], eff_ex_T_t["eff_l"])
+bTaggingEfficiencies["WJets"] = BTaggingEfficiency(eff_ex_T_t["eff_b"], eff_ex_WJets["eff_c"], eff_ex_WJets["eff_l"])
+bTaggingEfficiencies["TTbar"] = BTaggingEfficiency(eff_ex_T_t["eff_b"], eff_ex_WJets["eff_c"], eff_ex_TTbar["eff_l"])
