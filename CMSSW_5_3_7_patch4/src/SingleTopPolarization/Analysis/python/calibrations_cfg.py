426d52928e8f7134cf3736fad11b343660e280b2
Joosep Pata <joosep.pata@gmail.com>
2013-01-23 10:42:54 +0200

    added efficiencies for more samples

--- CMSSW_5_3_7_patch4/src/SingleTopPolarization/Analysis/python/calibrations_cfg.py
+++ CMSSW_5_3_7_patch4/src/SingleTopPolarization/Analysis/python/calibrations_cfg.py
@@ -15,5 +15,14 @@ eff_ex_TTbar = {"eff_b": 0.7639405204460966, "eff_c": 0.09345794392523364, "eff_
 
 bTaggingEfficiencies["T_t"] = BTaggingEfficiency(eff_ex_T_t["eff_b"], eff_ex_WJets["eff_c"], eff_ex_T_t["eff_l"])
 bTaggingEfficiencies["Tbar_t"] = BTaggingEfficiency(eff_ex_T_t["eff_b"], eff_ex_WJets["eff_c"], eff_ex_T_t["eff_l"])
+
 bTaggingEfficiencies["WJets"] = BTaggingEfficiency(eff_ex_T_t["eff_b"], eff_ex_WJets["eff_c"], eff_ex_WJets["eff_l"])
+bTaggingEfficiencies["WW"] = BTaggingEfficiency(eff_ex_T_t["eff_b"], eff_ex_WJets["eff_c"], eff_ex_WJets["eff_l"])
+bTaggingEfficiencies["WZ"] = BTaggingEfficiency(eff_ex_T_t["eff_b"], eff_ex_WJets["eff_c"], eff_ex_WJets["eff_l"])
+bTaggingEfficiencies["ZZ"] = BTaggingEfficiency(eff_ex_T_t["eff_b"], eff_ex_WJets["eff_c"], eff_ex_WJets["eff_l"])
+
 bTaggingEfficiencies["TTbar"] = BTaggingEfficiency(eff_ex_T_t["eff_b"], eff_ex_WJets["eff_c"], eff_ex_TTbar["eff_l"])
+bTaggingEfficiencies["T_tW"] = BTaggingEfficiency(eff_ex_T_t["eff_b"], eff_ex_WJets["eff_c"], eff_ex_TTbar["eff_l"])
+bTaggingEfficiencies["T_s"] = BTaggingEfficiency(eff_ex_T_t["eff_b"], eff_ex_WJets["eff_c"], eff_ex_TTbar["eff_l"])
+bTaggingEfficiencies["Tbar_tW"] = BTaggingEfficiency(eff_ex_T_t["eff_b"], eff_ex_WJets["eff_c"], eff_ex_TTbar["eff_l"])
+bTaggingEfficiencies["Tbar_s"] = BTaggingEfficiency(eff_ex_T_t["eff_b"], eff_ex_WJets["eff_c"], eff_ex_TTbar["eff_l"])
