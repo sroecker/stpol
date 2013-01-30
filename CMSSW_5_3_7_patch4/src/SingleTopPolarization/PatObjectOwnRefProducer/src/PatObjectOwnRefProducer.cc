566dc75e4b30e2edf8918c6c74df7752059d56dc
Joosep Pata <joosep.pata@gmail.com>
2013-01-30 13:08:16 +0200

    output collection

--- CMSSW_5_3_7_patch4/src/SingleTopPolarization/PatObjectOwnRefProducer/src/PatObjectOwnRefProducer.cc
+++ CMSSW_5_3_7_patch4/src/SingleTopPolarization/PatObjectOwnRefProducer/src/PatObjectOwnRefProducer.cc
@@ -2,14 +2,14 @@
 //
 // Package:    PatObjectOwnRefProducer<T>
 // Class:      PatObjectOwnRefProducer<T>
-// 
+//
 /**\class PatObjectOwnRefProducer<T> PatObjectOwnRefProducer.cc SingleTopPolarization/PatObjectOwnRefProducer/src/PatObjectOwnRefProducer.cc
-
+ 
  Description: [one line class summary]
-
+ 
  Implementation:
-     [Notes on implementation]
-*/
+ [Notes on implementation]
+ */
 //
 // Original Author:  Joosep Pata
 //         Created:  Wed Jan 30 09:28:16 EET 2013
@@ -29,7 +29,8 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include <DataFormats/PatCandidates/interface/Muon.h>
+#include <DataFormats/PatCandidates/interface/Jet.h>
+#include "FWCore/Utilities/interface/InputTag.h"
 
 
 //
@@ -37,23 +38,25 @@
 //
 template <typename T>
 class PatObjectOwnRefProducer : public edm::EDProducer {
-   public:
-      explicit PatObjectOwnRefProducer(const edm::ParameterSet&);
-      ~PatObjectOwnRefProducer();
-
-      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
-
-   private:
-      virtual void beginJob() ;
-      virtual void produce(edm::Event&, const edm::EventSetup&);
-      virtual void endJob() ;
-      
-      virtual void beginRun(edm::Run&, edm::EventSetup const&);
-      virtual void endRun(edm::Run&, edm::EventSetup const&);
-      virtual void beginLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&);
-      virtual void endLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&);
-
-      // ----------member data ---------------------------
+public:
+    explicit PatObjectOwnRefProducer(const edm::ParameterSet&);
+    ~PatObjectOwnRefProducer();
+    
+    static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+    
+private:
+    virtual void beginJob() ;
+    virtual void produce(edm::Event&, const edm::EventSetup&);
+    virtual void endJob() ;
+    
+    virtual void beginRun(edm::Run&, edm::EventSetup const&);
+    virtual void endRun(edm::Run&, edm::EventSetup const&);
+    virtual void beginLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&);
+    virtual void endLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&);
+    
+    const edm::InputTag src;
+    
+    // ----------member data ---------------------------
 };
 
 //
@@ -70,59 +73,49 @@ class PatObjectOwnRefProducer : public edm::EDProducer {
 //
 template <typename T>
 PatObjectOwnRefProducer<T>::PatObjectOwnRefProducer(const edm::ParameterSet& iConfig)
+: src(iConfig.getParameter<edm::InputTag>("src"))
 {
-   //register your products
-/* Examples
-   produces<ExampleData2>();
-
-   //if do put with a label
-   produces<ExampleData2>("label");
- 
-   //if you want to put into the Run
-   produces<ExampleData2,InRun>();
-*/
-   //now do what ever other initialization is needed
-  
+    produces<std::vector<T>>();
 }
 
 
 template <typename T>
 PatObjectOwnRefProducer<T>::~PatObjectOwnRefProducer()
 {
- 
-   // do anything here that needs to be done at desctruction time
-   // (e.g. close files, deallocate resources etc.)
-
 }
 
-
-//
-// member functions
-//
-
-// ------------ method called to produce the data  ------------
 template <typename T>
 void
 PatObjectOwnRefProducer<T>::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace edm;
-/* This is an event example
-   //Read 'ExampleData' from the Event
-   Handle<ExampleData> pIn;
-   iEvent.getByLabel("example",pIn);
-
-   //Use the ExampleData to create an ExampleData2 which 
-   // is put into the Event
-   std::auto_ptr<ExampleData2> pOut(new ExampleData2(*pIn));
-   iEvent.put(pOut);
-*/
-
-/* this is an EventSetup example
-   //Read SetupData from the SetupRecord in the EventSetup
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
-*/
- 
+    using namespace edm;
+    
+    Handle<View<T>> inColl;
+    iEvent.getByLabel(src, inColl);
+    
+    /*if(! inColl.isValid()) {
+        throw new 
+    }*/
+    
+    std::auto_ptr<std::vector<T>> outColl(new std::vector<T>(*pIn));
+    
+    for( auto & elem : *outColl) {
+        elem.addUserFloat("asd", 0.0);
+    }
+    
+    /*
+     //Use the ExampleData to create an ExampleData2 which
+     // is put into the Event
+     std::auto_ptr<ExampleData2> pOut(new ExampleData2(*pIn));
+     iEvent.put(pOut);
+     */
+    
+    /* this is an EventSetup example
+     //Read SetupData from the SetupRecord in the EventSetup
+     ESHandle<SetupData> pSetup;
+     iSetup.get<SetupRecord>().get(pSetup);
+     */
+    
 }
 
 // ------------ method called once each job just before starting event loop  ------------
@@ -134,34 +127,34 @@ PatObjectOwnRefProducer<T>::beginJob()
 
 // ------------ method called once each job just after ending the event loop  ------------
 template <typename T>
-void 
+void
 PatObjectOwnRefProducer<T>::endJob() {
 }
 
 // ------------ method called when starting to processes a run  ------------
 template <typename T>
-void 
+void
 PatObjectOwnRefProducer<T>::beginRun(edm::Run&, edm::EventSetup const&)
 {
 }
 
 // ------------ method called when ending the processing of a run  ------------
 template <typename T>
-void 
+void
 PatObjectOwnRefProducer<T>::endRun(edm::Run&, edm::EventSetup const&)
 {
 }
 
 // ------------ method called when starting to processes a luminosity block  ------------
 template <typename T>
-void 
+void
 PatObjectOwnRefProducer<T>::beginLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&)
 {
 }
 
 // ------------ method called when ending the processing of a luminosity block  ------------
 template <typename T>
-void 
+void
 PatObjectOwnRefProducer<T>::endLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&)
 {
 }
@@ -170,12 +163,12 @@ PatObjectOwnRefProducer<T>::endLuminosityBlock(edm::LuminosityBlock&, edm::Event
 template <typename T>
 void
 PatObjectOwnRefProducer<T>::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-  //The following says we do not know what parameters are allowed so do no validation
-  // Please change this to state exactly what you do use, even if it is no parameters
-  edm::ParameterSetDescription desc;
-  desc.setUnknown();
-  descriptions.addDefault(desc);
+    //The following says we do not know what parameters are allowed so do no validation
+    // Please change this to state exactly what you do use, even if it is no parameters
+    edm::ParameterSetDescription desc;
+    desc.setUnknown();
+    descriptions.addDefault(desc);
 }
 
 //define this as a plug-in
-DEFINE_FWK_MODULE(PatObjectOwnRefProducer<pat::Muon>);
+DEFINE_FWK_MODULE(PatObjectOwnRefProducer<pat::Jet>);
