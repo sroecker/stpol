dce306e8387439a5fbf4039b58e9600172104534
Joosep Pata <joosep.pata@gmail.com>
2013-02-11 22:52:19 +0200

    working on plotting all samples

--- analysis_step2/plots.py
+++ analysis_step2/plots.py
@@ -11,14 +11,14 @@ enabledSamples = {
     "T_s": "WD_T_s.root",
     "Tbar_s": "WD_Tbar_s.root",
     
-    "DYJets": "WD_DYJets.root",
+#    "DYJets": "WD_DYJets.root",
     "WW": "WD_WW.root",
     "WZ": "WD_WZ.root",
-    "ZZ": "WD_ZZ.root",
+    #"ZZ": "WD_ZZ.root",
+    
     "WJets": "WD_WJets1.root",
     "TTbar": "WD_TTBar.root",
-
-       
+    
     "QCDMu": "WD_QCDMu.root",
     "QCD_Pt_20_30_BCtoE": "WD_QCD_Pt_20_30_BCtoE.root",
     "QCD_Pt_30_80_BCtoE": "WD_QCD_Pt_30_80_BCtoE.root",
@@ -39,6 +39,17 @@ samples = loadSamples(enabledSamples)
 lumiMu = samples["SingleMu"].lumi
 lumiEle = samples["SingleEle"].lumi
 
-h1 = samples["T_t"].plot1D("_goodSignalElectrons_0_mvaID", [20, 0, 1], integratedDataLumi=lumiEle)
-h2 = samples["Tbar_t"].plot1D("_goodSignalElectrons_0_mvaID", [20, 0, 1], integratedDataLumi=lumiEle)
-h2 = samples["QCDMu"].plot1D("_goodSignalElectrons_0_mvaID", [20, 0, 1], integratedDataLumi=lumiEle)
+plots_cosTheta_final = dict()
+pdb.set_trace()
+for (sampleName, sample) in samples.items():
+    plots_cosTheta_final[sampleName] = sample.plot1D("cosThetaLightJet_cosTheta", [20, -1, 1], cut=Cuts.finalMu, integratedDataLumi=lumiMu)
+
+merge = [
+    ["tW", ".+_tW$"],
+	["s", ".+_s$"],
+	["t#bar{t}", "TTBar$"],
+	["WJets", "WJets$"],
+	["QCD", "QCD.+"],
+	["diboson", "WW|WZ|ZZ"],
+	["t", ".+_t$"],
+]
