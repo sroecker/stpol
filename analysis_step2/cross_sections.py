3430038ced499d6ee0d1b71f27f640244c8262ec
Liis Rebane <polaarrebane@gmail.com>
2013-02-12 01:56:19 +0200

    add gen. filter efficiencies for QCD datasets

--- analysis_step2/cross_sections.py
+++ analysis_step2/cross_sections.py
@@ -20,19 +20,19 @@ xs = {
     , "QCDMu": 134680
 
     #http://cms.cern.ch/iCMS/prep/requestmanagement?dsn=QCD_Pt_*_*_EMEnriched_TuneZ2star_8TeV_pythia6*
-    , "QCD_Pt_20_30_EMEnriched": 2.886E8
-    , "QCD_Pt_30_80_EMEnriched": 7.433E7
-    , "QCD_Pt_80_170_EMEnriched": 1191000.0
-    , "QCD_Pt_170_250_EMEnriched": 30990.0
-    , "QCD_Pt_250_350_EMEnriched": 4250.0
-    , "QCD_Pt_350_EMEnriched": 810.0
+    , "QCD_Pt_20_30_EMEnriched": 2.886E8*0.0101
+    , "QCD_Pt_30_80_EMEnriched": 7.433E7*0.0621
+    , "QCD_Pt_80_170_EMEnriched": 1191000.0*0.1539
+    , "QCD_Pt_170_250_EMEnriched": 30990.0*0.148
+    , "QCD_Pt_250_350_EMEnriched": 4250.0*0.131
+    , "QCD_Pt_350_EMEnriched": 810.0*0.11
 
     #http://cms.cern.ch/iCMS/prep/requestmanagement?dsn=QCD_Pt_*_*_BCtoE_TuneZ2star_8TeV_pythia6*
-    , "QCD_Pt_20_30_BCtoE": 2.886E8
-    , "QCD_Pt_30_80_BCtoE": 7.424E7
-    , "QCD_Pt_80_170_BCtoE": 1191000.0
-    , "QCD_Pt_170_250_BCtoE": 30980.0
-    , "QCD_Pt_250_350_BCtoE": 4250.0
-    , "QCD_Pt_350_BCtoE": 811.0
+    , "QCD_Pt_20_30_BCtoE": 2.886E8*0.00058
+    , "QCD_Pt_30_80_BCtoE": 7.424E7*0.00225
+    , "QCD_Pt_80_170_BCtoE": 1191000.0*0.0109
+    , "QCD_Pt_170_250_BCtoE": 30980.0*0.0204
+    , "QCD_Pt_250_350_BCtoE": 4250.0*0.0243
+    , "QCD_Pt_350_BCtoE": 811.0*0.0295
 }
 
