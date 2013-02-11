9134197c57a763413a54ff0f36acd6ebb691924f
Joosep Pata <joosep.pata@gmail.com>
2013-02-11 13:25:23 +0200

    added file to make general AN plots (WIP)

---
+++ analysis_step2/plots.py
@@ -0,0 +1,7 @@
+from anfw import *
+
+enabledSamples = {
+    "SingleEle": "SingleEle_merged_4729_pb.root",
+    "SingleEle": "SingleMu_merged_8190_pb.root",
+}
+samples = loadSamples(enabledSamples)
