ff64313cdac773473c56280036b107e998568e97
jpata <joosep.pata@gmail.com>
2013-02-14 16:10:29 +0200

    added MET/MT, changed TCHP region

--- analysis_step2/fastSimValidationPlots.py
+++ analysis_step2/fastSimValidationPlots.py
@@ -95,6 +95,7 @@ cutNVtxA = Cut("nVtx_0_15", "nVertices_puWeightProducer>=0 && nVertices_puWeight
 cutNVtxB = Cut("nVtx_15_30", "nVertices_puWeightProducer>=15 && nVertices_puWeightProducer< 30")
 cutNVtxC = Cut("nVtx_30_50", "nVertices_puWeightProducer>=30 && nVertices_puWeightProducer< 50")
 baseCut = Cuts.initial# + cutNVtxB
+lepton="muon"
 leptonCut = Cuts.mu
 #defaultWeight = "1"
 defaultWeight = "PUWeight_puWeightProducer"
@@ -104,16 +105,16 @@ nVertRange = [20, 0, 50]
 prettyName = "N_{vtx.gen.}"
 title = " after skim (muon+electron)"
 
-#plotComp("nVerticesTrue_puWeightProducer", nVertRange, baseCut, varName=prettyName, title=title, filename="fastSimValid_nVerts_unweighted.png")
-#plotComp("nVerticesTrue_puWeightProducer", nVertRange, baseCut, varName=prettyName, title=title, filename="fastSimValid_nVerts_weighted.png", weight=defaultWeight)
-#plotComp("_offlinePVCount", nVertRange, Cuts.initial, varName=prettyName, title=title, filename="fastSimValid_nVertsOffline_unweighted.png")
-#plotComp("_offlinePVCount", nVertRange, Cuts.initial, varName=prettyName, title=title, filename="fastSimValid_nVertsOffline_weighted.png", weight=defaultWeight)
+plotComp("nVerticesTrue_puWeightProducer", nVertRange, baseCut, varName=prettyName, title=title, filename="fastSimValid_nVerts_unweighted.png")
+plotComp("nVerticesTrue_puWeightProducer", nVertRange, baseCut, varName=prettyName, title=title, filename="fastSimValid_nVerts_weighted.png", weight=defaultWeight)
+plotComp("_offlinePVCount", nVertRange, Cuts.initial, varName=prettyName, title=title, filename="fastSimValid_nVertsOffline_unweighted.png")
+plotComp("_offlinePVCount", nVertRange, Cuts.initial, varName=prettyName, title=title, filename="fastSimValid_nVertsOffline_weighted.png", weight=defaultWeight)
 
 #Leptons
-lepton="muon"
 filepref = "fastSimValid_%s_"%lepton
-#plotComp("_{0}sWithIso_0_Pt".format(lepton), [20, 0, 150], baseCut, varName="%s p_{T}"%lepton, title=" after skimming, before lepton ID", filename=filepref + "pt_weighted.png", weight=defaultWeight, normalization="unity")
-#plotComp("abs(_{0}sWithIso_0_Eta)".format(lepton), [20, 0, 2.6], baseCut, varName="%s |#eta|"%lepton, title=" after skimming, before lepton ID", filename=filepref + "eta_weighted.png", weight=defaultWeight, normalization="unity")
+lepShort = {"muon":"muon", "electron":"ele"}
+plotComp("_{0}sWithIso_0_Pt".format(lepShort[lepton]), [20, 0, 150], baseCut, varName="%s p_{T}"%lepton, title=" after skimming, before lepton ID", filename=filepref + "pt_weighted.png", weight=defaultWeight, normalization="unity")
+plotComp("abs(_{0}sWithIso_0_Eta)".format(lepShort[lepton]), [20, 0, 2.6], baseCut, varName="%s |#eta|"%lepton, title=" after skimming, before lepton ID", filename=filepref + "eta_weighted.png", weight=defaultWeight, normalization="unity")
 
 nbins=20
 lepIDVars = {
@@ -122,17 +123,21 @@ lepIDVars = {
         , ('_muonsWithIso_0_track_hitPattern_trackerLayersWithMeasurement', (20, 0, 20))
         , ('_muonsWithIso_0_globalTrack_hitPattern_numberOfValidMuonHits', (20, 0, 20))
         , ('_muonsWithIso_0_innerTrack_hitPattern_numberOfValidPixelHits', (10, 0, 10))
-        , ('_muonsWithIso_0_db', (nbins, 0, 0.1))
-        , ('_muonsWithIso_0_dz', (nbins, 0, 0.1))
+        , ('_muonsWithIso_0_db', (nbins, 0, 0.05))
+        , ('_muonsWithIso_0_dz', (nbins, 0, 0.05))
         , ('_muonsWithIso_0_numberOfMatchedStations', (10, 0, 10))
+    ],
+    "electron":[
+        ('_elesWithIso_0_mvaID', (20, 0, 1))
+        , ('_elesWithIso_0_gsfTrack_trackerExpectedHitsInner_numberOfHits', (10, 0, 10))
     ]
 }
 lepPtEtaCuts = {
     "muon": Cut("muPtEta", "_muonsWithIso_0_Pt>26 && abs(_muonsWithIso_0_Eta)<2.1")
-    , "electron": Cut("elePtEta", "_elesithIso_0_Pt>30 && abs(_elesWithIso_0_Eta)<2.5")
+    , "electron": Cut("elePtEta", "_elesWithIso_0_Pt>30 && abs(_elesWithIso_0_Eta)<2.5")
 }
-#for idvar, r in lepIDVars[lepton]:
-#    plotComp(idvar, r, baseCut + lepPtEtaCuts[lepton], varName=varNamePretty(idvar), title=" after skimming, lepton p_{T}, #eta cut", filename=filepref + idvar + "_weighted.png", weight=defaultWeight, normalization="unity")
+for idvar, r in lepIDVars[lepton]:
+    plotComp(idvar, r, baseCut + lepPtEtaCuts[lepton], varName=varNamePretty(idvar), title=" after skimming, lepton p_{T}, #eta cut", filename=filepref + idvar + "_weighted.png", weight=defaultWeight, normalization="unity")
 
 #Reliso after ID
 title = "in %s channel, lepton ID requirement, iso" % lepton
@@ -141,12 +146,22 @@ lepIsoVars = {
     "muon": "_muonsWithIso_0_relIso"
     , "electron": "_elesWithIso_0_relIso"
 }
-#plotComp(lepIsoVars[lepton], [20, 0, 0.15], baseCut+leptonCut, varName="N_{jets}", title=title, filename=filepref+".png", weight=defaultWeight, dtype="float", normalization="unity")
+plotComp(lepIsoVars[lepton], [20, 0, 0.15], baseCut+leptonCut, varName="N_{jets}", title=title, filename=filepref+".png", weight=defaultWeight, dtype="float", normalization="unity")
 
 #Jet count in mu/ele
 title=" in %s channel, after skim, ref. sel. lepton selection" % lepton
 filepref="fastSimValid_nJets_%s" % lepton
-#plotComp("_bJetCount + _lightJetCount", [10, 0, 10], baseCut+leptonCut + Cut("jetOK", "_bJetCount>=0 && _lightJetCount>=0"), varName="N_{jets}", title=title, filename=filepref+".png", weight=defaultWeight, dtype="float", normalization="unity")
+plotComp("_bJetCount + _lightJetCount", [10, 0, 10], baseCut+leptonCut + Cut("jetOK", "_bJetCount>=0 && _lightJetCount>=0"), varName="N_{jets}", title=title, filename=filepref+".png", weight=defaultWeight, dtype="float", normalization="unity")
+
+title=" in %s channel, after skim, ref. sel, lepton selection" % lepton
+metVars = [
+    #("_eleAndMETMT", "eleMT"),
+    ("_muAndMETMT", "muMT"),
+    ("_patMETs_0_Pt", "MET"),
+]
+
+for var, varName in metVars:
+    plotComp(var, [20, 0, 200], baseCut+leptonCut, varName=varName, title=title, filename="fastSimValid_%s.png"%var, weight=defaultWeight, dtype="float", normalization="unity")
 
 #Jet pt/eta in mu/ele
 title=" in %s channel, after skim, ref. sel. lepton selection" % lepton
@@ -156,25 +171,32 @@ jetVars = [
     , ("abs(_goodJets_0_Eta)", "leading jet |#eta|", "jet0eta", [20, 0, 5])
     , ("abs(_goodJets_1_Eta)", "second jet |#eta|", "jet1eta", [20, 0, 5])
 ]
-#for var, varNamePr, varNameF, r in jetVars:
-#    plotComp(var, r, baseCut+leptonCut, varName=varNamePr, title=title, filename="fastSimValid_%s_%s.png"%(varNameF, lepton), weight=defaultWeight, normalization="unity")
+for var, varNamePr, varNameF, r in jetVars:
+    plotComp(var, r, baseCut+leptonCut, varName=varNamePr, title=title, filename="fastSimValid_%s_%s.png"%(varNameF, lepton), weight=defaultWeight, normalization="unity")
 
 
 #Jet bDisriminator TCHP in mu & >=2J
 jetVars = [
-      ("_goodJets_0_bDiscriminatorTCHP", "leading jet TCHP discr.", "jet0tchp")
-    , ("_goodJets_1_bDiscriminatorTCHP", "second jet TCHP discr.", "jet1tchp")
-    , ("_goodJets_0_bDiscriminatorCSV_MVA", "leading jet CSV-MVA discr.", "jet0csvmva")
-    , ("_goodJets_1_bDiscriminatorCSV_MVA", "second jet CSV-MVA discr.", "jet1csvmva")
+      ("_goodJets_0_bDiscriminatorTCHP", "leading jet TCHP discr.", "jet0tchp", [20, -5, 10])
+    , ("_goodJets_1_bDiscriminatorTCHP", "second jet TCHP discr.", "jet1tchp", [20, -5, 10])
+    , ("_goodJets_0_bDiscriminatorCSV_MVA", "leading jet CSV-MVA discr.", "jet0csvmva", [20, 0, 1])
+    , ("_goodJets_1_bDiscriminatorCSV_MVA", "second jet CSV-MVA discr.", "jet1csvmva", [20, 0, 1])
 ]
 title=" in %s channel, after skim, ref. sel. lepton selection, >=2J" % lepton
-#for var, varNamePretty, varNameF in jetVars:
-#    plotComp(var, [20, 0, 1], baseCut+leptonCut+Cuts.jets_2plusJ, varName=varNamePretty, title=title, filename="fastSimValid_%s.png"%varNameF, weight=defaultWeight, normalization="unity")
+for var, varName, varNameF, r in jetVars:
+    plotComp(var, r, baseCut+leptonCut+Cuts.jets_2plusJ, varName=varName, title=title, filename="fastSimValid_%s.png"%varNameF, weight=defaultWeight, normalization="unity")
 
 #|eta_lj| in 2J1T
 
 title=" in %s channel, after skim, ref. sel. lepton selection, 2J1T" % lepton
-#plotComp("abs(_lowestBTagJet_0_Eta)", [20, 0, 5], baseCut+leptonCut+Cuts.jets_2J1T, varName="|%s|"%varNamePretty("_lowestBTagJet_0_Eta"), title=title, filename="fastSimValid_etalj_2J1T.png", weight=defaultWeight, normalization="unity")
+plotComp("abs(_lowestBTagJet_0_Eta)",
+    [20, 0, 5],
+    baseCut+leptonCut+Cuts.jets_2J1T,
+    varName="|%s|"%varNamePretty("_lowestBTagJet_0_Eta"),
+    title=title,
+    filename="fastSimValid_etalj_2J1T.png",
+    weight=defaultWeight,
+    normalization="unity")
 
 #Mtop, cosTheta* in  2j1t, 2j0t, 3j1t, 3j2t
 cuts = {
@@ -183,30 +205,31 @@ cuts = {
     "3J1T": Cuts.jets_3J1T,
     "3J1T": Cuts.jets_3J2T,
 }
-#for (cutn, cut) in cuts.items():
-#    title = =" in muon channel, skim, ref. sel., {0}".format(cutn)
-#    plotComp("_recoTop_0_Mass", [20, 100, 400], baseCut+leptonCut+cut, varName="m_{bl#nu}", title=title, filename="fastSimValid_mlnu_{0}.png".format(cutn), weight=defaultWeight, normalization="unity")
-#    plotComp("_recoTop_0_Pt", [20, 0, 400], baseCut+leptonCut+cut, varName="p_{T, top}", title=title, filename="fastSimValid_topPt_{0}.png".format(cutn), weight=defaultWeight, normalization="unity")
-#    plotComp("abs(_recoTop_0_Eta)", [20, 0, 5], baseCut+leptonCut+cut, varName="|#eta|_{T, top}", title=title, filename="fastSimValid_topEta_{0}.png".format(cutn), weight=defaultWeight, normalization="unity")
-#    plotComp("cosThetaLightJet_cosTheta", [20, -1, 1], baseCut+leptonCut+cut, varName="cos #theta *", title=title, filename="fastSimValid_cosTheta_{0}.png".format(cutn), weight=defaultWeight, normalization="unity")
+for (cutn, cut) in cuts.items():
+    title = " in {1} channel, skim, ref. sel., {0}".format(cutn, lepton)
+    plotComp("_recoTop_0_Mass", [20, 100, 400], baseCut+leptonCut+cut, varName="m_{bl#nu}", title=title, filename="fastSimValid_mlnu_{0}.png".format(cutn), weight=defaultWeight, normalization="unity")
+    plotComp("_recoTop_0_Pt", [20, 0, 400], baseCut+leptonCut+cut, varName="p_{T, top}", title=title, filename="fastSimValid_topPt_{0}.png".format(cutn), weight=defaultWeight, normalization="unity")
+    plotComp("abs(_recoTop_0_Eta)", [20, 0, 5], baseCut+leptonCut+cut, varName="|#eta|_{T, top}", title=title, filename="fastSimValid_topEta_{0}.png".format(cutn), weight=defaultWeight, normalization="unity")
+    plotComp("cosThetaLightJet_cosTheta", [20, -1, 1], baseCut+leptonCut+cut, varName="cos #theta *", title=title, filename="fastSimValid_cosTheta_{0}.png".format(cutn), weight=defaultWeight, normalization="unity")
 
 vars3J1T = [
     ("_goodJets_0_Pt", "leading jet pt", [20, 0, 300], "jet0pt")
     , ("abs(_goodJets_0_Eta)", "leading jet eta", [20, 0, 5], "jet0eta")
-    , ("cosThetaLightJet_cosTheta", "leading jet eta", [20, -1, 1], "cos #theta *")
+    , ("cosThetaLightJet_cosTheta", "cos #theta *", [20, -1, 1], "costheta")
     , ("_goodSignalMuons_0_Pt", "muon pt", [20, 0, 300], "muPt")
     , ("_goodSignalElectrons_0_Pt", "electrons pt", [20, 0, 300], "elePt")
 ]
 
 eta_low = 1.0
 eta_high = 3.0
-etaCut = Cut("midJetEta", "abs(_fwdMostLightJet_0_Eta)>%f && abs(_fwdMostLightJet_0_Eta<%f)" % (eta_low, etea_high))
+etaCut = Cut("midJetEta", "abs(_lowestBTagJet_0_Eta)>%f && abs(_lowestBTagJet_0_Eta<%f)" % (eta_low, eta_high))
 for var, varName, r, varNameF in vars3J1T:
-    plotComp(var, varName,
-    baseCut+leptonCut+Cuts.jets_3J1T+etaCut,
-    varName=varName,
-    title=" in muon channel, skim, ref.sel., 3J1T, %d < |#eta_{lj}| < %d"%(round(eta_low), round(eta_high)),
-    filename="fastSimValid_3J1T_midEta_%s.png"%varNameF,
-    weight=defaultWeight, normalization="unity"
+    plotComp(var,
+        r,
+        baseCut+leptonCut+Cuts.jets_3J1T+etaCut,
+        varName=varName,
+        title=" in %s channel, skim, ref.sel., 3J1T, %d < |#eta_{lj}| < %d"%(lepton, round(eta_low), round(eta_high)),
+        filename="fastSimValid_3J1T_midEta_%s.png"%varNameF,
+        weight=defaultWeight, normalization="unity"
     )
 
