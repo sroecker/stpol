5b1c9c627c14cc74cb503c3a8863bf3d84c4e89b
jpata <joosep.pata@gmail.com>
2013-02-14 16:10:53 +0200

    add in two passes

--- util/addCrabTrees.sh
+++ util/addCrabTrees.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
-for i in WD_*
+for i in `find . -name 'WD_*' -type d`
 do
-    hadd $i.root `find . -wholename './'$i'/res/*.root'`
-    if [ "$?" -ne 0 ]; then break; echo "Error!"; fi
+    echo $i
+    rm -f /tmp/joosep/tree_*.root
+    echo "First pass"
+    find $i/res -name "*.root" | parallel -n10 'hadd -f /tmp/joosep/tree_{#}.root {} > /dev/null'
+    echo "Second pass"
+    hadd -f ${i:5}.root /tmp/joosep/tree_*.root > /dev/null
+    rm /tmp/joosep/tree_*.root
 done
