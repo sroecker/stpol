afeec125621b4e43080a62007403074d8db2422c
jpata <joosep.pata@gmail.com>
2013-02-18 19:48:36 +0200

    tabbing

--- newplots/plotfw/drawfw.py
+++ newplots/plotfw/drawfw.py
@@ -5,55 +5,55 @@ from methods import Sample, MCSample, DataSample, SampleList, PlotParams
 
 class SampleListGenerator:
 	"""Helper class that makes it easier to generate sample lists for MC.
-	
+
 	It assumes that all the samples are in the directory.
 	It uses the color and cross sections defined in params.py for the
 	corresponding sample and group parameters.
-	
+
 	"""
 	def __init__(self, directory):
 		self._directory = directory
 		self._samplelist = SampleList()
-		
+
 	def add(self, groupname, samplename, fname):
 		if groupname not in self._samplelist.groups:
 			g = methods.SampleGroup(groupname, params.colors[samplename])
 			self._samplelist.addGroup(g)
-			
+
 		# Create the sample
 		if samplename in params.xs:
 			xs = params.xs[samplename]
 		else:
 			logging.warning('Notice: cross section fallback to group (g: %s, s: %s)', groupname, samplename)
-			xs = params.xs[groupname] 
+			xs = params.xs[groupname]
 		s = methods.MCSample(fname, xs, samplename, directory=self._directory)
 		self._samplelist.groups[groupname].add(s)
-	
+
 	def getSampleList(self):
 		return self._samplelist
 
 class StackedPlotCreator:
 	"""Class that is used to create stacked plots
-	
+
 	Initalizer takes the data and MC samples which are then plotted.
 	datasamples is either of type DataSample or [DataSample].
 	mcsamples has to be of type SampleList
-	
+
 	"""
 	def __init__(self, datasamples, mcsamples):
 		self._mcs = mcsamples
-		
+
 		# if a single data sample is given it does not have to be a list
 		if isinstance(datasamples, list):
 			self._data = datasamples
 		else:
 			self._data = [datasamples]
-	
+
 	def plot(self, cut, plots):
 		"""Method takes a cut and list of plots and then returns a list plot objects."""
 		# Apply cuts
 		self._cutstr = cut.cutStr
-		
+
 		# Plot
 		retplots = []
 		for p in plots:
@@ -61,45 +61,46 @@ class StackedPlotCreator:
 			#mpo.save(p.var, log=False)
 			retplots.append(mpo)
 		return retplots
-	
+
 	def _plot(self, pp):
 		"""Internally used plotting method.
-		
+
 		It takes a PlotParams class and returns the corresponding Plot
 		object.
-		
+
 		"""
 		print 'Plotting:', pp
-		
+
 		plotname = 'plot_' + pp.getName()
-		
+
 		p = Plot(pp)
 		p.log.addParam('Variable', pp.var)
 		p.log.addParam('HT min', pp.hmin)
 		p.log.addParam('HT max', pp.hmax)
 		p.log.addParam('HT bins', pp.hbins)
+		enabledBranches = []
 		#p.log.addParam('Integrated scaling', str(intsc))
-		
+
 		cut_string = self._cutstr
 		p.log.setCuts([''], cut_string)
-		
+
 		# Create the legend
 		p.legend = ROOT.TLegend(0.80, 0.65, 1.00, 0.90)
-		
+
 		# Create log variables
 		p.log.addVariable('filled', 'Events filled')
 		p.log.addVariable('int', 'Integrated events')
-		
+
 		# Create data histogram
 		total_luminosity = 0.0
 		dt_hist_name = '%s_hist_data'%plotname
 		p.dt_hist = ROOT.TH1F(dt_hist_name, '', pp.hbins, pp.hmin, pp.hmax)
 		p.dt_hist.SetMarkerStyle(20)
 		for d in self._data:
-			dt_filled = d.tree.Draw('%s>>%s'%(pp.var, dt_hist_name), cut_string, 'goff')
+			dt_filled = d.tree.Draw('%s>>+%s'%(pp.var, dt_hist_name), cut_string, 'goff')
 			total_luminosity += d.luminosity
 		dt_int = p.dt_hist.Integral()
-		
+
 		'''
 		p.log.addProcess('data', ismc=False)
 		p.log.setVariable('data', 'crsec', self._data.luminosity)
@@ -107,16 +108,16 @@ class StackedPlotCreator:
 		p.log.setVariable('data', 'filled', dt_filled)
 		p.log.setVariable('data', 'int', dt_int)
 		'''
-		
+
 		# TODO: implement effectice luminosity
 		#effective_lumi = self._data.luminosity*float(self._data.tree.GetEntries())/float(self._data.getTotalEvents())
 		effective_lumi = total_luminosity
 		p.log.addParam('Luminosity', total_luminosity)
 		p.log.addParam('Effective luminosity', effective_lumi)
-		
+
 		data_max = p.dt_hist.GetMaximum()
 		p.log.addParam('Data binmax', data_max)
-		
+
 		class TempMCS:
 			def __init__(self, g, s):
 				self.fname = s.fname
@@ -124,7 +125,7 @@ class StackedPlotCreator:
 				self.crsec = s.xs
 				self.color = g.color
 				self.tree = s.tree
-				
+
 				self._totev = s.getTotalEvents()
 			def getTotalEvents(self):
 				return self._totev
@@ -134,7 +135,7 @@ class StackedPlotCreator:
 			g = self._mcs.groups[gk]
 			for s in g.samples:
 				temp_mcs.append(TempMCS(g, s))
-		
+
 		mc_int = 0
 		p.mc_hists = []
 		for mc in temp_mcs:
@@ -142,88 +143,88 @@ class StackedPlotCreator:
 			p.log.setVariable(mc.name, 'crsec', mc.crsec)
 			p.log.setVariable(mc.name, 'fname', mc.fname)
 			hist_name = 'hist_%s_mc_%s'%(plotname, mc.name)
-			
+
 			mc_hist = ROOT.TH1F(hist_name, '', pp.hbins, pp.hmin, pp.hmax)
 			mc_hist.SetFillColor(mc.color)
 			mc_hist.SetLineWidth(0)
 			p.mc_hists.append(mc_hist)
-			
+
 			mc_filled = mc.tree.Draw('%s>>%s'%(pp.var,hist_name), cut_string, 'goff')
 			p.log.setVariable(mc.name, 'filled', mc_filled)
-			
+
 			# MC scaling
 			expected_events = mc.crsec*effective_lumi
 			total_events = mc.getTotalEvents()
 			scale_factor = float(expected_events)/float(total_events)
 			mc_hist.Scale(scale_factor)
-			
+
 			p.legend.AddEntry(mc_hist, mc.name, 'F')
-			
+
 			mc_int += mc_hist.Integral()
-			
+
 			p.log.setVariable(mc.name, 'totev', total_events)
 			p.log.setVariable(mc.name, 'expev', expected_events)
 			p.log.setVariable(mc.name, 'scf', scale_factor)
 			p.log.setVariable(mc.name, 'int', mc_int)
-		
+
 		'''
 		if intsc:
 			for mc_hist in p.mc_hists:
 				mc_hist.Scale(dt_int/mc_int)
 		'''
-		
+
 		# Kolmorogov test
 		basemc = ROOT.TH1F('hist_mc_ktbase', '', pp.hbins, pp.hmin, pp.hmax)
 		for mc_hist in p.mc_hists:
 			basemc.Add(mc_hist)
-		
+
 		mc_max = basemc.GetMaximum()
 		p.log.addParam('MC binmax', mc_max)
-		
+
 		p.log.addParam('Kolmogorov test', p.dt_hist.KolmogorovTest(basemc))
-		
+
 		# Stacking the histograms
 		plot_title = '%s (%s)'%(pp.var, plotname)
 		#if self.chstring is not None:
 		#	plot_title += ' [' + str(self.chstring) + ']'
 		p.stack = ROOT.THStack('stack_%s'%plotname, plot_title)
-		
+
 		for ht in p.mc_hists:
 			p.stack.Add(ht)
-			
+
 		p.stack.SetMaximum(1.1*max(data_max, mc_max))
 		#p.stack.GetXaxis().SetTitle('This is the x-axis title. (GeV)')
 		#p.stack.GetYaxis().SetTitle('This is the Y-axis title. (GeV)')
-		
+
 		# return the plot object where it can be drawn etc.
 		return p
 
 class Plot:
 	"""This class represents a single plot and has the methods to export it.
-	
+
 	This class puts everything together (different histograms, legend etc)
 	and allows to export the plot easily. It also handles the metadata
 	logging.
-	
+
 	"""
 	def __init__(self, pp):
 		self.log = plotlog.PlotLog()
 		self._pp = pp
-	
+
 	def draw(self):
 		self.stack.Draw('')
 		self.dt_hist.Draw('E1 SAME')
 		self.legend.Draw('SAME')
-	
+
 	def save(self, w=550, h=400, log=False, fmt='png', fout=None):
 		if fout is None:
 			fout = self._pp.getName()
 		ofname = fout+'.'+fmt
-		
+
 		logging.info('Saving as: %s', ofname)
 		self.cvs = ROOT.TCanvas('tcvs_%s'%self._pp.var, self._pp.var, w, h)
 		self.draw()
 		self.cvs.SaveAs(ofname)
-		
+
 		if log:
 			self.log.save(fout+'.pylog')
--- newplots/plotfw/plotlog.py
+++ newplots/plotfw/plotlog.py
@@ -4,23 +4,23 @@ class PlotLog:
 	def __init__(self, verbose=True):
 		self.cuts = None
 		self.cutstring = None
-		
+
 		self._variables = {}
 		self._processes = {}
 		self._params = []
-		
+
 		self._v = verbose
-	
+
 	def setCuts(self, cuts, cutstring):
 		self.cuts = cuts
 		self.cutstring = cutstring
-		
+
 		if self._v:
 			print 'Cuts applied:'
 			for c in self.cuts:
 				print '>', c
 			logging.info('Generated cutstring: %s', self.cutstring)
-	
+
 	def addProcess(self, name, title=None, ismc=True):
 		if name in self._processes:
 			logging.warning('Process `%s` already exists!', name)
@@ -30,33 +30,33 @@ class PlotLog:
 				'ismc': bool(ismc),
 				'vars': {}
 			}
-	
+
 	def addParam(self, title, value):
 		np = {
 			'title': title,
 			'value': value
 		}
-		
+
 		self._params.append(np)
-		
+
 		if self._v:
 			logging.info('P `%s`: %s', title, value)
-		
+
 	def setVariable(self, proc, var, value):
 		if proc not in self._processes:
 			logging.warning('Warning: Process `%s` does not exists!', var)
 			return
-			
+
 		if var not in self._variables:
 			self.addVariable(var)
-		
+
 		if var in self._processes[proc]['vars']:
 			logging.info('Notice: Variable already has value! (%s::%s)', proc, var)
-		
+
 		self._processes[proc]['vars'][var] = value
 		if self._v:
 			logging.info('%s::%s: %s', proc, var, value)
-	
+
 	def addVariable(self, var, title=None):
 		if var in self._variables:
 			logging.info('Notice: Variable `%s` already exists!', var)
@@ -64,7 +64,7 @@ class PlotLog:
 			self._variables[var] = {
 				'title': title if title is not None else var
 			}
-	
+
 	def save(self, fname):
 		logging.info('Pickling log to `%s`', fname)
 		fh = open(fname, 'w')
