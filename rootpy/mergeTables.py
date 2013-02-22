bba4cd11a4ccffbf7a2d1e947b9c14cf53cd2fc7
jpata <joosep.pata@gmail.com>
2013-02-23 00:14:22 +0200

    better gui

--- rootpy/mergeTables.py
+++ rootpy/mergeTables.py
@@ -6,6 +6,7 @@ from collections import OrderedDict
 from numpy.lib.recfunctions import merge_arrays
 import math
 import numpy
+import time
 
 print "Running merge script"
 infile = sys.argv[1]
@@ -18,7 +19,6 @@ nodes = []
 allcols = dict()
 for node in fi:
     if hasattr(node, "name") and node.name == "eventTree":
-        print node.name
         for coln in node.colnames:
             allcols[coln] = node.coldescrs[coln]
         nodes.append(node._v_pathname)
@@ -37,7 +37,7 @@ for key in allcols.keys():
 nRows = fi.getNode(nodes[0]).nrows
 #row = newT.row
 #for i in range(nRows):
-blocksize = 10000
+blocksize = 50000
 nBlocks = int(math.ceil(nRows/blocksize))
 print nBlocks
 arrs = []
@@ -45,19 +45,26 @@ for nodeName in nodes:
     node = fi.getNode(nodeName)
     arrs.append(node[0])
 merged = merge_arrays((arrs), asrecarray=True, flatten=True)
-newT = of.createTable("/", "events", merged, expectedrows=nRows, filters=tables.Filters(complevel=0, complib='zlib', fletcher32=False))
+newT = of.createTable("/", "events", merged, expectedrows=nRows, filters=tables.Filters(complevel=9, complib='blosc', fletcher32=False))
+time_elapsed = 0
+times_elapsed = []
+
 for i in range(nBlocks):
+    time_1 = time.time()
     arrs = []
+    ndots = 100*i/nBlocks
+    sys.stdout.write("\r" + "[" + "."*ndots + " "*(100-ndots) + "] " + str(i*blocksize) + "/" + str(nRows) + " ETA:" + str(int(numpy.mean(times_elapsed)*(nBlocks-i))) + " s")
+    sys.stdout.flush()
     for nodeName in nodes:
         node = fi.getNode(nodeName)
         arrs.append(node[i*blocksize:(i+1)*blocksize])
     [newT.append(numpy.empty((1, ), dtype=newT.dtype)) for j in range(blocksize)]
     for arr in arrs:
         newT.modifyColumns(i*blocksize, (i+1)*blocksize, columns=arr, names=arr.dtype.fields.keys())
-    ndots = 100*i/nBlocks
-    sys.stdout.write("\r" + "[" + "."*ndots + " "*(100-ndots) + "]")
-    sys.stdout.flush()
     newT.flush()
+    time_2 = time.time()
+    time_elapsed = time_2 - time_1
+    times_elapsed.append(time_elapsed)
 
 print "Put {0} of {1} rows".format(newT.nrows, nRows)
 #for nodeName in nodes:
