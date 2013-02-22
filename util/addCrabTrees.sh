2b4d1388eec5f86c9858277fa2be0d30e4c2aa05
jpata <joosep.pata@gmail.com>
2013-02-22 10:46:55 +0200

    proper subdir

--- util/addCrabTrees.sh
+++ util/addCrabTrees.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+WD="/tmp/joosep/"${PWD##*/}
+export WD
+mkdir $WD
 for i in `find . -name 'WD_*' -type d`
 do
     echo $i
-    rm -f /tmp/joosep/tree_*.root
+    rm -f $WD/tree_*.root
     echo "First pass"
-    find $i/res -name "*.root" | parallel -n10 'hadd -f /tmp/joosep/tree_{#}.root {} > /dev/null'
+    find $i/res -name "*.root" | parallel -n10 'hadd -f $WD/tree_{#}.root {} > /dev/null'
     echo "Second pass"
-    hadd -f ${i:5}.root /tmp/joosep/tree_*.root > /dev/null
-    rm /tmp/joosep/tree_*.root
+    hadd -f ${i:5}.root $WD/tree_*.root > /dev/null
+    rm $WD/tree_*.root
 done
