af7a31cf5b2e8fa678dd6526949b2cbec8486550
Joosep Pata <joosep.pata@gmail.com>
2013-02-22 23:56:32 +0200

    more efficient merge

--- rootpy/mergeTables.py
+++ rootpy/mergeTables.py
@@ -2,6 +2,11 @@ import tables
 import sys
 import pdb
 import time
+from collections import OrderedDict
+from numpy.lib.recfunctions import merge_arrays
+import math
+import numpy
+
 print "Running merge script"
 infile = sys.argv[1]
 outfile = infile.replace(".h5", "_opt.h5")
@@ -19,7 +24,7 @@ for node in fi:
         nodes.append(node._v_pathname)
 
 
-nodesToCreate = {}
+nodesToCreate = OrderedDict()
 
 for key in allcols.keys():
     if allcols[key].dtype == "float64":
@@ -30,20 +35,40 @@ for key in allcols.keys():
         nodesToCreate[key] = allcols[key]
 
 nRows = fi.getNode(nodes[0]).nrows
-newT = of.createTable("/", "events", nodesToCreate, expectedrows=nRows, filters=tables.Filters(complevel=0, complib='zlib', fletcher32=False))
-row = newT.row
-for i in range(nRows):
-    row.append()
-newT.flush()
-print len(nodes)
+#row = newT.row
+#for i in range(nRows):
+blocksize = 10000
+nBlocks = int(math.ceil(nRows/blocksize))
+print nBlocks
+arrs = []
 for nodeName in nodes:
     node = fi.getNode(nodeName)
+    arrs.append(node[0])
+merged = merge_arrays((arrs), asrecarray=True, flatten=True)
+newT = of.createTable("/", "events", merged, expectedrows=nRows, filters=tables.Filters(complevel=0, complib='zlib', fletcher32=False))
+for i in range(nBlocks):
+    arrs = []
+    for nodeName in nodes:
+        node = fi.getNode(nodeName)
+        arrs.append(node[i*blocksize:(i+1)*blocksize])
+    [newT.append(numpy.empty((1, ), dtype=newT.dtype)) for j in range(blocksize)]
+    for arr in arrs:
+        newT.modifyColumns(i*blocksize, (i+1)*blocksize, columns=arr, names=arr.dtype.fields.keys())
+    ndots = 100*i/nBlocks
+    sys.stdout.write("\r" + "[" + "."*ndots + " "*(100-ndots) + "]")
+    sys.stdout.flush()
+    newT.flush()
 
-    for coln in node.colnames:
-        t0 = time.time()
-        print "putting {0}".format(coln)
-        newT.modifyColumn(colname=coln, column=node.colinstances[coln][:])
-        newT.flush()
-        t1 = time.time()
-        dt = t1-t0
-        print "Done putting {0} in {1:.2f} sec".format(coln, dt)
+print "Put {0} of {1} rows".format(newT.nrows, nRows)
+#for nodeName in nodes:
+#    node = fi.getNode(nodeName)
+#
+#    for coln in node.colnames:
+#        t0 = time.time()
+#        print "putting {0}".format(coln)
+#        pdb.set_trace()
+#        newT.modifyColumn(colname=coln, column=node.colinstances[coln][:])
+#        newT.flush()
+#        t1 = time.time()
+#        dt = t1-t0
+#        print "Done putting {0} in {1:.2f} sec".format(coln, dt)
