d4877c82bd5cd2ac3ea08813ff8feccfcd58a794
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-02-27 11:18:15 +0200

    Refactor samples.py. Add ttbar splitting.

--- newplots/samples.py
+++ newplots/samples.py
@@ -3,91 +3,77 @@ from plotfw import drawfw
 
 import ROOT
 
-#_directory = '/home/joosep/singletop/data/trees/Feb18/Iso'
-_directory = '/scratch/joosep/Iso'
+# Parameters with reasonable defaults
+directory = '/home/joosep/singletop/data/trees/Feb18/Iso'
+fulldata = True
+split_ttbar = False
 
-datasmplsMu = [
-	drawfw.DataSample('SingleMuAB_5269_pb.root', 5269, name="SingleMuAB", directory=_directory),
-	drawfw.DataSample('SingleMuC_6655_pb.root', 6655, name="SingleMuC", directory=_directory),
-	drawfw.DataSample('SingleMuD_7104_pb.root', 7104, name="SingleMuD", directory=_directory),
-]
+# Output variables
+pltcMu = None
+pltcEle = None
 
-smplsMu = drawfw.SampleGroup("mu", ROOT.kBlack, "single mu")
-smplsMu.pretty_name = "single #mu^{#pm}"
-smplsMu.addList(datasmplsMu)
+def load():
+	global directory, fulldata, split_ttbar # parameters
+	global pltcMu, pltcEle # output variables
+	
+	# Data samples
+	datasmplsMu = [
+		drawfw.DataSample('SingleMuAB_5299_pb.root', 5299, name="SingleMuAB", directory=directory),
+		drawfw.DataSample('SingleMuC_6790_pb.root', 6790, name="SingleMuC", directory=directory),
+		drawfw.DataSample('SingleMuD_7274_pb.root', 7247, name="SingleMuD", directory=directory),
+	]
 
-datasmplsEle = [
-	drawfw.DataSample('SingleEleA1_82_pb.root', 82, name="SingleEleA", directory=_directory),
-	drawfw.DataSample('SingleEleB_5125_pb.root', 5125, name="SingleEleB", directory=_directory),
-	drawfw.DataSample('SingleEleC1_495_pb.root', 495, name="SingleEleC1", directory=_directory),
-	drawfw.DataSample('SingleEleC2_5938_pb.root', 5938, name="SingleEleC2", directory=_directory),
-	drawfw.DataSample('SingleEleD_7002_pb.root', 7002, name="SingleEleD", directory=_directory)
-]
-smplsEle = drawfw.SampleGroup("ele", ROOT.kBlack, "single ele")
-smplsEle.addList(datasmplsEle)
+	datasmplsEle = [
+		drawfw.DataSample('SingleEleA1_82_pb.root', 82, directory=directory),
+		drawfw.DataSample('SingleEleC1_495_pb.root', 495, directory=directory),
+		drawfw.DataSample('SingleEleC2_6118_pb.root', 6118, directory=directory),
+		drawfw.DataSample('SingleEleD_7234_pb.root', 7234, directory=directory)
+	]
+	
+	if not fulldata:
+		datasmplsMu = datasmplsMu[0:1]
+		datasmplsEle = datasmplsEle[0:2]
 
-smplsgen = plotfw.methods.SampleListGenerator(_directory)
-#smplsgen.add('TTbar', 'TTbar', 'TTJets_MassiveBinDECAY.root')
-smplsgen.add('TTbar', 'TTJets_FullLept', 'TTJets_FullLept.root')
-smplsgen.add('TTbar', 'TTJets_SemiLept', 'TTJets_SemiLept.root')
+	# Monte Carlo samples
+	smplsgen = drawfw.SampleListGenerator(directory)
+	
+	#smplsgen.add('TTbar', 'TTbar', 'TTJets_MassiveBinDECAY.root')
+	smplsgen.add('TTJets_SemiLept' if split_ttbar else 'TTbar', 'TTJets_SemiLept', 'TTJets_SemiLept.root')
+	smplsgen.add('TTJets_FullLept' if split_ttbar else 'TTbar', 'TTJets_FullLept', 'TTJets_FullLept.root')
 
-smplsgen.add('t-channel', 'T_t', 'T_t.root')
-smplsgen.add('t-channel', 'Tbar_t', 'Tbar_t.root')
-smplsgen.add('s-channel', 'T_s', 'T_s.root')
-smplsgen.add('s-channel', 'Tbar_s', 'Tbar_s.root')
-smplsgen.add('tW-channel', 'T_tW', 'T_tW.root')
-smplsgen.add('tW-channel', 'Tbar_tW', 'Tbar_tW.root')
+	smplsgen.add('t-channel', 'T_t', 'T_t.root')
+	smplsgen.add('t-channel', 'Tbar_t', 'Tbar_t.root')
+	smplsgen.add('s-channel', 'T_s', 'T_s.root')
+	smplsgen.add('s-channel', 'Tbar_s', 'Tbar_s.root')
+	smplsgen.add('tW-channel', 'T_tW', 'T_tW.root')
+	smplsgen.add('tW-channel', 'Tbar_tW', 'Tbar_tW.root')
 
-#smplsgen.add('WJets', 'WJets', 'WJets_inclusive.root')
-smplsgen.add('WJets', 'W1Jets', 'W1Jets_exclusive.root')
-smplsgen.add('WJets', 'W2Jets', 'W2Jets_exclusive.root')
-smplsgen.add('WJets', 'W3Jets', 'W3Jets_exclusive.root')
-smplsgen.add('WJets', 'W4Jets', 'W4Jets_exclusive.root')
+	smplsgen.add('WJets', 'WJets', 'WJets_inclusive.root')
 
-smplsgen.add('DYJets', 'DYJets', 'DYJets.root')
+	smplsgen.add('DYJets', 'DYJets', 'DYJets.root')
 
-smplsgen.add('diboson', 'WW', 'WW.root')
-smplsgen.add('diboson', 'WZ', 'WZ.root')
-smplsgen.add('diboson', 'ZZ', 'ZZ.root')
+	smplsgen.add('diboson', 'WW', 'WW.root')
+	smplsgen.add('diboson', 'WZ', 'WZ.root')
+	##FIXME: ZZ is missing
 
-smplsgen.add('QCD', 'QCDMu', 'QCDMu.root')
+	smplsgen.add('QCD', 'QCDMu', 'QCDMu.root')
 
-smplsgen.add('QCD', 'GJets1', 'GJets1.root')
-smplsgen.add('QCD', 'GJets2', 'GJets2.root')
+	smplsgen.add('QCD', 'QCD_Pt_20_30_BCtoE', 'QCD_Pt_20_30_BCtoE.root')
+	smplsgen.add('QCD', 'QCD_Pt_30_80_BCtoE', 'QCD_Pt_30_80_BCtoE.root')
+	smplsgen.add('QCD', 'QCD_Pt_80_170_BCtoE', 'QCD_Pt_80_170_BCtoE.root')
+	smplsgen.add('QCD', 'QCD_Pt_170_250_BCtoE', 'QCD_Pt_170_250_BCtoE.root')
+	smplsgen.add('QCD', 'QCD_Pt_250_350_BCtoE', 'QCD_Pt_250_350_BCtoE.root')
 
-smplsgen.add('QCD', 'QCD_Pt_20_30_BCtoE', 'QCD_Pt_20_30_BCtoE.root')
-smplsgen.add('QCD', 'QCD_Pt_30_80_BCtoE', 'QCD_Pt_30_80_BCtoE.root')
-smplsgen.add('QCD', 'QCD_Pt_80_170_BCtoE', 'QCD_Pt_80_170_BCtoE.root')
-smplsgen.add('QCD', 'QCD_Pt_170_250_BCtoE', 'QCD_Pt_170_250_BCtoE.root')
-smplsgen.add('QCD', 'QCD_Pt_250_350_BCtoE', 'QCD_Pt_250_350_BCtoE.root')
-smplsgen.add('QCD', 'QCD_Pt_350_BCtoE', 'QCD_Pt_350_BCtoE.root')
+	smplsgen.add('QCD', 'QCD_Pt_20_30_EMEnriched', 'QCD_Pt_20_30_EMEnriched.root')
+	smplsgen.add('QCD', 'QCD_Pt_30_80_EMEnriched', 'QCD_Pt_30_80_EMEnriched.root')
+	smplsgen.add('QCD', 'QCD_Pt_80_170_EMEnriched', 'QCD_Pt_80_170_EMEnriched.root')
+	##FIXME: 170_250 is missing
+	smplsgen.add('QCD', 'QCD_Pt_350_EMEnriched', 'QCD_Pt_350_EMEnriched.root')
 
-smplsgen.add('QCD', 'QCD_Pt_20_30_EMEnriched', 'QCD_Pt_20_30_EMEnriched.root')
-smplsgen.add('QCD', 'QCD_Pt_30_80_EMEnriched', 'QCD_Pt_30_80_EMEnriched.root')
-smplsgen.add('QCD', 'QCD_Pt_80_170_EMEnriched', 'QCD_Pt_80_170_EMEnriched.root')
-smplsgen.add('QCD', 'QCD_Pt_170_250_EMEnriched', 'QCD_Pt_170_250_EMEnriched.root')
-smplsgen.add('QCD', 'QCD_Pt_250_350_EMEnriched', 'QCD_Pt_250_350_EMEnriched.root')
-smplsgen.add('QCD', 'QCD_Pt_350_EMEnriched', 'QCD_Pt_350_EMEnriched.root')
+	smpls = smplsgen.getSampleList()
+	if 'TTbar' in smpls.groups:
+		smpls.groups['TTbar'].prettyName = 't#bar{t}'
 
-smpls = smplsgen.getSampleList()
-smpls.groups["TTbar"].pretty_name = "t#bar{t} (#rightarrow ll, lj)"
-smpls.groups["WJets"].pretty_name = "W(#rightarrow l#nu) + jets(inc.)"
-smpls.groups["QCD"].pretty_name = "QCD (MC)"
-
-for sample in smpls.groups["QCD"].samples:
-	sample.disabled_weights = ["bTagWeight_bTagWeightProducer"]
-
-pltcMu = drawfw.StackedPlotCreator(datasmplsMu, smpls)
-pltcEle = drawfw.StackedPlotCreator(datasmplsEle, smpls)
-
-smplsTest = plotfw.methods.SampleList()
-smplsTest.addGroup(smpls.groups["t-channel"])
-smplsTest.addGroup(smpls.groups["TTbar"])
-datasmplsMuTest = [
-	drawfw.DataSample('SingleMuAB_5269_pb.root', 5269, name="SingleMuAB", directory=_directory)
-]
-pltcMuTest = drawfw.StackedPlotCreator(datasmplsMuTest, smplsTest)
-
-smplsAllMC = plotfw.methods.SampleGroup("allmc", ROOT.kRed, "full MC")
-for group in smpls.groups.values():
-	smplsAllMC.samples += group.samples
+	# Output the plotcreators
+	pltcMu = drawfw.StackedPlotCreator(datasmplsMu, smpls)
+	pltcEle = drawfw.StackedPlotCreator(datasmplsEle, smpls)
