e50314c81b672dc6f24223d8f33f53bfa6cdd5d9
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-03-14 00:31:58 +0200

    Update example.py

--- newplots/example.py
+++ newplots/example.py
@@ -1,41 +1,42 @@
-from plotfw import drawfw
-from plotfw.params import Cuts as cutlist
+import logging
+logging.basicConfig(level=logging.DEBUG, format="%(levelname)s %(message)s")
+
+from plotfw import drawfw, methods
+from plotfw.params import Cuts as cuts, Vars as variables
 
 # Set samples
-datasmpl = drawfw.DataSample('WD_SingleMuAB_4665_pb.root', 4665, directory='/home/joosep/singletop/data/trees/Feb8_A/Iso')
+datasmpl = methods.DataSample('WD_SingleEleC1', 459, directory='/home/joosep/singletop/stpol/crabs/step2_Data_Iso_Mar11/')
 
-smplsgen = drawfw.SampleListGenerator('/home/joosep/singletop/data/trees/Feb8_A/Iso')
-smplsgen.add('Diboson', 'WW', 'WD_WW.root')
-smplsgen.add('Diboson', 'WZ', 'WD_WZ.root')
-smplsgen.add('Diboson', 'ZZ', 'WD_ZZ.root')
-smplsgen.add('TTbar', 'TTbar', 'WD_TTbar.root')
-smplsgen.add('WJets', 'W1Jets', 'W1Jets.root')
-smplsgen.add('WJets', 'W2Jets', 'W2Jets.root')
-smplsgen.add('WJets', 'W3Jets', 'W3Jets.root')
-smplsgen.add('WJets', 'W4Jets', 'W4Jets.root')
-smplsgen.add('T', 'T_t', 'WD_T_t.root')
-smplsgen.add('T', 'T_s', 'WD_T_s.root')
-smplsgen.add('T', 'T_tW', 'WD_T_tW.root')
-smplsgen.add('Tbar', 'Tbar_t', 'WD_Tbar_t.root')
-smplsgen.add('Tbar', 'Tbar_s', 'WD_Tbar_s.root')
-smplsgen.add('Tbar', 'Tbar_tW', 'WD_Tbar_tW.root')
+smplsgen = methods.SampleListGenerator('/home/joosep/singletop/stpol/crabs/step2_MC_Iso_Mar11/')
+#smplsgen.add('TTbar', 'TTJets_SemiLept', 'WD_TTJets_SemiLept')
+#smplsgen.add('TTbar', 'TTJets_FullLept', 'WD_TTJets_FullLept')
+smplsgen.add('t-channel', 'T_t', 'WD_T_t')
+#smplsgen.add('t-channel', 'Tbar_t', 'WD_Tbar_t')
+smplsgen.add('s-channel', 'T_s', 'WD_T_s')
+#smplsgen.add('s-channel', 'Tbar_s', 'WD_Tbar_s')
+smplsgen.add('tW-channel', 'T_tW', 'WD_T_tW')
+#smplsgen.add('tW-channel', 'Tbar_tW', 'WD_Tbar_tW')
+#smplsgen.add('WJets', 'W1Jets', 'WD_W1Jets_exclusive')
+#smplsgen.add('WJets', 'W2Jets', 'WD_W2Jets_exclusive')
+#smplsgen.add('WJets', 'W3Jets', 'WD_W3Jets_exclusive')
+#smplsgen.add('WJets', 'W4Jets', 'WD_W4Jets_exclusive')
 smpls = smplsgen.getSampleList()
 
 smpls.listSamples() # print sample list
 
 # Set the cut
 #cut = drawfw.methods.Cut('', '')
-cut = cutlist.finalMu
+cut = cuts.finalEle
 print 'Cut:', str(cut)
 
 # Set plots
 plots = [
-	drawfw.PlotParams('_recoTop_0_Mass', (130, 220)),
-	drawfw.PlotParams('abs(_lowestBTagJet_0_Eta)', (2.5, 4.5))
+	drawfw.PlotParams(variables.top_mass, (130, 220)),
+	drawfw.PlotParams(variables.etalj, (2.5, 4.5))
 ]
 
 # Plot
-pltc = drawfw.StackedPlotCreator(datasmpl, smpls)
+pltc = drawfw.StackPlotCreator(datasmpl, smpls)
 ps = pltc.plot(cut, plots)
 
 for p in ps:
