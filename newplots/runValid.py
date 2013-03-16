8a56a60fe0800e2f14ed718d7b3fe2c6129ac89e
jpata <joosep.pata@gmail.com>
2013-03-16 21:07:31 +0200

    W3Jets test

--- newplots/runValid.py
+++ newplots/runValid.py
@@ -1,6 +1,6 @@
 import autoLoad
 import logging
-logging.basicConfig(level=logging.WARNING, format="%(asctime)s;%(levelname)s;%(name)s;%(message)s")
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s;%(levelname)s;%(name)s;%(message)s")
 import plotfw.methods
 import plotfw.drawfw
 import plotfw.params
@@ -14,15 +14,17 @@ import numpy
 import unittest
 
 
-mc_dir = "/home/joosep/singletop/stpol/crabs/step2_MC_Iso_Mar14/"
+#mc_dir = "/home/joosep/singletop/stpol/crabs/step2_MC_Iso_Mar14/"
+mc_dir = "/scratch/joosep/step2_MC_Iso_Mar14/"
+#mc_dir = "/hdfs/local/joosep/stpol/step2_MC_Iso_Mar14"
 data_dir = "/home/joosep/singletop/stpol/crabs/step2_Data_Iso_Mar15/"
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 class SampleTestBase(unittest.TestCase):
-    max_lines = 25000.0
+    max_lines = 1000000
     def loadMCSample(self, sample_name, indir):
-        return plotfw.methods.MCSample("WD_%s" % (sample_name), name=sample_name, directory=indir)
+        return plotfw.methods.MCSample("WD_%s" % (sample_name), xs=1, name=sample_name, directory=indir)
 
     def loadDataSample(self, sample_name, indir):
         return plotfw.methods.DataSample("WD_%s" % (sample_name), 1, name=sample_name, directory=indir)
@@ -35,7 +37,7 @@ class SampleTestBase(unittest.TestCase):
         assert(numpy.all(numpy.abs(cos_vals) < 1) == True)
 
     def base_test_sample_b_weights(self, sample):
-        vals = sample.getColumn(Vars.b_weight["nominal"], Cuts.finalMu_2J1T, maxLines=self.max_lines)
+        vals = sample.getColumn(Vars.b_weight["nominal"], Cuts.finalMu_2J0T, maxLines=self.max_lines)
         mean_weight = numpy.mean(vals)
         logger.info("sample %s mean b_weight[nominal] = %.2f" % (sample, mean_weight))
         assert(len(vals)>0)
@@ -54,15 +56,26 @@ class SampleTestBase(unittest.TestCase):
         self.base_test_sample_b_weights(sample)
         self.base_test_sample_pu_weight(sample)
 
-    def test_TTJets_FullLept(self):
-        sample = self.loadMCSample("TTJets_FullLept", mc_dir)
+    def test_W3Jets(self):
+        sample = self.loadMCSample("W3Jets_exclusive", mc_dir)
+        perf_stats = ROOT.TTreePerfStats("ioperf", sample.tree)
         self.base_test_sample_costheta(sample)
         self.base_test_sample_b_weights(sample)
         self.base_test_sample_pu_weight(sample)
+        perf_stats.Finish()
+        perf_stats.Print()
+        perf_stats.Draw()
+        perf_stats.SaveAs("perf.root")
 
-    def test_SingleMuD(self):
-        sample = self.loadDataSample("SingleMuD", data_dir)
-        self.base_test_sample_costheta(sample)
+    #def test_TTJets_FullLept(self):
+    #    sample = self.loadMCSample("TTJets_FullLept", mc_dir)
+    #    self.base_test_sample_costheta(sample)
+    #    self.base_test_sample_b_weights(sample)
+    #    self.base_test_sample_pu_weight(sample)
+
+    #def test_SingleMuD(self):
+    #    sample = self.loadDataSample("SingleMuD", data_dir)
+    #    self.base_test_sample_costheta(sample)
 
 if __name__=="__main__":
     unittest.main()
