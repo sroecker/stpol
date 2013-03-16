c95886fbf5edfbbc9afe93d16097ae9bea79bb0b
jpata <joosep.pata@gmail.com>
2013-03-16 22:28:57 +0200

    added more comprehensive step2 test

---
+++ util/test_step2_signal.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+IN=$CMSSW_BASE/../fileLists/T_t.txt
+OFDIR=$CMSSW_BASE/../testing_step2_signal
+rm -Rf $OFDIR
+mkdir $OFDIR
+
+time cmsRun $CMSSW_BASE/../runconfs/step2_newCmdLine_cfg.py doDebug=False inputFiles_load=$IN isMC=True channel=signal subChannel=T_t outputFile=$OFDIR/out.root maxEvents=10000 &> $OFDIR/log.txt
+EX=$?
+echo "Exit code:"$EX 
+if [ "$EX" -ne 0 ]
+then
+    grep "Exception" -A10 $OFDIR/log*.txt
+fi
+grep "CPU/real" $OFDIR/log*.txt
