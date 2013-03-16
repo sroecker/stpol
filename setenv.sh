93709a7870d6262bf327ed597ca08a72c889569c
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-03-16 21:52:34 +0200

    Reorganize newplots files.
    Create subdirectories for different script.
    Add newplots to PYTHONPATH in setenv.

--- setenv.sh
+++ setenv.sh
@@ -6,6 +6,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #echo "Current:" $CURRENT_DIR
 #echo "Script:" $SCRIPT_DIR
 
+# Set up CMSSW env
 export local_dbs_instance=cms_dbs_ph_analysis_02
 export local_dbs_url=https://cmsdbsprod.cern.ch:8443/cms_dbs_ph_analysis_02_writer/servlet/DBSServlet
 source /cvmfs/cms.cern.ch/cmsset_default.sh
@@ -13,4 +14,8 @@ cd ${SCRIPT_DIR}/CMSSW_5_3_7_patch4
 #cmsenv
 eval `scramv1 runtime -sh`
 
+# Add plotfw to python library path
+PYTHONPATH=$PYTHONPATH:${SCRIPT_DIR}/newplots
+
+# Return to original directory
 cd $CURRENT_DIR
