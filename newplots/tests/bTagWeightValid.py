3b3524d1136e55d90901db7f0945ea08e4f51848
jpata <joosep.pata@gmail.com>
2013-03-16 22:57:52 +0200

    all samples

--- newplots/tests/bTagWeightValid.py
+++ newplots/tests/bTagWeightValid.py
@@ -1,3 +1,5 @@
+#This script draws the b-tag weight validation plots in different jet/tag bins
+#author: joosep pata joosep.pata@cern.ch
 import autoLoad
 import multiprocessing
 import math
@@ -24,6 +26,8 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--ofdir', type=str, default="plots_out_" + ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(4)))
     parser.add_argument('-p', '--percent', type=float, default=100.0)
+    parser.add_argument('-i', '--mc_dir', type=str, default="/scratch/joosep/step2_MC_Iso_Mar14/")
+    parser.add_argument('-n', '--n_cores', type=int, default=8)
     parser.add_argument('--doBWeightDistributions', action='store_true')
     parser.add_argument('--doDataMC', action='store_true')
     parser.add_argument('--doEffs', action='store_true')
@@ -34,39 +38,28 @@ if __name__=="__main__":
     parser.add_argument('--do3J1T', action='store_true')
     parser.add_argument('--do3J2T', action='store_true')
     #parser.add_argument('--allSamples', action='store_true')
-
-
     args = parser.parse_args()
-    #parser.add_argument('--allSamples', action='store_true')
-
-
-    args = parser.parse_args()
-
 
     #def get_proof_output(self, name):
     #    return self.GetOutputList().FindObject(name)
 
     if args.withPROOF:
-        p = ROOT.TProof.Open("workers=8")
+        p = ROOT.TProof.Open("workers=%d" % args.n_cores)
     else:
         p = None
 
     samples = dict()
     samples_data = dict()
 
-    #path = "/hdfs/local/joosep/stpol/step2_MC_Iso/"
-    #path = "/home/joosep/singletop/stpol/crabs/step2_MC_Iso_Mar14/"
-    path = "/scratch/joosep/step2_MC_Iso_Mar14/"
-
-    samples["TTJets_FullLept"] = plotfw.methods.MCSample("WD_TTJets_FullLept", name="TTJets_FullLept", directory=path)
-    samples["TTJets_SemiLept"] = plotfw.methods.MCSample("WD_TTJets_SemiLept", name="TTJets_SemiLept", directory=path)
-    #samples["TTJets_inclusive"] = plotfw.methods.MCSample(path + "WD_TTJets_MassiveBinDECAY/res/*.root", name="TTbar")
-    #samples["T_t"] = plotfw.methods.MCSample("WD_T_t", name="T_t", directory=path)
-    samples["W1Jets"] = plotfw.methods.MCSample(path + "WD_W1Jets_exclusive", name="W1Jets")
-    samples["W2Jets"] = plotfw.methods.MCSample(path + "WD_W2Jets_exclusive", name="W2Jets")
-    samples["W3Jets"] = plotfw.methods.MCSample(path + "WD_W3Jets_exclusive", name="W3Jets")
-    samples["W4Jets"] = plotfw.methods.MCSample(path + "WD_W4Jets_exclusive", name="W4Jets")
-    #samples["WJets_inclusive"] = plotfw.methods.MCSample(path + "WD_WJets_inclusive/res/*.root", name="WJets")
+    samples["TTJets_FullLept"] = plotfw.methods.MCSample("WD_TTJets_FullLept", name="TTJets_FullLept", directory=args.mc_dir)
+    samples["TTJets_SemiLept"] = plotfw.methods.MCSample("WD_TTJets_SemiLept", name="TTJets_SemiLept", directory=args.mc_dir)
+    samples["TTJets_inclusive"] = plotfw.methods.MCSample("WD_TTJets_MassiveBinDECAY", name="TTbar", directory=args.mc_dir)
+    samples["T_t"] = plotfw.methods.MCSample("WD_T_t", name="T_t", directory=args.mc_dir)
+    samples["W1Jets"] = plotfw.methods.MCSample("WD_W1Jets_exclusive", name="W1Jets", directory=args.mc_dir)
+    samples["W2Jets"] = plotfw.methods.MCSample("WD_W2Jets_exclusive", name="W2Jets", directory=args.mc_dir)
+    samples["W3Jets"] = plotfw.methods.MCSample("WD_W3Jets_exclusive", name="W3Jets", directory=args.mc_dir)
+    samples["W4Jets"] = plotfw.methods.MCSample("WD_W4Jets_exclusive", name="W4Jets", directory=args.mc_dir)
+    samples["WJets_inclusive"] = plotfw.methods.MCSample("WD_WJets_inclusive", name="WJets", directory=args.mc_dir)
 
     groups = plotfw.methods.SampleGroup.fromList(samples.values())
 
@@ -77,14 +70,13 @@ if __name__=="__main__":
     if args.doBWeightDistributions:
         comp_samples = plotfw.drawfw.ShapePlotCreator(sl)
         comp_samples.frac_entries = args.percent/100.0
-        comp_samples.set_n_cores(1)
+        comp_samples.set_n_cores(args.n_cores)
         comp_samples.proof = p
         weightPlots = [
             plotfw.drawfw.PlotParams(Vars.b_weight["nominal"], (0, 2), doLogY=True, normalize_to="unity"),
             plotfw.drawfw.PlotParams(Vars.jet_counts_true["l"], (0, 5), bins=5, doLogY=True, normalize_to="unity"),
             plotfw.drawfw.PlotParams(Vars.jet_counts_true["b"], (0, 5), bins=5, doLogY=True, normalize_to="unity"),
             plotfw.drawfw.PlotParams(Vars.jet_counts_true["c"], (0, 5), bins=5, doLogY=True, normalize_to="unity"),
-            #plotfw.drawfw.PlotParams(Vars.cos_theta, (-1, 1))
         ]
         weightPlots[0].putStats()
 
