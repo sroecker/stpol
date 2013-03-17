27b3743ed3ae86c4185ad438bca1350138c652b6
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-03-17 12:46:22 +0200

    Update eletest.

--- newplots/tests/eletest.py
+++ newplots/tests/eletest.py
@@ -1,12 +1,23 @@
 import logging
-logging.basicConfig(level=logging.DEBUG, format="%(levelname)s %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(levelname)s %(message)s")
+
+testlog = logging.getLogger('eletest')
+testlog_fh = logging.FileHandler('eletest.log')
+testlog_fh.setLevel(logging.DEBUG)
+testlog_fh.setFormatter(logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s'))
+testlog.addHandler(testlog_fh)
 
 from plotfw import drawfw, methods
 from plotfw.params import Cuts as cuts, Vars as variables
 
 # Set samples
+directory = '/scratch/joosep/step2_Data_Iso_Mar15/'
 smpls = [
-	methods.DataSample('WD_SingleEleC1', 459, directory='/home/joosep/singletop/stpol/crabs/step2_Data_Iso_Mar11/')
+	methods.DataSample('WD_SingleEleA1', 459, directory=directory),
+	methods.DataSample('WD_SingleEleB', 459, directory=directory),
+	methods.DataSample('WD_SingleEleC1', 459, directory=directory),
+	methods.DataSample('WD_SingleEleC2', 459, directory=directory),
+	methods.DataSample('WD_SingleEleD', 459, directory=directory),
 ]
 
 # Set the cuts (all basic combinations of finalEle)
@@ -21,8 +32,9 @@ cuts = [
 	cuts.finalEle
 ]
 
-for cut in cuts:
-	print 'Cut:', str(cut)
-	for s in smpls:
-		print 'Events for %s:  %i'%(s.name, s.cacheEntryList(cut))
-	print
+for s in smpls:
+	testlog.info('Sample `%s`', s.name)
+	for cut in cuts:
+		testlog.info('> cut: `%s`', str(cut))
+		testlog.info('> > events: `%i`', s.cacheEntryList(cut))
+	testlog.info('')
