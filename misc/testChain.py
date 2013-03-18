e4353bfe531bdcb50bc1e27ad4c91db893f71a62
jpata <joosep.pata@gmail.com>
2013-03-18 08:37:14 +0200

    event list getting

--- misc/testChain.py
+++ misc/testChain.py
@@ -2,7 +2,11 @@ import ROOT
 
 chain = ROOT.TChain("tree")
 chain.Add("*.root")
-proof = ROOT.TProof("workers=2")
-
-chain.Draw(">>elist", "x>0.5")
+proof = ROOT.TProof.Open("workers=8")
+chain.SetProof(True)
+chain.Draw(">>elist", "x>0.5", "entrylist")
 elist = proof.GetOutputList().FindObject("elist")
+if not elist:
+    print "Error getting entry list: elist=" + str(elist)
+else:
+    print elist.GetN()
