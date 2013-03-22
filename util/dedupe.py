625b6687c4841087ea51ce71b4bb1941a62c5700
jpata <joosep.pata@gmail.com>
2013-03-22 12:43:03 +0200

    stdin-stdout usage

--- util/dedupe.py
+++ util/dedupe.py
@@ -1,19 +1,20 @@
+#!/usr/bin/python
 import re
 import sys
 import pdb
 pat = re.compile(".*_([0-9]+)_([0-9]+)_.*\.root")
-lines = open(sys.argv[1]).readlines()
+
+lines = sys.stdin.readlines()
+#lines = open(sys.argv[1]).readlines()
+lines = map(lambda x: x.strip(), lines)
 lines.sort()
 
 fileD = dict()
-print len(lines)
 for line in lines:
     mat = pat.match(line)
     jobN = mat.group(1)
     fileN = mat.group(2)
     fileD[jobN] = line
 
-ofile = open(sys.argv[1] + ".deduped", "w+")
 for v in sorted(fileD.values()):
-    ofile.write(v)
-ofile.close()
+    print v
