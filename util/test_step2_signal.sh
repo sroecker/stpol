589c853bbdd7c4b548bed05a4de1594f90fd88ab
jpata <joosep.pata@gmail.com>
2013-03-28 19:56:41 +0200

    debug mode

--- util/test_step2_signal.sh
+++ util/test_step2_signal.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-IN=$CMSSW_BASE/../fileLists/T_t.txt
+IN=$CMSSW_BASE/../fileLists_Step1/T_t.txt
 OFDIR=$CMSSW_BASE/../testing_step2_signal
 rm -Rf $OFDIR
 mkdir $OFDIR
 
-time cmsRun $CMSSW_BASE/../runconfs/step2_newCmdLine_cfg.py doDebug=False inputFiles_load=$IN isMC=True channel=signal subChannel=T_t outputFile=$OFDIR/out.root maxEvents=10000 &> $OFDIR/log.txt
+time cmsRun $CMSSW_BASE/../runconfs/step2_newCmdLine_cfg.py doDebug=True inputFiles_load=$IN isMC=True channel=signal subChannel=T_t outputFile=$OFDIR/out.root maxEvents=10000 &> $OFDIR/log.txt
 EX=$?
 echo "Exit code:"$EX 
 if [ "$EX" -ne 0 ]
