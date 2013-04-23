40fb5030c8070fd84677a0d7a6c70f99e8b9d649
jpata <joosep.pata@gmail.com>
2013-04-23 18:27:56 +0300

    added optional tag

--- analysis_step2/create_crab_jobs.sh
+++ analysis_step2/create_crab_jobs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-timestamp=`eval date +%m_%d`
+timestamp=`eval date +%m_%d`"_"$TAG
 
 python $CMSSW_BASE/../util/datasets.py -t stpol_step2_Iso -T $CMSSW_BASE/../crabs/crab_MC_step2_local_Iso.cfg -d S2_MC -o crabs/step2_MC_Iso_$timestamp
 python $CMSSW_BASE/../util/datasets.py -t stpol_step2_Iso -T $CMSSW_BASE/../crabs/crab_Data_step2_Iso.cfg -d S2_D -o crabs/step2_Data_Iso_$timestamp
