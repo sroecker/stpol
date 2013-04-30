a238b799bc86b8a850c92cd68688ff4299223bd9
jpata <joosep.pata@gmail.com>
2013-04-30 05:56:15 +0300

    step3 running changes

--- analysis_step3/make_trees.sh
+++ analysis_step3/make_trees.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-OFDIR=out_step3_04_29_2
+OFDIR=out_step3_04_29_3
 FLDIR=filelist_step2_04_25
 mkdir $OFDIR
 mkdir $OFDIR/iso
@@ -10,6 +10,6 @@ mkdir $OFDIR/anti-iso/mc
 mkdir $OFDIR/anti-iso/data
 
 STPOL_ISMC=false analysis_step3/suball.sh $OFDIR/iso/data $FLDIR/iso/data/*
-STPOL_ISMC=false analysis_step3/suball.sh $OFDIR/anti-iso/data $FLDIR/anti-iso/data/*
+#STPOL_ISMC=false analysis_step3/suball.sh $OFDIR/anti-iso/data $FLDIR/anti-iso/data/*
 STPOL_ISMC=true analysis_step3/suball.sh $OFDIR/iso/mc $FLDIR/iso/mc/*
-STPOL_ISMC=true analysis_step3/suball.sh $OFDIR/anti-iso/mc $FLDIR/anti-iso/mc/*
+#STPOL_ISMC=true analysis_step3/suball.sh $OFDIR/anti-iso/mc $FLDIR/anti-iso/mc/*
--- analysis_step3/run_step3_eventloop.sh
+++ analysis_step3/run_step3_eventloop.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e #Abort if errors
+uname -a
 
 WD=$CMSSW_BASE/..
 INFILE=$1
@@ -11,5 +13,10 @@ fi
 
 cd $WD
 source setenv.sh CMSSW_5_3_8
+ls /hdfs
 cd $OUTDIR
+#for f in `cat $INFILE`
+#do
+#    edmFileUtil $f
+#done
 time cat $INFILE | STPOL_STEP3_OUTPUTFILE="out_step3_"$OFNAME".root" $CMSSW_BASE/bin/slc5_amd64_gcc462/Step3_EventLoop $CMSSW_BASE/../runconfs/step3_eventLoop_cfg.py
