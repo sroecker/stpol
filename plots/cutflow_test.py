c59587eece2610c859535a2051381a5d9cb92884
Joosep Pata <joosep.pata@gmail.com>
2013-04-30 16:55:10 +0300

    use ordereddict

--- plots/cutflow_test.py
+++ plots/cutflow_test.py
@@ -1,11 +1,13 @@
 from project_histos import Cuts, filter_alnum, Sample
 from make_plots import plot_hists, canvas_margin, legend
 import argparse
+from collections import OrderedDict as dict
+
 if __name__=="__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--infile', type=str, required=True)
-    parser.add_argument('--normalize_lumi', action="store_true")
+    parser.add_argument('--normalize_lumi', action="store_true", default=True)
     args = parser.parse_args()
 
     #fname = "/Users/joosep/Documents/stpol/data/step3_trees_Mar28/T_t.root"
@@ -18,25 +20,35 @@ if __name__=="__main__":
 #            ev_ids[evid] = 1
 #        else:
 #            ev_ids[evid] += 1
-    sequential_cuts = [Cuts.hlt_isomu, Cuts.mt_mu, Cuts.n_jets(2), Cuts.n_tags(1), Cuts.eta_lj, Cuts.top_mass_sig]
-    cut_names = ["hlt_isomu", "MT_mu", "2J", "1T", "etalj", "Mtop"]
 
-    def makeSequences(cuts):
+    cuts = dict()
+    cuts["MT_mu"] = Cuts.mt_mu
+    cuts["2J"] = Cuts.n_jets(2)
+    cuts["1T"] = Cuts.n_tags(1)
+    #cuts["eta_lj"] = Cuts.eta_lj
+    cuts["rms_lj"] = Cuts.rms_lj
+    cuts["eta_jet"] = Cuts.eta_jet
+    cuts["top_mass_sig"] = Cuts.top_mass_sig
+
+    def makeSequences(cutsD):
+        cuts = cutsD.values()
         cut_sequences = [cuts[0]]
         for cut in cuts[1:]:
             new_sequence = cut_sequences[-1]*cut
             cut_sequences.append(new_sequence)
         return cut_sequences
 
-    sequences = makeSequences(sequential_cuts)
+    sequences = makeSequences(cuts)
 
     hists = []
-    for (cut, name) in zip(sequences, cut_names):
-        hist = sample.drawHistogram("cos_theta", cut.cut_str, plot_range=[40, -1, 1])
-
-        #normalize to 20/fb
+    for (cut, name) in zip(sequences, cuts.keys()):
+        hist = sample.drawHistogram("cos_theta", cut.cut_str,
+            plot_range=[40, -1, 1],
+            weight="muon_IDWeight*muon_IsoWeight*pu_weight*b_weight_nominal"
+        )
+        lumi = 12210
         if args.normalize_lumi:
-            hist.normalize_lumi(12210)
+            hist.normalize_lumi(lumi)
 
         hist.hist.SetName(name)
         hist.hist.SetTitle("%s: %.3E" % (name, hist.hist.Integral()))
@@ -47,7 +59,7 @@ if __name__=="__main__":
 
     title = "%s cos-theta cutflow" % sample.name
     if args.normalize_lumi:
-        title += " normalized to 20/fb"
+        title += " normalized to %.1f/fb" % (lumi/1000.0)
 
     canv = plot_hists(hists, do_log_y=False, title=title, x_label="cos #theta")
     leg = legend(hists, pos="top-right-small")
