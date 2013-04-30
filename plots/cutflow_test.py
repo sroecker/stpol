af34f38c948da16f1a09c6f33257ac5ec659b421
Joosep Pata <joosep.pata@gmail.com>
2013-04-30 05:53:17 +0300

    changed umi

--- plots/cutflow_test.py
+++ plots/cutflow_test.py
@@ -7,12 +7,19 @@ if __name__=="__main__":
     parser.add_argument('-i', '--infile', type=str, required=True)
     parser.add_argument('--normalize_lumi', action="store_true")
     args = parser.parse_args()
-    
+
     #fname = "/Users/joosep/Documents/stpol/data/step3_trees_Mar28/T_t.root"
     sample = Sample.fromFile(args.infile)
 
-    sequential_cuts = [Cuts.mt_mu, Cuts.n_jets(2), Cuts.n_tags(1), Cuts.eta_lj, Cuts.top_mass_sig]
-    cut_names = ["MT_mu", "2J", "1T", "etalj", "Mtop"]
+#    ev_ids = dict()
+#    for ev in sample.tree:
+#        evid = int(ev.event_id)
+#        if evid not in ev_ids.keys():
+#            ev_ids[evid] = 1
+#        else:
+#            ev_ids[evid] += 1
+    sequential_cuts = [Cuts.hlt_isomu, Cuts.mt_mu, Cuts.n_jets(2), Cuts.n_tags(1), Cuts.eta_lj, Cuts.top_mass_sig]
+    cut_names = ["hlt_isomu", "MT_mu", "2J", "1T", "etalj", "Mtop"]
 
     def makeSequences(cuts):
         cut_sequences = [cuts[0]]
@@ -26,22 +33,22 @@ if __name__=="__main__":
     hists = []
     for (cut, name) in zip(sequences, cut_names):
         hist = sample.drawHistogram("cos_theta", cut.cut_str, plot_range=[40, -1, 1])
-        
+
         #normalize to 20/fb
         if args.normalize_lumi:
-            hist.normalize_lumi(20000)
-        
+            hist.normalize_lumi(12210)
+
         hist.hist.SetName(name)
         hist.hist.SetTitle("%s: %.3E" % (name, hist.hist.Integral()))
         hist.update()
-        
+
         hists.append(hist)
-        print "%s: %.3E" % (name, hist.hist.Integral())
+        print "%s: %d" % (name, hist.hist.Integral())
 
     title = "%s cos-theta cutflow" % sample.name
     if args.normalize_lumi:
         title += " normalized to 20/fb"
-        
+
     canv = plot_hists(hists, do_log_y=False, title=title, x_label="cos #theta")
     leg = legend(hists, pos="top-right-small")
     canv.SaveAs("cutflow_%s.pdf" % sample.name)
