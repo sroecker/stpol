96b4b556196cd8dcc34a59d4c7ebaea2be90d40e
Joosep Pata <joosep.pata@gmail.com>
2013-05-03 17:12:25 +0300

    syntax

--- plots/common/pretty_names.py
+++ plots/common/pretty_names.py
@@ -11,10 +11,10 @@ sample_names = dict()
 for (name, items) in merge_cmds.items():
     for i in items:
         sample_names[i] = name
-        
+
 variable_names = dict()
 variable_names["eta_lj"] = "#eta_{lj.}"
-variable_names["abs_eta_lj" = "|#eta_{lj.}|"
+variable_names["abs_eta_lj"] = "|#eta_{lj.}|"
 
 variable_units = dict()
 variable_units["eta_lj"] = "GeV"
