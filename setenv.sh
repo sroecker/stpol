351d5cddd2f926eedc5a7d4a89aa0e1045982162
Joosep Pata <joosep.pata@gmail.com>
2013-05-03 15:50:46 +0300

    add plotting dir to path

--- setenv.sh
+++ setenv.sh
@@ -19,7 +19,7 @@ eval `scramv1 runtime -sh`
 
 # Add plotfw to python library path
 #PYTHONPATH=$PYTHONPATH:${SCRIPT_DIR}/newplots
-PYTHONPATH=$PYTHONPATH:`readlink -f runconfs`
+PYTHONPATH=$PYTHONPATH:`readlink -f runconfs`:$SCRIPT_DIR/plots
 
 # Return to original directory
 cd $CURRENT_DIR
