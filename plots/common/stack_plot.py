fb108ec55d09cdb449b5de7bdf54cac461aae1bd
Joosep Pata <joosep.pata@gmail.com>
2013-05-03 15:34:04 +0300

    add stack plot

---
+++ plots/common/stack_plot.py
@@ -0,0 +1,147 @@
+import ROOT
+
+#Need to import ordereddict from python 2.7
+from collections import OrderedDict
+
+def plot_hists_stacked(canv, hist_groups, **kwargs):
+    """
+    ***Mandatory arguments
+    canv - a TCanvas instance to use
+    hist_groups - a dictionary with the list of histograms to use as different stacks.
+                For example:
+                {
+                "data": [data_hist],
+                "mc:" [mc_hist1, mc_hist2, ...]
+                }
+                Each key will become a different stack that is compared side-by-side.
+                Each list will be inside the stack on top of each other in the order specified.
+    ***Optional arguments
+    draw_styles - a dictionary with the stack names and the corresponding TH1::Draw styles
+    title - plot title (set on the first stack)
+    x_label - label of the x axis
+    y_label - label of the y axis
+    do_log_y - True/False to set log scale on y axis
+    min_bin - the lower cutoff on the y axis, which may be necessary for log y scale
+    
+    ***returns
+    a dictionary with the instances of the drawn TStacks.
+    """
+    
+    stacks = OrderedDict()
+    
+    draw_styles = kwargs.get("draw_styles", {"data": "E1"})
+    
+    do_log_y = kwargs.get("do_log_y", False)
+    
+    title = kwargs.get("title", "title")
+    
+    x_label = kwargs.get("x_label", "x_label")
+    y_label = kwargs.get("y_label", "y_label")
+    
+    min_bin = kwargs.get("min_bin", 10)
+
+    
+    for name, group in hist_groups.items():
+        stacks[name] = ROOT.THStack(name, name)
+        for hist in group:
+            stacks[name].Add(hist)
+
+    max_bin = max([st.GetMaximum() for st in stacks.values()])
+
+    #Need to draw the stacks one time to initialize everything
+    for name, stack in stacks.items():
+        stack.Draw()
+    canv.Draw()
+
+    #Now draw really
+    first = True
+    for name, stack in stacks.items():
+        if name in draw_styles.keys():
+            drawcmd = draw_styles[name]
+        else:
+            drawcmd = "BAR HIST goff" #default stack style
+        if not first:
+            drawcmd += " SAME"
+        stack.Draw(drawcmd)
+
+        #Set the plot style on the first stack
+        if first:
+            stack.SetMaximum(1.8*max_bin)
+            if do_log_y:
+                stack.SetMinimum(min_bin)
+            stack.SetTitle(title)
+            stack.GetXaxis().SetTitle(x_label)
+            stack.GetYaxis().SetTitle(y_label)
+
+        first = False
+    if do_log_y:
+        canv.SetLogy()
+    return stacks
+
+
+if __name__=="__main__":
+    import ROOT
+    import random
+    import tdrstyle
+    tdrstyle.tdrstyle()
+
+    #Make some test data histograms
+    h_d1 = ROOT.TH1F("h_d1", "data1", 20, -5, 5)
+    for i in range(1000):
+        h_d1.Fill(random.normalvariate(0, 1))
+
+    h_d2 = ROOT.TH1F("h_d2", "data2", 20, -5, 5)
+    for i in range(10000):
+        h_d2.Fill(random.normalvariate(0, 0.8))
+    
+    #Merge the different data samples
+    h_d1.Add(h_d2)
+    h_d1.SetTitle("data")
+
+    #Make some test MC histograms
+    h_mc1 = ROOT.TH1F("h_mc1", "TTJets", 20, -5, 5)
+    for i in range(1500):
+        h_mc1.Fill(random.normalvariate(0, 1))
+
+    h_mc2 = ROOT.TH1F("h_mc2", "t-channel", 20, -5, 5)
+    for i in range(900):
+        h_mc2.Fill(random.normalvariate(0, 0.9))
+
+    h_mc3 = ROOT.TH1F("h_mc3", "W+Jets", 20, -5, 5)
+    for i in range(5000):
+        h_mc3.Fill(random.normalvariate(0,  1.2))
+
+    #Set the histogram styles and colors
+    from sample_style import Styling
+    Styling.mc_style(h_mc1, "TTJets_FullLept")
+    Styling.mc_style(h_mc2, "T_t")
+    Styling.mc_style(h_mc3, "W1Jets_exclusive")
+    Styling.data_style(h_d1)
+
+    #Create the canvas
+    canv = ROOT.TCanvas("c", "c")
+    canv.SetWindowSize(500, 500)
+    canv.SetCanvasSize(600, 600)
+
+    #!!!!LOOK HERE!!!!!
+    #----
+    #Draw the stacked histograms
+    #----
+    stacks_d = OrderedDict() #<<< need to use OrderedDict to have data drawn last (dict does not preserve order)
+    stacks_d["mc"] = [h_mc1, h_mc2, h_mc3] # <<< order is important here, mc1 is bottom-most
+    stacks_d["data"] = [h_d1]
+    stacks = plot_hists_stacked(
+        canv,
+        stacks_d,
+        x_label="variable x [GeV]",
+        y_label="",
+        do_log_y=False
+    )
+
+    #Draw the legend
+    import legend
+    leg = legend.legend(
+        [h_d1, h_mc3, h_mc2, h_mc1], # <<< need to reverse MC order here
+        styles=["p", "f"],
+        width=0.2
+    )
