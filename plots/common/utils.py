3dd03d333ab3c9d151b4172a1d00cf3accfe3019
Liis Rebane <polaarrebane@gmail.com>
2013-05-06 18:00:09 +0300

    change coordinates of text, change order of samples

--- plots/common/utils.py
+++ plots/common/utils.py
@@ -3,16 +3,16 @@ from odict import OrderedDict as dict
 
 #Here the latter items will become topmost in stacks
 merge_cmds = dict()
-merge_cmds["single #mu"] = ["SingleMuAB", "SingleMuC", "SingleMuD"]
+merge_cmds["data"] = ["SingleMuAB", "SingleMuC", "SingleMuD"]
 merge_cmds["diboson"] = ["WW", "WZ", "ZZ"]
-merge_cmds["DY-jets"] = ["DYJets"]
-merge_cmds["s-channel"] = ["T_s", "Tbar_s"]
-merge_cmds["tW-channel"] = ["T_tW", "Tbar_tW"]
-merge_cmds["t#bar{t}"] = ["TTJets_FullLept", "TTJets_SemiLept"]
 merge_cmds["W(#rightarrow l #nu) + jets"] = ["W1Jets_exclusive", "W2Jets_exclusive", "W3Jets_exclusive", "W4Jets_exclusive"]
+merge_cmds["DY-jets"] = ["DYJets"]
+merge_cmds["t#bar{t}"] = ["TTJets_FullLept", "TTJets_SemiLept"]
+merge_cmds["tW-channel"] = ["T_tW", "Tbar_tW"]
+merge_cmds["s-channel"] = ["T_s", "Tbar_s"]
 merge_cmds["t-channel"] = ["T_t", "Tbar_t"]
 
-def lumi_textbox(lumi, pos="top-center"):
+def lumi_textbox(lumi, pos="top-left"):
     """
     This method creates and draws the "CMS Preliminary" luminosity box,
     displaying the lumi in 1/fb and the COM energy.
@@ -26,8 +26,11 @@ def lumi_textbox(lumi, pos="top-center"):
     **returns:
     A TPaveText instance with the lumi information
     """
-    if pos=="top-center":
+    if pos=="top-left":
         coords = [0.2, 0.86, 0.66, 0.91]
+    if pos=="top-right":
+        coords = [0.5, 0.86, 0.96, 0.91]
+
     text = ROOT.TPaveText(coords[0], coords[1], coords[2], coords[3], "NDC")
     text.AddText("CMS preliminary #sqrt{s} = 8 TeV, #int L dt = %.1f fb^{-1}" % (float(lumi)/1000.0))
     text.SetShadowColor(ROOT.kWhite)
