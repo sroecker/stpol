e6497cb969716445d144385e7651afcde91be480
jpata <joosep.pata@gmail.com>
2013-05-06 17:36:04 +0300

    added wjets incl

--- plots/b_eff_calcs.py
+++ plots/b_eff_calcs.py
@@ -10,24 +10,22 @@ def getBTaggingEff(sample, flavour, cut):
 
     N_tagged = numpy.sum(sample.getColumn("true_%s_tagged_count" % flavour, cut))
     N = numpy.sum(sample.getColumn("true_%s_count" % flavour, cut))
-    print N_tagged,N
     return N_tagged/N
 
 if __name__=="__main__":
 
-    sample_dir = "out_step3_05_03_17_42/iso/mc/"
+    sample_dir = "out_step3_05_06_15_53/iso/mc/"
     samples = []
-    samples.append(project_histos.Sample.fromFile(sample_dir + "TTJets_FullLept.root"))
+    #samples.append(project_histos.Sample.fromFile(sample_dir + "TTJets_FullLept.root"))
     samples.append(project_histos.Sample.fromFile(sample_dir + "TTJets_MassiveBinDECAY.root"))
-    samples.append(project_histos.Sample.fromFile(sample_dir + "TTJets_SemiLept.root"))
+    #samples.append(project_histos.Sample.fromFile(sample_dir + "TTJets_SemiLept.root"))
     samples.append(project_histos.Sample.fromFile(sample_dir + "T_t.root"))
-    #samples.append(project_histos.Sample.fromFile(sample_dir + "WJets_inclusive.root"))
+    samples.append(project_histos.Sample.fromFile(sample_dir + "WJets_inclusive.root"))
     logging.info("Doing b-tagging efficiency calculations")
-    cuts = [Cuts.hlt_isomu * Cuts.n_jets(2) * Cuts.eta_lj * Cuts.top_mass_sig]
+    cuts = [Cuts.hlt_isomu * Cuts.n_jets(3) * Cuts.eta_lj * Cuts.top_mass_sig]
 #    cuts.append("n_jets==2 && !(top_mass>130 && top_mass<220) && mt_mu>50")
 #    cuts.append("n_jets==3 && !(top_mass>130 && top_mass<220) && mt_mu>50")
     for cut in cuts:
-        print cut
         for sample in samples:
             for flavour in ["b", "c", "l"]:
                 entries = sample.getEntries(str(cut))
