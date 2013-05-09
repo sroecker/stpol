49d1b3ac01bd37158a89976e2e304df72a62758c
jpata <joosep.pata@gmail.com>
2013-05-09 22:54:26 +0300

    print out column info

--- plots/b_eff_calcs.py
+++ plots/b_eff_calcs.py
@@ -3,13 +3,20 @@ import project_histos
 from project_histos import Cuts
 import logging
 import numpy
+import pdb
 
 def getBTaggingEff(sample, flavour, cut):
     if flavour not in ["b", "c", "l"]:
         raise ValueError("Flavour must be b, c or l")
 
-    N_tagged = numpy.sum(sample.getColumn("true_%s_tagged_count" % flavour, cut))
-    N = numpy.sum(sample.getColumn("true_%s_count" % flavour, cut))
+    colA = sample.getColumn("true_%s_tagged_count" % flavour, cut)
+    colB = sample.getColumn("true_%s_count" % flavour, cut)
+    print min(colA), min(colB), max(colA), max(colB)
+
+    pdb.set_trace()
+    N_tagged = numpy.sum(colA)
+    N = numpy.sum(colB)
+
     print N_tagged, N
     return N_tagged/N
 
@@ -24,7 +31,8 @@ if __name__=="__main__":
     #samples.append(project_histos.Sample.fromFile(sample_dir + "T_t.root"))
     #samples.append(project_histos.Sample.fromFile(sample_dir + "WJets_inclusive.root"))
     logging.info("Doing b-tagging efficiency calculations")
-    cuts = [Cuts.mu * Cuts.n_jets(2) * Cuts.eta_lj * Cuts.top_mass_sig * Cuts.mt_mu]
+    cuts = [Cuts.mu * Cuts.n_jets(2) * Cuts.eta_lj * Cuts.mt_mu * Cuts.top_mass_sig]
+#    cuts = [Cuts.mu]
 #    cuts.append("n_jets==2 && !(top_mass>130 && top_mass<220) && mt_mu>50")
 #    cuts.append("n_jets==3 && !(top_mass>130 && top_mass<220) && mt_mu>50")
     for cut in cuts:
