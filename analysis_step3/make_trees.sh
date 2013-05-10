61cb48084b930175d4aedaa5c3b7174c410db251
jpata <joosep.pata@gmail.com>
2013-05-10 16:00:29 +0300

    need to run antiiso mc

--- analysis_step3/make_trees.sh
+++ analysis_step3/make_trees.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 timestamp=`eval date +%m_%d_%H_%M`
 OFDIR=out_step3_$timestamp
-FLDIR=filelist_step2_05_01
+FLDIR=filelist_step2_05_10
 mkdir $OFDIR
 mkdir $OFDIR/iso
 mkdir $OFDIR/iso/mc
@@ -13,4 +13,4 @@ mkdir $OFDIR/anti-iso/data
 STPOL_ISMC=false analysis_step3/suball.sh $OFDIR/iso/data $FLDIR/iso/data/*
 STPOL_ISMC=false analysis_step3/suball.sh $OFDIR/anti-iso/data $FLDIR/anti-iso/data/*
 STPOL_ISMC=true analysis_step3/suball.sh $OFDIR/iso/mc $FLDIR/iso/mc/*
-#STPOL_ISMC=true analysis_step3/suball.sh $OFDIR/anti-iso/mc $FLDIR/anti-iso/mc/*
+STPOL_ISMC=true analysis_step3/suball.sh $OFDIR/anti-iso/mc $FLDIR/anti-iso/mc/*
