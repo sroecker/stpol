7b0fef1c706176347719acf568bd405a386de2cf
jpata <joosep.pata@gmail.com>
2013-05-15 00:24:33 +0300

    use git HEAD as tag

--- analysis_step2/create_crab_jobs.sh
+++ analysis_step2/create_crab_jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-timestamp=`eval date +%m_%d`"_"$TAG
 TAG=`git rev-parse HEAD`
+timestamp=`eval date +%m_%d`"_"$TAG
 python $STPOL_DIR/util/datasets.py -t stpol_step2_Iso_$TAG -T $STPOL_DIR/crabs/crab_MC_step2_local_Iso.cfg -d S2_MC -o crabs/step2_MC_Iso_$timestamp
 python $STPOL_DIR/util/datasets.py -t stpol_step2_Iso_$TAG -T $STPOL_DIR/crabs/crab_Data_step2_Iso.cfg -d S2_D -o crabs/step2_Data_Iso_$timestamp
 
