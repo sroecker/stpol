3c10b8600fe1e1820828790f8c4d876a935d361d
jpata <joosep.pata@gmail.com>
2013-05-15 13:15:28 +0300

    add git commit number as tag

--- analysis_step1/create_crab_jobs.sh
+++ analysis_step1/create_crab_jobs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-timestamp=`eval date +%m_%d`
+TAG=`git rev-parse HEAD`
+timestamp=`eval date +%m_%d`_$TAG
 
 python $CMSSW_BASE/../util/datasets.py -t stpol_step1_$timestamp -T $CMSSW_BASE/../crabs/crab_MC_step1.cfg -d S1_MC -o crabs/step1_MC_$timestamp
 python $CMSSW_BASE/../util/datasets.py -t stpol_step1_$timestamp -T $CMSSW_BASE/../crabs/crab_MC_step1_glideInRemote.cfg -d S1_MC_syst -o crabs/step1_MC_syst_$timestamp
