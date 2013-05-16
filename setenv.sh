583a55926cbc3a649facea346e191835e69aa3a8
Joosep Pata <joosep.pata@gmail.com>
2013-05-16 20:07:06 +0300

    changed order, remoed runconfs2

--- setenv.sh
+++ setenv.sh
@@ -12,18 +12,19 @@ if [ -z "$CMSSW_DIR" ]; then CMSSW_DIR="CMSSW_5_3_7_patch4"; fi
 export STPOL_DIR=$SCRIPT_DIR
 export local_dbs_instance=cms_dbs_ph_analysis_02
 export local_dbs_url=https://cmsdbsprod.cern.ch:8443/cms_dbs_ph_analysis_02_writer/servlet/DBSServlet
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-cd ${SCRIPT_DIR}/$CMSSW_DIR
-#cmsenv
-eval `scramv1 runtime -sh`
 
 # Add plotfw to python library path
 #PYTHONPATH=$PYTHONPATH:${SCRIPT_DIR}/newplots
-PYTHONPATH=$PYTHONPATH:`readlink -f runconfs`:$STPOL_DIR:$STPOL_DIR/plots:$STPOL_DIR/local/lib/python2.6/site-packages/
+#PYTHONPATH=$PYTHONPATH:`readlink -f runconfs`:$STPOL_DIR/:$STPOL_DIR/plots/:$STPOL_DIR/local/lib/python2.6/site-packages/
+PYTHONPATH=$PYTHONPATH:$STPOL_DIR/:$STPOL_DIR/plots/:$STPOL_DIR/local/lib/python2.6/site-packages/
 
 #Add QCD estimation stuff
-PYTHONPATH=$PYTHONPATH:$STPOL_DIR/qcd_estimation
+PYTHONPATH=$PYTHONPATH:$STPOL_DIR/qcd_estimation/
 PYTHONPATH=$PYTHONPATH:"/home/andres/theta_testing/utils2/theta_auto"
+
+cd ${SCRIPT_DIR}/$CMSSW_DIR
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+eval `scramv1 runtime -sh`
 
 # Return to original directory
 cd $CURRENT_DIR
