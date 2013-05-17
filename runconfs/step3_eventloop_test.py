c149433eff55b3c4c240eb95bb07cdb81f1228f8
jpata <joosep.pata@gmail.com>
2013-05-17 20:57:29 +0300

    run on more events

--- runconfs/step3_eventloop_test.py
+++ runconfs/step3_eventloop_test.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 from runconfs.step3_eventLoop_cfg import *
-process.fwliteInput.maxEvents = cms.int32(20000)
+process.fwliteInput.maxEvents = cms.int32(50000)
 process.muonCuts.requireOneMuon = cms.bool(False)
 process.muonCuts.requireOneElectron = cms.bool(False)
