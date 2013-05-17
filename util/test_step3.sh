521cedf7b4c7878cbba84dd722c07aa3a02815d8
jpata <joosep.pata@gmail.com>
2013-05-17 19:45:59 +0300

    added step3 testing

---
+++ util/test_step3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Running step3 test"
+cd "$STPOL_DIR"
+source setenv.sh CMSSW_5_3_8
+OFDIR="$STPOL_DIR"/test_step3
+if [ -d "$OFDIR" ]; then
+    echo "removing '"$OFDIR"'"
+    rm -Rf "$OFDIR"
+fi
+mkdir "$OFDIR"
+echo "Calling Step3_EventLoop"
+cat "$STPOL_DIR"/filelist_step2_latest/iso/mc/TTJets_MassiveBinDECAY.txt | STPOL_STEP3_OUTPUTFILE="$OFDIR"/out.root "$CMSSW_BASE"/bin/"$SCRAM_ARCH"/Step3_EventLoop "$STPOL_DIR"/runconfs/step3_eventloop_test.py &> "$OFDIR"/log_step3.txt
+tail -n10 "$OFDIR"/log_step3.txt
