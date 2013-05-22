7a9c8df5575a86368d22bab08bacc224d113910d
Joosep Pata <joosep.pata@gmail.com>
2013-05-22 20:55:28 +0300

    made local server starting more reliable

--- misc/start_nbserver_local.sh
+++ misc/start_nbserver_local.sh
@@ -1,3 +1,6 @@
-ipcluster start --n=16 &
-ipython notebook --profile=nbserver
+ipcontroller&
+ipcluster engines&
+ipython notebook --pylab=inline
+#ipcluster start --n=16 &
+#ipython notebook --profile=nbserver
 
