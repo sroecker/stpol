e5c78e71a56167bf2a96f39f99bba776bef4fd9d
jpata <joosep.pata@gmail.com>
2013-05-23 10:24:32 +0300

    sync running in step3

--- sync/run_step1.sh
+++ sync/run_step1.sh
@@ -4,3 +4,4 @@ echo "Inclusive sample"
 cmsRun $STPOL_DIR/runconfs/step1_newCmdLine_cfg.py doSkimming=False inputFiles_load=$STPOL_DIR/sync/inclusive/files.txt outputFile=$STPOL_DIR/sync/inclusive/step1.root &> $STPOL_DIR/sync/inclusive/log_step1.txt
 echo "Exclusive sample"
 cmsRun $STPOL_DIR/runconfs/step1_newCmdLine_cfg.py doSkimming=False inputFiles_load=$STPOL_DIR/sync/exclusive/files.txt outputFile=$STPOL_DIR/sync/exclusive/step1.root &> $STPOL_DIR/sync/exclusive/log_step1.txt
+echo "step1 done"
--- sync/run_step3.sh
+++ sync/run_step3.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo "step3 sync"
+
+echo "inclusive/step2.root" | $STPOL_DIR/CMSSW_5_3_8/bin/slc5_amd64_gcc462/Step3_EventLoop $STPOL_DIR/runconfs/step3_eventloop_sync.py --outputFile=$STPOL_DIR/sync/inclusive/step3.root
+echo "exclusive/step2.root" | $STPOL_DIR/CMSSW_5_3_8/bin/slc5_amd64_gcc462/Step3_EventLoop $STPOL_DIR/runconfs/step3_eventloop_sync.py --outputFile=$STPOL_DIR/sync/exclusive/step3.root
