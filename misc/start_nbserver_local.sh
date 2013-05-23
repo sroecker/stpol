a397b5cbbac5ba49bf0949d0b8b68484c193c2ee
Joosep Pata <joosep.pata@gmail.com>
2013-05-23 10:25:21 +0300

    logging

--- misc/start_nbserver_local.sh
+++ misc/start_nbserver_local.sh
@@ -1,5 +1,5 @@
-ipcontroller&
-ipcluster engines&
+ipcontroller --log-to-file &> /dev/null &
+ipcluster engines --log-to-file &> /dev/null &
 ipython notebook --pylab=inline
 #ipcluster start --n=16 &
 #ipython notebook --profile=nbserver
