a4c9f7509df661dd6042c4d71b1fbdba085e712c
jpata <joosep.pata@gmail.com>
2013-05-26 19:58:53 +0300

    do control vars by default

--- analysis_step3/make_trees.sh
+++ analysis_step3/make_trees.sh
@@ -6,7 +6,7 @@ mkdir -p $OFDIR/mu
 mkdir -p $OFDIR/mu/iso/nominal
 mkdir -p $OFDIR/mu/antiiso/nominal
 
-$STPOL_DIR/analysis_step3/suball.sh "--mtw " $OFDIR/mu/iso/nominal $FLDIR/iso/nominal/data/*
+$STPOL_DIR/analysis_step3/suball.sh " --mtw " $OFDIR/mu/iso/nominal $FLDIR/iso/nominal/data/*
 $STPOL_DIR/analysis_step3/suball.sh "--mtw " $OFDIR/mu/antiiso/nominal $FLDIR/antiiso/nominal/data/*
-$STPOL_DIR/analysis_step3/suball.sh "--mtw --isMC" $OFDIR/mu/iso/nominal $FLDIR/iso/nominal/mc/*
-$STPOL_DIR/analysis_step3/suball.sh "--mtw --isMC" $OFDIR/mu/antiiso/nominal $FLDIR/antiiso/nominal/mc/*
+$STPOL_DIR/analysis_step3/suball.sh "--doControlVars --mtw --isMC" $OFDIR/mu/iso/nominal $FLDIR/iso/nominal/mc/*
+$STPOL_DIR/analysis_step3/suball.sh "--doControlVars --mtw --isMC" $OFDIR/mu/antiiso/nominal $FLDIR/antiiso/nominal/mc/*
