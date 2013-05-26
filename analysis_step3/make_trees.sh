220a87390f5d7dbab3dd54161a29ae75c7336b98
jpata <joosep.pata@gmail.com>
2013-05-26 17:16:57 +0300

    set mtw cut

--- analysis_step3/make_trees.sh
+++ analysis_step3/make_trees.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 timestamp=`eval date +%m_%d_%H_%M`
 OFDIR=out_step3_$timestamp
-FLDIR=filelist_step2_05_10
-mkdir -p $OFDIR
-mkdir -p $OFDIR/iso/nominal
-mkdir -p $OFDIR/antiiso/nominal
+FLDIR=filelist_step2_latest
+mkdir -p $OFDIR/mu
+mkdir -p $OFDIR/mu/iso/nominal
+mkdir -p $OFDIR/mu/antiiso/nominal
 
-$STPOL_DIR/analysis_step3/suball.sh " " $OFDIR/iso/nominal $FLDIR/iso/data/*
-$STPOL_DIR/analysis_step3/suball.sh " " $OFDIR/antiiso/nominal $FLDIR/antiiso/data/*
-$STPOL_DIR/analysis_step3/suball.sh "--isMC" $OFDIR/iso/nominal $FLDIR/iso/mc/*
-$STPOL_DIR/analysis_step3/suball.sh "--isMC" $OFDIR/antiiso/nominal $FLDIR/antiiso/mc/*
+$STPOL_DIR/analysis_step3/suball.sh "--mtw " $OFDIR/mu/iso/nominal $FLDIR/iso/nominal/data/*
+$STPOL_DIR/analysis_step3/suball.sh "--mtw " $OFDIR/mu/antiiso/nominal $FLDIR/antiiso/nominal/data/*
+$STPOL_DIR/analysis_step3/suball.sh "--mtw --isMC" $OFDIR/mu/iso/nominal $FLDIR/iso/nominal/mc/*
+$STPOL_DIR/analysis_step3/suball.sh "--mtw --isMC" $OFDIR/mu/antiiso/nominal $FLDIR/antiiso/nominal/mc/*
