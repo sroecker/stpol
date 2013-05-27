6fccab04dfa2af18be23e0de690ae945f69e1893
jpata <joosep.pata@gmail.com>
2013-05-27 10:11:39 +0300

    added ele

--- analysis_step3/make_trees.sh
+++ analysis_step3/make_trees.sh
@@ -5,8 +5,19 @@ FLDIR=filelist_step2_latest
 mkdir -p $OFDIR/mu
 mkdir -p $OFDIR/mu/iso/nominal
 mkdir -p $OFDIR/mu/antiiso/nominal
+mkdir -p $OFDIR/ele
+mkdir -p $OFDIR/ele/iso/nominal
+mkdir -p $OFDIR/ele/antiiso/nominal
 
-$STPOL_DIR/analysis_step3/suball.sh " --mtw " $OFDIR/mu/iso/nominal $FLDIR/iso/nominal/data/*
-$STPOL_DIR/analysis_step3/suball.sh "--mtw " $OFDIR/mu/antiiso/nominal $FLDIR/antiiso/nominal/data/*
-$STPOL_DIR/analysis_step3/suball.sh "--doControlVars --mtw --isMC" $OFDIR/mu/iso/nominal $FLDIR/iso/nominal/mc/*
-$STPOL_DIR/analysis_step3/suball.sh "--doControlVars --mtw --isMC" $OFDIR/mu/antiiso/nominal $FLDIR/antiiso/nominal/mc/*
+$STPOL_DIR/analysis_step3/suball.sh "--lepton=mu" $OFDIR/mu/iso/nominal $FLDIR/iso/nominal/data/*
+$STPOL_DIR/analysis_step3/suball.sh "--lepton=mu" $OFDIR/mu/antiiso/nominal $FLDIR/antiiso/nominal/data/*
+$STPOL_DIR/analysis_step3/suball.sh "--lepton=mu --doControlVars --isMC" $OFDIR/mu/iso/nominal $FLDIR/iso/nominal/mc/*
+$STPOL_DIR/analysis_step3/suball.sh "--lepton=mu --doControlVars --isMC" $OFDIR/mu/antiiso/nominal $FLDIR/antiiso/nominal/mc/*
+
+$STPOL_DIR/analysis_step3/suball.sh "--lepton=ele" $OFDIR/ele/iso/nominal $FLDIR/iso/nominal/data/*
+$STPOL_DIR/analysis_step3/suball.sh "--lepton=ele" $OFDIR/ele/antiiso/nominal $FLDIR/antiiso/nominal/data/*
+$STPOL_DIR/analysis_step3/suball.sh "--lepton=ele --doControlVars --isMC" $OFDIR/ele/iso/nominal $FLDIR/iso/nominal/mc/*
+$STPOL_DIR/analysis_step3/suball.sh "--lepton=ele --doControlVars --isMC" $OFDIR/ele/antiiso/nominal $FLDIR/antiiso/nominal/mc/*
+
+
+
