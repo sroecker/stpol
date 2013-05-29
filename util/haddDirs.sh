cc435e8c7279fb827e05a6c3c968db7938348729
jpata <joosep.pata@gmail.com>
2013-05-29 14:52:10 +0300

    some comments

--- util/haddDirs.sh
+++ util/haddDirs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#This script will take the out_step3 folder and generate merged the trees from all the subdirectories
+#Use as haddDirs.sh /path/to/out_step3/
 
 files=`find $1 -name "*.root" -exec dirname {} \; | sort | uniq`
 
