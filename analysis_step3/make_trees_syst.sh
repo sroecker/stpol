295c88db1e8df26e531968c4360253d667e01a5e
andresti <andres.tiko@cern.ch>
2013-06-01 18:19:34 +0300

    Step3 systematics

--- analysis_step3/make_trees_syst.sh
+++ analysis_step3/make_trees_syst.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 timestamp=`eval date +%m_%d`
 OFDIR=out_step3_$timestamp
-FILELIST=filelist_step2_systematics_28_05
+FILELIST_SYST=filelist_step2_systematics_28_05
+FILELIST=filelist_step2_latest
 
 mkdir $OFDIR
 mkdir $OFDIR/iso
 mkdir $OFDIR/antiiso
+mkdir $OFDIR/iso/SYST
+
+
+$STPOL_DIR/analysis_step3/suball_syst.sh "" $OFDIR/iso/Nominal $FILELIST/iso/nominal/data/SingleMu*
+$STPOL_DIR/analysis_step3/suball_syst.sh "--isAntiIso" $OFDIR/antiiso/Nominal $FILELIST/antiiso/nominal/data/SingleMu*
+$STPOL_DIR/analysis_step3/suball_syst.sh "" $OFDIR/iso/SYST $FILELIST_SYST/Iso/SYST/*
+
+#Just in case, to avoid overload...    
+sleep 300
 
 for SYST in "Nominal" "ResUp" "ResDown" "UnclusteredEnUp" "UnclusteredEnDown"
 do
     mkdir $OFDIR/iso/$SYST
-    analysis_step3/suball_syst.sh "" $OFDIR/iso/$SYST $FILELIST/Iso/$SYST/*
+    $STPOL_DIR/analysis_step3/suball_syst.sh "" $OFDIR/iso/$SYST $FILELIST_SYST/Iso/$SYST/*
 
     #Just in case, to avoid overload...    
     sleep 300
@@ -20,7 +30,8 @@ done
 for SYST in "EnUp" "EnDown" "Nominal" "ResUp" "ResDown" "UnclusteredEnUp" "UnclusteredEnDown"
 do
     mkdir $OFDIR/antiiso/$SYST
-    analysis_step3/suball_syst.sh "--isAntiIso" $OFDIR/antiiso/$SYST $FILELIST/antiIso/$SYST/*
+    $STPOL_DIR/analysis_step3/suball_syst.sh "--isAntiIso" $OFDIR/antiiso/$SYST $FILELIST_SYST/antiIso/$SYST/*
 
+    #Just in case, to avoid overload...    
     sleep 300
 done
--- analysis_step3/slurm_sub_step3.sh
+++ analysis_step3/slurm_sub_step3.sh
@@ -21,7 +21,7 @@ split $INFILE -a4 -l 50 -d
 for file in x*
 do
     echo "Submitting step3 job $CONF on file $file"
-    until sbatch -x comp-d-[006,033,094] -p prio $STPOL_DIR/analysis_step3/run_step3_eventloop.sh `readlink -f $file` $OUTDIR $CONF
+    until sbatch -x comp-d-[006,033,094] -p main $STPOL_DIR/analysis_step3/run_step3_eventloop.sh `readlink -f $file` $OUTDIR $CONF
     do 
         echo "ERROR!: could not submit slurm job on file $file, retrying after sleep"
         sleep 20
