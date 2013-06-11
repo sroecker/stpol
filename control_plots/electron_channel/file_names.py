1e23ab7d6da7bb35f3fec4253cf8bff05de3ef14
Liis Rebane <polaarrebane@gmail.com>
2013-06-11 14:55:34 +0300

    update file_names with separate ele and mu

--- control_plots/electron_channel/file_names.py
+++ control_plots/electron_channel/file_names.py
@@ -1,6 +1,6 @@
 mcFiles = {
-    "T_t": "T_t.root",
-    "Tbar_t": "Tbar_t.root",
+#    "T_t": "T_t.root",
+#    "Tbar_t": "Tbar_t.root",
     "T_t_ToLeptons": "T_t_ToLeptons.root",
     "Tbar_t_ToLeptons": "Tbar_t_ToLeptons.root",
     "T_tW": "T_tW.root",
@@ -15,63 +15,47 @@ mcFiles = {
 
 #    "WJets": "WJets_inclusive.root",
     
-    "W1Jets_exclusive": "W1Jets.root",
-    "W2Jets_exclusive": "W2Jets.root",
-    "W3Jets_exclusive": "W3Jets.root",
-    "W4Jets_exclusive": "W4Jets.root",
+    "W1Jets_exclusive": "W1Jets_exclusive.root",
+    "W2Jets_exclusive": "W2Jets_exclusive.root",
+    "W3Jets_exclusive": "W3Jets_exclusive.root",
+    "W4Jets_exclusive": "W4Jets_exclusive.root",
     
 #    "TTJets_MassiveBinDECAY": "TTJets_MassiveBinDECAY.root",
     "TTJets_SemiLept": "TTJets_SemiLept.root",
     "TTJets_FullLept": "TTJets_FullLept.root",
 
-#    "GJets1": "GJets1.root",
-#    "GJets2": "GJets2.root",
+    "GJets1": "GJets1.root",
+    "GJets2": "GJets2.root",
     
-    "QCD_Pt_20_30_BCtoE": "QCD_20_30.root",
-    "QCD_Pt_30_80_BCtoE": "QCD_30_80.root",
-    "QCD_Pt_80_170_BCtoE": "QCD_80_170.root",
-    "QCD_Pt_170_250_BCtoE": "QCD_170_250.root",
-    "QCD_Pt_250_350_BCtoE":"QCD_250_350.root",
-    "QCD_Pt_350_BCtoE": "QCD_350.root",
+#    "QCD_Pt_20_30_BCtoE": "QCD_Pt_20_30_BCtoE.root",
+#    "QCD_Pt_30_80_BCtoE": "QCD_Pt_30_80_BCtoE.root",
+#    "QCD_Pt_80_170_BCtoE": "QCD_Pt_80_170_BCtoE.root",
+#    "QCD_Pt_170_250_BCtoE": "QCD_Pt_170_250_BCtoE.root",
+#    "QCD_Pt_250_350_BCtoE":"QCD_Pt_250_350_BCtoE.root",
+#    "QCD_Pt_350_BCtoE": "QCD_Pt_350_BCtoE.root",
     
-    "QCD_Pt_20_30_EMEnriched": "QCD_20_30_EM.root",
-    "QCD_Pt_30_80_EMEnriched": "QCD_30_80_EM.root",
-    "QCD_Pt_80_170_EMEnriched": "QCD_80_170_EM.root",
-    "QCD_Pt_170_250_EMEnriched": "QCD_170_250_EM.root",
-    "QCD_Pt_250_350_EMEnriched": "QCD_250_350_EM.root",
-    "QCD_Pt_350_EMEnriched": "QCD_350_EM.root",
+#    "QCD_Pt_20_30_EMEnriched": "QCD_Pt_20_30_EMEnriched.root",
+#    "QCD_Pt_30_80_EMEnriched": "QCD_Pt_30_80_EMEnriched.root",
+#    "QCD_Pt_80_170_EMEnriched": "QCD_Pt_80_170_EMEnriched.root",
+#    "QCD_Pt_170_250_EMEnriched": "QCD_Pt_170_250_EMEnriched.root",
+#    "QCD_Pt_250_350_EMEnriched": "QCD_Pt_250_350_EMEnriched.root",
+#    "QCD_Pt_350_EMEnriched": "QCD_Pt_350_EMEnriched.root",
+#    "QCDMu": "QCDMu.root"
     
     }
 
 
-dataFiles = {
-    "Ele_A1": "SingleEleA1.root",
-    "Ele_B" : "SingleEleB.root",
-    "Ele_C1": "SingleEleC1.root",
-    "Ele_C2": "SingleEleC2.root",
-    "Ele_D": "SingleEleD.root"
+dataFiles_ele = {
+    "Ele": "SingleEle.root"
+    }
+dataFiles_mu = {
+    "Mu": "SingleMu.root"
     }
 
-dataLumi = {
-    "Ele_A1": 78,
-    "Ele_B" : 4696,
-    "Ele_C1": 485,
-    "Ele_C2": 5724,
-    "Ele_D": 6510
+dataLumi_ele = {
+    "Ele": 19728
     }
 
-qcdTempFiles = {
- #   "Ele_A1": "SingleEleA1__pb.root",
- #   "Ele_B" : "SingleEleB_5138_pb.root",
- #   "Ele_C1": "SingleEleC1_495_pb.root",
- #   "Ele_C2": "SingleEleC2_5948_pb.root",
- #   "Ele_D": "SingleEleD_6929_pb.root"
-    }
-
-invLumi = {
-    "Ele_A1": 82,
-    "Ele_B": 5138,
-    "Ele_C1": 495,
-    "Ele_C2": 5948,
-    "Ele_D": 6929
+dataLumi_mu = {
+    "Mu" : 19739 
     }
