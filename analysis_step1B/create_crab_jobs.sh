449c29f93b10138cf01d431f5667c7dc34cbc3f4
jpata <joosep.pata@gmail.com>
2013-06-13 12:38:38 +0300

    cleanup

--- analysis_step1B/create_crab_jobs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-timestamp=`eval date +%m_%d`
-
-python $CMSSW_BASE/../util/datasets.py -t stpol_step1_$timestamp -T $CMSSW_BASE/../crabs/crab_MC_step1B_local.cfg -d S1B_MC -o crabs/step1B_MC_$timestamp
-
