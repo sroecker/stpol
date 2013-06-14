6048da9e5d1d78329770bb2760653dd4a6005deb
jpata <joosep.pata@gmail.com>
2013-06-14 15:52:43 +0300

    comments

--- analysis_step3/resubmit_failed_task.sh
+++ analysis_step3/resubmit_failed_task.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #Resubmits the step3 job based on the failed slurm-JOBID.out file as the argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 /path/to/failed/slurm-12345.out"
+    echo "This script will look for the x* file in the slurm.out and resubmit the task based on the corresponding task_x*"
+    exit 1
+fi
 infile=`readlink -f $1`
 TASKCODE=$(grep "Input file is" $infile | grep -oe "x[0-9]*")
 JOBID=$(grep "SLURM job ID" $infile | grep -oe "=[0-9]*")
