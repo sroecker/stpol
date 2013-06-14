825e568622de94ccb18568f81698ba4dc038ee50
Liis Rebane <polaarrebane@gmail.com>
2013-06-14 15:32:13 +0300

    specify electron and muon datasetes

--- analysis_step3/make_trees.sh
+++ analysis_step3/make_trees.sh
@@ -9,12 +9,12 @@ mkdir -p $OFDIR/ele
 mkdir -p $OFDIR/ele/iso/nominal
 mkdir -p $OFDIR/ele/antiiso/nominal
 
-$STPOL_DIR/analysis_step3/suball.sh "--lepton=mu" $OFDIR/mu/iso/nominal $FLDIR/iso/nominal/data/*
-$STPOL_DIR/analysis_step3/suball.sh "--lepton=mu" $OFDIR/mu/antiiso/nominal $FLDIR/antiiso/nominal/data/*
+$STPOL_DIR/analysis_step3/suball.sh "--lepton=mu" $OFDIR/mu/iso/nominal $FLDIR/iso/nominal/data/SingleMu*
+$STPOL_DIR/analysis_step3/suball.sh "--lepton=mu" $OFDIR/mu/antiiso/nominal $FLDIR/antiiso/nominal/data/SingleMu*
 $STPOL_DIR/analysis_step3/suball.sh "--lepton=mu --doControlVars --isMC" $OFDIR/mu/iso/nominal $FLDIR/iso/nominal/mc/*
 $STPOL_DIR/analysis_step3/suball.sh "--lepton=mu --doControlVars --isMC" $OFDIR/mu/antiiso/nominal $FLDIR/antiiso/nominal/mc/*
 
-$STPOL_DIR/analysis_step3/suball.sh "--lepton=ele" $OFDIR/ele/iso/nominal $FLDIR/iso/nominal/data/*
-$STPOL_DIR/analysis_step3/suball.sh "--lepton=ele" $OFDIR/ele/antiiso/nominal $FLDIR/antiiso/nominal/data/*
+$STPOL_DIR/analysis_step3/suball.sh "--lepton=ele" $OFDIR/ele/iso/nominal $FLDIR/iso/nominal/data/SingleEle*
+$STPOL_DIR/analysis_step3/suball.sh "--lepton=ele" $OFDIR/ele/antiiso/nominal $FLDIR/antiiso/nominal/data/SingleEle*
 $STPOL_DIR/analysis_step3/suball.sh "--lepton=ele --doControlVars --isMC" $OFDIR/ele/iso/nominal $FLDIR/iso/nominal/mc/*
 $STPOL_DIR/analysis_step3/suball.sh "--lepton=ele --doControlVars --isMC" $OFDIR/ele/antiiso/nominal $FLDIR/antiiso/nominal/mc/*
