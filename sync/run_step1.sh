4a5aa10cb4fc72ca32be1f99ebd6c0fd74429447
jpata <joosep.pata@gmail.com>
2013-06-14 20:58:21 +0300

    run step1 without debug

--- sync/run_step1.sh
+++ sync/run_step1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "step1 sync"
 echo "Inclusive sample"
-time cmsRun $STPOL_DIR/runconfs/step1_newCmdLine_cfg.py doSkimming=False doDebug=True inputFiles_load=$STPOL_DIR/sync/inclusive/files.txt outputFile=$STPOL_DIR/sync/inclusive/step1.root &> $STPOL_DIR/sync/inclusive/log_step1.txt &
+time cmsRun $STPOL_DIR/runconfs/step1_newCmdLine_cfg.py doSkimming=False doDebug=False inputFiles_load=$STPOL_DIR/sync/inclusive/files.txt outputFile=$STPOL_DIR/sync/inclusive/step1.root &> $STPOL_DIR/sync/inclusive/log_step1.txt &
 echo "Exclusive sample"
-time cmsRun $STPOL_DIR/runconfs/step1_newCmdLine_cfg.py doSkimming=False doDebug=True  inputFiles_load=$STPOL_DIR/sync/exclusive/files.txt outputFile=$STPOL_DIR/sync/exclusive/step1.root &> $STPOL_DIR/sync/exclusive/log_step1.txt &
+time cmsRun $STPOL_DIR/runconfs/step1_newCmdLine_cfg.py doSkimming=False doDebug=False  inputFiles_load=$STPOL_DIR/sync/exclusive/files.txt outputFile=$STPOL_DIR/sync/exclusive/step1.root &> $STPOL_DIR/sync/exclusive/log_step1.txt &
